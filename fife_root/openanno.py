2b8692940652c1d1bc1a54a6cb20157172a4268f
LinuxDonald <LinuxDonald@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2007-10-27 20:24:50 +0000

    *sync with fife
    git-svn-id: http://svn.unknown-horizons.org/trunk@31 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- fife_root/openanno.py
+++ fife_root/openanno.py
@@ -347,7 +347,7 @@ class World(object):
 			if (evtlistener.newTarget):
 				ec = self.cameras['main'].toElevationCoordinates(evtlistener.newTarget)
 				self.target.setElevationCoordinates(ec)
-				self.agent.act('walk', self.target, 0.5)
+				self.agent.act('walk', self.target, 1.5)
 				evtlistener.newTarget = None
 			
 			if (evtlistener.quitRequested):
