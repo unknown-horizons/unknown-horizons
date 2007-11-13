3a8a0bdc1688ba42abd4b202129ec50f9e4b6843
LinuxDonald <LinuxDonald@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2007-11-13 22:13:43 +0000

    *added mainship animation *deltet char_ani and added mainship_ani from the map *maked the grafic`s transparent
    git-svn-id: http://svn.unknown-horizons.org/trunk@44 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- fife_root/openanno.py
+++ fife_root/openanno.py
@@ -302,7 +302,7 @@ class World(object):
 		# no movement at start
 		self.target.setLayerCoordinates(fife.ModelCoordinate(4,4))
 		
-		self.agent = self.agent_layer.getInstances('id', 'char_ani')[0]
+		self.agent = self.agent_layer.getInstances('id', 'mainship_ani')[0]
 		self.agent.addListener(self.reactor)
 		self.agent.act_here('walk', self.target, True)
 		#self.agentcoords = self.target.getElevationCoordinates()
