1abe83fbdaf7e3fb84a1a5127797a014767b7178
LinuxDonald <LinuxDonald@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-03-31 17:28:21 +0000

    *first test button when you click it the gui exit. The Image for the Button is from Andre
    git-svn-id: http://svn.unknown-horizons.org/trunk@116 d3468d7a-13f2-4ea4-bf00-366cb6352a40

---
+++ oa-gui-test/pychan_test.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys, os, re
+
+def _jp(path):
+	return os.path.sep.join(path.split('/'))
+
+_paths = ('../../engine/swigwrappers/python', '../../engine/extensions')
+for p in _paths:
+	if p not in sys.path:
+		sys.path.append(_jp(p))
+
+import fife
+import fifelog
+import basicapplication
+import pychan
+
+class PyChanExample(object):
+	def __init__(self,xmlFile):
+		self.xmlFile = xmlFile
+		self.widget = None
+	
+	def start(self):
+		self.widget = pychan.loadXML(self.xmlFile)
+		eventMap = {
+			'closeButton':self.stop,
+			'okButton'   :self.stop,
+			'button'     :self.stop
+		}
+		self.widget.mapEvents(eventMap, ignoreMissing = True)
+		self.widget.show()
+
+	def stop(self):
+		if self.widget:
+			self.widget.hide()
+		self.widget = None
+
+class DemoApplication(basicapplication.ApplicationBase):
+	def __init__(self):
+		super(DemoApplication,self).__init__()
+		
+		pychan.init(self.engine,debug=True)
+		pychan.setupModalExecution(self.mainLoop,self.breakFromMainLoop)
+		
+		self.gui = pychan.loadXML('content/gui/gui.xml')
+		
+		eventMap = {
+			'creditsLink'  : self.showCredits,
+			'closeButton'  : self.quit,
+			'button'  : self.quit,
+		}
+		self.gui.mapEvents(eventMap)
+		self.gui.show()
+		
+		self.currentExample = None
+		self.creditsWidget = None
+
+	def showCredits(self):
+		print pychan.loadXML('content/gui/credits.xml').execute({ 'okButton' : "Yay!" })
+
+if __name__ == '__main__':
+	import sys
+	if len(sys.argv) == 2:
+		app = TestXMLApplication(sys.argv[1])
+	else:
+		app = DemoApplication()
+	app.run()
