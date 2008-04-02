b844e796885ac4180f0c12f76060bd868f59e8c8
LinuxDonald <LinuxDonald@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-04-02 00:54:23 +0000

    *added oa_gui start script
    git-svn-id: http://svn.unknown-horizons.org/trunk@132 d3468d7a-13f2-4ea4-bf00-366cb6352a40

---
+++ start_oa_gui.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Change to your FIFE checkout dir
+fife_dir="/home/linuxdonald/dev/svn/fife/"
+
+echo "Trying to start OA with FIFE in '$fife_dir'"
+
+export PYTHONPATH="$fife_dir/engine/swigwrappers/python:$fife_dir/engine/extensions"
+export LD_LIBRARY_PATH="$fife_dir/ext/minizip:$fife_dir/ext/install/lib"
+
+cd oa-gui-test
+
+python ./pychan_test.py
