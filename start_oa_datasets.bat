accf0b6ef88652f37e0b8737c3fc7942d137968a
spq <spq@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-04-08 11:24:07 +0000

    oa start script for fifes datasets branch
    git-svn-id: http://svn.unknown-horizons.org/trunk@184 d3468d7a-13f2-4ea4-bf00-366cb6352a40

---
+++ start_oa_datasets.bat
@@ -0,0 +1,7 @@
+set fife_path=../FIFE/branches/active/datasets
+
+set PYTHONPATH=%fife_path%/engine/swigwrappers/python;%fife_path%/engine/extensions;%fife_path%
+
+set PATH=%PATH%;%fife_path%
+openanno.py
+pause
