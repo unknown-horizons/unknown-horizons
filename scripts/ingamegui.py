97dcc537dd1d988bd4c7155eb252d0591077978e
nihathrael <nihathrael@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-04-23 20:11:12 +0000

    * Added build tabs
    git-svn-id: http://svn.unknown-horizons.org/trunk@258 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- scripts/ingamegui.py
+++ scripts/ingamegui.py
@@ -28,17 +28,22 @@ class IngameGui():
         self.game = game
         self.gui = {}
         self.gui['status'] = pychan.loadXML('content/gui/status.xml')
-        self.gui['main'] = pychan.loadXML('content/gui/hud_main.xml')
-        self.toggle_visible('main')        
-        self.gui['main'].mapEvents({
-            'build' : pychan.tools.callbackWithArguments(self.toggle_visible, 'build'),
-            'zoomIn' : self.game.zoom_in,
-            'zoomOut' : self.game.zoom_out,
-            'rotateRight' : self.game.rotate_map_right,
-            'rotateLeft' : self.game.rotate_map_left,
-            'escButton' : self.game.main.gui.show
-        })
-        self.gui['build'] = pychan.loadXML('content/gui/hud_build.xml')
+        self.gui['build1'] = pychan.loadXML('content/gui/build_menu/hud_build.xml')
+        self.gui['build2'] = pychan.loadXML('content/gui/build_menu/hud_build_2.xml')
+        self.gui['build3'] = pychan.loadXML('content/gui/build_menu/hud_build_3.xml')
+        self.gui['build4'] = pychan.loadXML('content/gui/build_menu/hud_build_4.xml')
+        self.gui['build5'] = pychan.loadXML('content/gui/build_menu/hud_build_5.xml')
+        self.gui['build6'] = pychan.loadXML('content/gui/build_menu/hud_build_6.xml')
+        self.active_build = self.gui['build1']
+        for num in range(1,7):
+            self.gui['build'+str(num)].mapEvents({
+                'servicesTab' : pychan.tools.callbackWithArguments(self.build_menu_show, 1),
+                'residentsTab' : pychan.tools.callbackWithArguments(self.build_menu_show, 2),
+                'companiesTab' : pychan.tools.callbackWithArguments(self.build_menu_show, 3),
+                'militaryTab' : pychan.tools.callbackWithArguments(self.build_menu_show, 4),
+                'streetsTab' : pychan.tools.callbackWithArguments(self.build_menu_show, 5),
+                'specialTab' : pychan.tools.callbackWithArguments(self.build_menu_show, 6)
+            })
         self.gui['buildinfo'] = pychan.loadXML('content/gui/hud_buildinfo.xml')
         self.gui['chat'] = pychan.loadXML('content/gui/hud_chat.xml')
         self.gui['cityinfo'] = pychan.loadXML('content/gui/hud_cityinfo.xml')
@@ -48,7 +53,16 @@ class IngameGui():
         self.gui['ship'].mapEvents({
             'foundSettelmentButton' : self._ship_build
         })
-
+        self.gui['main'] = pychan.loadXML('content/gui/hud_main.xml')
+        self.toggle_visible('main')        
+        self.gui['main'].mapEvents({
+            'build' : self.build_toggle,
+            'zoomIn' : self.game.zoom_in,
+            'zoomOut' : self.game.zoom_out,
+            'rotateRight' : self.game.rotate_map_right,
+            'rotateLeft' : self.game.rotate_map_left,
+            'escButton' : self.game.main.gui.show
+        })
 
     def status_set(self, label, value):
         """Sets a value on the status bar.
@@ -75,3 +89,16 @@ class IngameGui():
         else:
             self.gui[guiname].show()
 
+    def build_menu_show(self, num):
+        """Shows the selected build menu
+        @var num: int with the menu id
+        """
+        self.active_build.hide()
+        self.active_build = self.gui['build' + str(num)]
+        self.active_build.show()
+
+    def build_toggle(self):
+        if self.active_build.isVisible():
+            self.active_build.hide()
+        else:
+            self.active_build.show()
