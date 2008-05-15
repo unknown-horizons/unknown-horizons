c7d21e55582fb4937ee77d6abf3e03efcde24e3c
spq <spq@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-05-15 00:56:42 +0000

    cleanup settings file
    git-svn-id: http://svn.unknown-horizons.org/trunk@308 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- settings.py
+++ settings.py
@@ -1,8 +1,2 @@
-# fife paths
-path                = '../fife/'
-
-# test settings
-MapFile             = 'content/datasets/maps/openanno-test-map.xml'
-TestCameraPlacement = 0
-TestRotationLayerName = "landLayer"
-TestAgentSpeed      = 0.5
+#fife path:
+path = '../fife/'
