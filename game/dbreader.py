12a7c1e977a8883e7d07b3fd0446114ffa6c8f73
spq <spq@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-05-29 00:53:04 +0000

    nicer the database and result class
    git-svn-id: http://svn.unknown-horizons.org/trunk@408 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- game/dbreader.py
+++ game/dbreader.py
@@ -37,6 +37,9 @@ class DbReader(object):
 		self.connection.create_function("regexp", 2, regexp)
 		self.cur = self.connection.cursor()
 
+	def __call__(self, query, *vals):
+		return self.query(query, tuple(*vals))
+
 	def query(self, command, vals = ()):
 		"""Executes a sql command.
 		@var command: str containing the raw sql command, with ? as placeholders for values (eg. SELELCT ? FROM ?).
@@ -71,8 +74,36 @@ class DbReader(object):
 
 class SqlResult(object):
 	"""Represents a SQL result"""
+
 	def __init__(self):
 		self.success = True
 		self.rows = None
 		self.error = None
 		self.affected = None
+
+	def __getattr__(self, name):
+		return getattr(self.rows, name)
+
+	def __iadd__(self, *args, **kwargs): return self.rows.__iadd__(*args, **kwargs)
+	def __imul__(self, *args, **kwargs): return self.rows.__imul__(*args, **kwargs)
+	def __iter__(self, *args, **kwargs): return self.rows.__iter__(*args, **kwargs)
+	def __le__(self, *args, **kwargs): return self.rows.__le__(*args, **kwargs)
+	def __len__(self, *args, **kwargs): return self.rows.__len__(*args, **kwargs)
+	def __lt__(self, *args, **kwargs): return self.rows.__lt__(*args, **kwargs)
+	def __mul__(self, *args, **kwargs): return self.rows.__mul__(*args, **kwargs)
+	def __ne__(self, *args, **kwargs): return self.rows.__ne__(*args, **kwargs)
+	def __repr__(self, *args, **kwargs): return self.rows.__repr__(*args, **kwargs)
+	def __reversed__(self, *args, **kwargs): return self.rows.__reversed__(*args, **kwargs)
+	def __rmul__(self, *args, **kwargs): return self.rows.__rmul__(*args, **kwargs)
+	def __setitem__(self, *args, **kwargs): return self.rows.__setitem__(*args, **kwargs)
+	def __setslice__(self, *args, **kwargs): return self.rows.__setslice__(*args, **kwargs)
+	def __add__(self, *args, **kwargs): return self.rows.__add__(*args, **kwargs)
+	def __contains__(self, *args, **kwargs): return self.rows.__contains__(*args, **kwargs)
+	def __delitem__(self, *args, **kwargs): return self.rows.__delitem__(*args, **kwargs)
+	def __delslice__(self, *args, **kwargs): return self.rows.__delslice__(*args, **kwargs)
+	def __eq__(self, *args, **kwargs): return self.rows.__eq__(*args, **kwargs)
+	def __ge__(self, *args, **kwargs): return self.rows.__ge__(*args, **kwargs)
+	def __getitem__(self, *args, **kwargs): return self.rows.__getitem__(*args, **kwargs)
+	def __getslice__(self, *args, **kwargs): return self.rows.__getslice__(*args, **kwargs)
+	def __gt__(self, *args, **kwargs): return self.rows.__gt__(*args, **kwargs)
+	def __hash__(self, *args, **kwargs): return self.rows.__hash__(*args, **kwargs)
