5a09d986de99e6c57245293a1880b68dfe9ac088
spq <spq@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-06-11 00:11:57 +0000

    server list classes which should handle the different types of queries
    git-svn-id: http://svn.unknown-horizons.org/trunk@455 d3468d7a-13f2-4ea4-bf00-366cb6352a40

---
+++ game/network.py
@@ -0,0 +1,109 @@
+# ###################################################
+# Copyright (C) 2008 The OpenAnno Team
+# team@openanno.org
+# This file is part of OpenAnno.
+#
+# OpenAnno is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+import urllib
+import re
+import game.main
+
+class Server(object):
+	re_ip_port = re.compile("^((?:[0-1]?[0-9]{1,2}|2(?:[0-4][0-9]|5[0-5]))[.](?:[0-1]?[0-9]{1,2}|2(?:[0-4][0-9]|5[0-5]))[.](?:[0-1]?[0-9]{1,2}|2(?:[0-4][0-9]|5[0-5]))[.](?:[0-1]?[0-9]{1,2}|2(?:[0-4][0-9]|5[0-5])))(?::((?:[0-5]?[0-9]{1,4}|6(?:[0-4][0-9]{3}|5(?:[0-4][0-9]{2}|5(?:[0-2][0-9]|3[0-5]))))))?$")
+	def __init__(self, address, port):
+		self.address = address
+		self.port = port
+		self.ping, self.map, self.players, self.maxplayers = None, None, None, None
+
+	def __eq__(self, other):
+		return self.address == other.address and self.port == other.port
+
+class ServerList(object):
+	def __init__(self):
+		self._servers = []
+		#todo: setup socket
+
+	def _clear(self):
+		self._servers.clear()
+
+	def _present(self, server):
+		return server in self._servers
+
+	def _add(self, server):
+		if self._present(server):
+			self._servers.remove(server)
+		self._servers.append(server)
+		self._query(server.address, server.port)
+
+	def _remove(self, server):
+		self._servers.remove(server)
+
+	def _query(self, address, port):
+		#todo: query server
+		pass
+
+	def _response(self, server):
+		if self._present(server):
+			#todo: update server
+			pass
+
+	#todo: add list functions
+
+class WANServerList(ServerList):
+	def __init__(self):
+		super(WANServerList, self).__init__()
+		self.update()
+
+	def update(self):
+		self._clear()
+		for server in urllib.urlopen(game.main.settings.network.url_servers):
+			match = Server.re_ip_port.match(server)
+			if match:
+				server = Server(match.group(1), match.group(2) or game.main.settings.network.port)
+				if not self._present(server):
+					self._add(server)
+
+class LANServerList(ServerList):
+	def __init__(self):
+		super(LANServerList, self).__init__()
+		self.update()
+
+	def update(self):
+		self._query('255.255.255.255', game.main.settings.network.port)
+
+	def _response(self, server):
+		if not self._present(server):
+			self._add(server)
+		super(LANServerList, self)._response(server)
+
+class FavoriteServerList(ServerList):
+	def __init__(self):
+		super(FavoriteServerList, self).__init__()
+		#load from settings
+
+	def add(self, address, port):
+		self._add(Server(address, port))
+		#save to setings
+
+	def remove(self, address, port):
+		self._remove(Server(address, port))
+		#save to setings
+
+	def clear(self):
+		self._clear()
+		#save to setings
