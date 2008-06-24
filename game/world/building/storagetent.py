1b7779057609a658e99605f1176fbe1f491d11d7
nihathrael <nihathrael@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-06-24 09:46:10 +0000

    * add storagetent class, missed that yesterday night
    git-svn-id: http://svn.unknown-horizons.org/trunk@601 d3468d7a-13f2-4ea4-bf00-366cb6352a40

---
+++ game/world/building/storagetent.py
@@ -0,0 +1,59 @@
+# ###################################################
+# Copyright (C) 2008 The OpenAnno Team
+# team@openanno.org
+# This file is part of OpenAnno.
+#
+# OpenAnno is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+from building import Building
+import game.main
+
+class Storagetent(Building):
+	"""Class used for storage buildings that pic up ressources for the settlement"""
+	def __init__(self, x, y, owner, instance = None):
+		self.x = x
+		self.y = y
+		self.owner = owner
+		if instance == None:
+			self.createInstance(x, y)
+		else:
+			self._instance = instance
+			game.main.session.entities.updateInstance(self._instance.getId(), self)
+		self.health = 100
+		self.queue = []
+		self.pickups_active = 0
+		self.pickups = 2
+		self.storage = True
+
+	def start(self):
+		self.tick()
+
+	def tick(self):
+		if self.pickups_active < self.pickups and len(self.queue) > 0:
+			info = self.queue.pop(0) # Needs to be set, otherwise the item will not pop until the lambda function is called.
+			game.main.session.scheduler.add_new_object(lambda: self.pickup(info), self, int(game.main.session.timer.ticks_per_second*6))
+			self.pickups_active += 1
+		game.main.session.scheduler.add_new_object(self.tick, self, int(game.main.session.timer.ticks_per_second))
+
+	def pickup(self, info):
+		"""@param info: tuple (building, ressource_id)"""
+		self.pickups_active -= 1
+		load = info[0].get_ressources(info[1])
+		self.settlement.inventory.alter_inventory(info[1], load)
+
+	def add_to_queue(self, buiding_id, res_id):
+		self.queue.append((buiding_id, res_id))
