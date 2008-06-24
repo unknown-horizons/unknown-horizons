3bbd9e7ee3ab8a09a03439d3701218435f1e1357
spq <spq@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-06-24 23:52:16 +0000

    overwrite build preview method
    git-svn-id: http://svn.unknown-horizons.org/trunk@623 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- game/world/building/path.py
+++ game/world/building/path.py
@@ -22,4 +22,24 @@
 from building import Building
 
 class Path(Building):
-	pass
+	@classmethod
+	def getBuildList(cls, point1, point2):
+		island = None
+		settlement = None
+		buildings = []
+		for x in xrange(int(min(round(point1[0]), round(point2[0]))), 1 + int(max(round(point1[0]), round(point2[0])))):
+			for y in xrange(int(min(round(point1[1]), round(point2[1]))), 1 + int((min if x < int(max(round(point1[0]), round(point2[0]))) else max)(round(point1[1]), round(point2[1])))):
+				if x ==
+				new_island = game.main.session.world.get_island(x, y)
+				if new_island == None or (island != None and island != new_island):
+					continue
+				island = new_island
+
+				new_settlement = island.get_settlements(x, y, x, y)
+				new_settlement = None if len(new_settlement) == 0 else new_settlement.pop()
+				if new_settlement == None or (settlement != None and settlement != new_settlement): #we cant build where no settlement is or from one settlement to another
+					continue
+				settlement = new_settlement
+
+				buildings.append({'x' : x, 'y' : y})
+		return None if len(buildings) == 0 else {'island' : island, 'settlement' : settlement, 'buildings' : buildings}
