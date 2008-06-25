26a524428f939e594e6d369655d2e10a79c710e9
spq <spq@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-06-25 11:58:51 +0000

    use relative paths
    git-svn-id: http://svn.unknown-horizons.org/trunk@641 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- editor/loaders.py
+++ editor/loaders.py
@@ -42,6 +42,26 @@ def _save(file, engine, map):
 
 	db("detach island")
 
+def pathsplit(p, rest=[]):
+	(h,t) = os.path.split(p)
+	if len(h) < 1: return [t]+rest
+	if len(t) < 1: return [h]+rest
+	return pathsplit(h,[t]+rest)
+
+def commonpath(l1, l2, common=[]):
+	if len(l1) < 1: return (common, l1, l2)
+	if len(l2) < 1: return (common, l1, l2)
+	if l1[0] != l2[0]: return (common, l1, l2)
+	return commonpath(l1[1:], l2[1:], common+[l1[0]])
+
+def relpath(p1, p2):
+	(common,l1,l2) = commonpath(pathsplit(p1), pathsplit(p2))
+	p = []
+	if len(l1) > 0:
+		p = [ '../' * len(l1) ]
+	p = p + l2
+	return os.path.join( *p )
+
 def _init(engine):
 	global db
 	db = DbReader(':memory:')
@@ -55,7 +75,7 @@ def _init(engine):
 		visual = object.get2dGfxVisual()
 
 		for rotation, file in db("SELECT rotation, (select file from data.animation where data.animation.animation_id = data.action.animation order by frame_end limit 1) FROM data.action where ground=?", ground_id):
-			img = engine.getImagePool().addResourceFromFile(os.path.abspath(os.path.dirname(__file__) + '/../' + str(file)))
+			img = engine.getImagePool().addResourceFromFile(relpath(os.getcwd(), os.path.abspath(os.path.dirname(__file__) + '/../' + str(file))))
 			visual.addStaticImage(int(rotation), img)
 			img = engine.getImagePool().getImage(img)
 			img.setXShift(0)
