ed21d005baf7d14c2df71ee65f9f7782ad499e93
greyghost <greyghost@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-06-25 11:16:21 +0000

    remove unused stuff ..
    git-svn-id: http://svn.unknown-horizons.org/trunk@638 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- development/debug_oa.bat
+++
@@ -1,6 +0,0 @@
-set fife_path=../fife/trunk
-
-set PYTHONPATH=%fife_path%/engine/swigwrappers/python;%fife_path%/engine/extensions;%fife_path%
-
-set PATH=%PATH%;%fife_path%;F:\Python25;F:\Dokumente und Einstellungen\spq\Desktop\Projekte\FIFE\trunk\build\win32\applications\mingw\bin
-gdb --args python openanno.py
