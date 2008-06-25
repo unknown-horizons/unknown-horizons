847e07c615bf7eff4dd3aef3f2fd00778a0d3274
spq <spq@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-06-25 11:44:53 +0000

    work on editor...
    git-svn-id: http://svn.unknown-horizons.org/trunk@639 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- editor/loaders.py
+++ editor/loaders.py
@@ -26,13 +26,7 @@ import os.path
 
 fileExtensions = ('sqlite',)
 _inited = False
-
 def _load(file, engine):
-	global _inited
-	if not _inited:
-		_init(engine)
-		_inited = True
-
 	if not db("attach ? AS island", file).success:
 		raise WrongFileType(file)
 
@@ -41,14 +35,9 @@ def _load(file, engine):
 	db("detach island")
 
 def _save(file, engine, map):
-	global _inited
-	if not _inited:
-		_init(engine)
-		_inited = True
-
 	if not db("attach ? AS island", file).success:
 		raise WrongFileType(file)
-	
+
 	print 'todo'
 
 	db("detach island")
@@ -56,19 +45,50 @@ def _save(file, engine, map):
 def _init(engine):
 	global db
 	db = DbReader(':memory:')
-	db("attach ? AS data", os.path.abspath(os.path.basename(__file__) + '/../content/openanno.sqlite'))
+	db("attach ? AS data", os.path.abspath(os.path.dirname(__file__) + '/../content/openanno.sqlite'))
+
+	for (ground_id,) in db("SELECT rowid FROM data.ground"):
+		print 'Loading ground #' + str(ground_id) + '...'
+		object = engine.getModel().createObject(str(ground_id), 'ground')
+		object.thisown = 0
+		fife.ObjectVisual.create(object)
+		visual = object.get2dGfxVisual()
+
+		for rotation, file in db("SELECT rotation, (select file from data.animation where data.animation.animation_id = data.action.animation order by frame_end limit 1) FROM data.action where ground=?", ground_id):
+			img = engine.getImagePool().addResourceFromFile(os.path.abspath(os.path.dirname(__file__) + '/../' + str(file)))
+			visual.addStaticImage(int(rotation), img)
+			img = engine.getImagePool().getImage(img)
+			img.setXShift(0)
+			img.setYShift(0)
 
 def loadMapFile(path, engine, content = ''):
+	global _inited
+	if not _inited:
+		_init(engine)
+		_inited = True
 	_load(path, engine)
 
 def saveMapFile(path, engine, map, importList=[]):
+	global _inited
+	if not _inited:
+		_init(engine)
+		_inited = True
 	_save(path, engine, map)
 
 def loadImportFile(path, engine):
-	pass
+	global _inited
+	if not _inited:
+		_init(engine)
+		_inited = True
 
 def loadImportDir(path, engine):
-	pass
+	global _inited
+	if not _inited:
+		_init(engine)
+		_inited = True
 
 def loadImportDirRec(path, engine):
-	pass
+	global _inited
+	if not _inited:
+		_init(engine)
+		_inited = True
