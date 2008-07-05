d290ed4de8b050ad1113b59eca83b92ff62dbf22
totycro <totycro@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-07-05 18:08:44 +0000

    Added possibly buggy move callback
    git-svn-id: http://svn.unknown-horizons.org/trunk@814 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- game/world/units/unit.py
+++ game/world/units/unit.py
@@ -26,15 +26,16 @@ class Unit(fife.InstanceActionListener):
 	def __init__(self, x, y):
 		if self._object is None:
 			self.__class__._loadObject()
-		self.last_position = (x, y)
-		self.position = (x, y)
-		self.target = (x, y)
+		self.last_unit_position = (x, y)
+		self.unit_position = (x, y)
+		self.move_target = (x, y)
 		self.next_target = (x, y)
 		self._instance = game.main.session.view.layers[1].createInstance(self._object, fife.ModelCoordinate(int(x), int(y), 0), game.main.session.entities.registerInstance(self))
 		fife.InstanceVisual.create(self._instance)
 		self._instance.act('default', self._instance.getLocation(), True)
 		super(Unit, self).__init__()
 		self._instance.addActionListener(self)
+		self.move_callback = None
 
 	def start(self):
 		pass
@@ -45,7 +46,7 @@ class Unit(fife.InstanceActionListener):
 		@param action: string representing the action that is finished.
 		"""
 		location = fife.Location(self._instance.getLocation().getLayer())
-		location.setExactLayerCoordinates(fife.ExactModelCoordinate(self.position[0] + self.position[0] - self.last_position[0], self.position[1] + self.position[1] - self.last_position[1], 0))
+		location.setExactLayerCoordinates(fife.ExactModelCoordinate(self.unit_position[0] + self.unit_position[0] - self.last_unit_position[0], self.unit_position[1] + self.unit_position[1] - self.last_unit_position[1], 0))
 		self._instance.act('default', location, True)
 		game.main.session.view.cam.refresh()
 
@@ -54,51 +55,55 @@ class Unit(fife.InstanceActionListener):
 		@param x: int x coordinate
 		@param y: int y coordinate
 		"""
-		self.target = (x, y)
+		self.move_target = (x, y)
 		self.move_callback = callback
-		if self.next_target == self.position:
+		if self.next_target == self.unit_position:
 			#calculate next target
-			self.next_target = self.position
-			if self.target[0] > self.position[0]:
-				self.next_target = (self.position[0] + 1, self.next_target[1])
-			elif self.target[0] < self.position[0]:
-				self.next_target = (self.position[0] - 1, self.next_target[1])
-			if self.target[1] > self.position[1]:
-				self.next_target = (self.next_target[0], self.position[1] + 1)
-			elif self.target[1] < self.position[1]:
-				self.next_target = (self.next_target[0], self.position[1] - 1)
+			self.next_target = self.unit_position
+			if self.move_target[0] > self.unit_position[0]:
+				self.next_target = (self.unit_position[0] + 1, self.next_target[1])
+			elif self.move_target[0] < self.unit_position[0]:
+				self.next_target = (self.unit_position[0] - 1, self.next_target[1])
+			if self.move_target[1] > self.unit_position[1]:
+				self.next_target = (self.next_target[0], self.unit_position[1] + 1)
+			elif self.move_target[1] < self.unit_position[1]:
+				self.next_target = (self.next_target[0], self.unit_position[1] - 1)
 			#setup movement
 			location = fife.Location(self._instance.getLocation().getLayer())
 			location.setExactLayerCoordinates(fife.ExactModelCoordinate(self.next_target[0], self.next_target[1], 0))
 			self._instance.move('default', location, 4.0/3.0)
 			#setup next timer
-			game.main.session.scheduler.add_new_object(self.move_tick, self, 12 if self.next_target[0] == self.position[0] or self.next_target[1] == self.position[1] else 17)
+			game.main.session.scheduler.add_new_object(self.move_tick, self, 12 if self.next_target[0] == self.unit_position[0] or self.next_target[1] == self.unit_position[1] else 17)
 
 	def move_tick(self):
 		"""Called by the schedular, moves the unit one step for this tick.
 		"""
-		#sync position
-		self.last_position = self.position
-		self.position = self.next_target
+		#sync unit_position
+		self.last_unit_position = self.unit_position
+		self.unit_position = self.next_target
 		location = fife.Location(self._instance.getLocationRef().getLayer())
-		location.setExactLayerCoordinates(fife.ExactModelCoordinate(self.position[0], self.position[1], 0))
+		location.setExactLayerCoordinates(fife.ExactModelCoordinate(self.unit_position[0], self.unit_position[1], 0))
 		self._instance.setLocation(location)
 
-		if self.position != self.target:
+		if self.unit_position != self.move_target:
 			#calculate next target
-			self.next_target = self.position
-			if self.target[0] > self.position[0]:
-				self.next_target = (self.position[0] + 1, self.next_target[1])
-			elif self.target[0] < self.position[0]:
-				self.next_target = (self.position[0] - 1, self.next_target[1])
-			if self.target[1] > self.position[1]:
-				self.next_target = (self.next_target[0], self.position[1] + 1)
-			elif self.target[1] < self.position[1]:
-				self.next_target = (self.next_target[0], self.position[1] - 1)
+			self.next_target = self.unit_position
+			if self.move_target[0] > self.unit_position[0]:
+				self.next_target = (self.unit_position[0] + 1, self.next_target[1])
+			elif self.move_target[0] < self.unit_position[0]:
+				self.next_target = (self.unit_position[0] - 1, self.next_target[1])
+			if self.move_target[1] > self.unit_position[1]:
+				self.next_target = (self.next_target[0], self.unit_position[1] + 1)
+			elif self.move_target[1] < self.unit_position[1]:
+				self.next_target = (self.next_target[0], self.unit_position[1] - 1)
 			#setup movement
 			location = fife.Location(self._instance.getLocation().getLayer())
 			location.setExactLayerCoordinates(fife.ExactModelCoordinate(self.next_target[0], self.next_target[1], 0))
 			self._instance.move('default', location, 4.0/3.0)
 			#setup next timer
-			game.main.session.scheduler.add_new_object(self.move_tick, self, 12 if self.next_target[0] == self.position[0] or self.next_target[1] == self.position[1] else 17)
+			game.main.session.scheduler.add_new_object(self.move_tick, self, 12 if self.next_target[0] == self.unit_position[0] or self.next_target[1] == self.unit_position[1] else 17)
+		else:
+			if self.move_callback is not None:
+				print "EXEC CALLB", self.move_callback
+				self.move_callback()
 
