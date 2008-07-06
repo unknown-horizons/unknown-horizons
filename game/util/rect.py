88107f90ea68a0794eac2d5a2028b4cf093791c5
totycro <totycro@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-07-06 11:51:44 +0000

    Fixed typo-like bug
    git-svn-id: http://svn.unknown-horizons.org/trunk@829 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- game/util/rect.py
+++ game/util/rect.py
@@ -19,7 +19,7 @@
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # ###################################################
 
-import Point
+from point import Point
 
 class Rect(object):
 	def __init__(self, *args):
