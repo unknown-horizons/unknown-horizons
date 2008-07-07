d16208f621653cd4dc3bcc5923aee17eecdd4018
totycro <totycro@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-07-07 13:35:12 +0000

    Fixed rect
    git-svn-id: http://svn.unknown-horizons.org/trunk@847 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- game/util/rect.py
+++ game/util/rect.py
@@ -23,19 +23,19 @@ from point import Point
 
 class Rect(object):
 	def __init__(self, *args):
-		if len(args == 1) and isinstance(args[0], Rect): #args: rect
+		if len(args) == 1 and isinstance(args[0], Rect): #args: rect
 			self.top, self.left, self.right, self.bottom = args[0].top, args[0].left, args[0].right, args[0].bottom
-		elif len(args == 2) and isinstance(args[0], Point) and isinstance(args[1], Point): #args: edge1, edge2
+		elif len(args) == 2 and isinstance(args[0], Point) and isinstance(args[1], Point): #args: edge1, edge2
 			self.top = min(args[0].y, args[1].y)
 			self.left = min(args[0].x, args[1].x)
 			self.right = max(args[0].x, args[1].x)
 			self.bottom = max(args[0].y, args[1].y)
-		elif len(args == 3) and isinstance(args[0], Point) and isinstance(args[1], int) and isinstance(args[2], int): #args: position, width, height
+		elif len(args) == 3 and isinstance(args[0], Point) and isinstance(args[1], int) and isinstance(args[2], int): #args: position, width, height
 			self.top = args[0].y
 			self.left = args[0].x
 			self.right = self.left + args[1]
 			self.bottom = self.top + args[2]
-		elif len(args == 4) and isinstance(args[0], int) and isinstance(args[1], int) and isinstance(args[2], int) and isinstance(args[3], int):
+		elif len(args) == 4 and isinstance(args[0], int) and isinstance(args[1], int) and isinstance(args[2], int) and isinstance(args[3], int):
 			self.top = min(args[1], args[3])
 			self.left = min(args[0], args[2])
 			self.right = max(args[0], args[2])
