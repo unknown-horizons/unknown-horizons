0dabf51205ee70bb85c912238622b46228f413b4
phiker <phiker@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-07-20 20:02:55 +0000

    spq: added worldobject class, missed in last commit (git is schuld :P)
    git-svn-id: http://svn.unknown-horizons.org/trunk@1099 d3468d7a-13f2-4ea4-bf00-366cb6352a40

---
+++ game/util/worldobject.py
@@ -0,0 +1,37 @@
+# ###################################################
+# Copyright (C) 2008 The OpenAnno Team
+# team@openanno.org
+# This file is part of OpenAnno.
+#
+# OpenAnno is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+class WorldObject(object):
+	next_id = 1
+	objects = []
+
+	def __init__(self, *args, **kwargs):
+		self.__id = WorldObject.next_id
+		WorldObject.next_id = WorldObject.next_id + 1
+		super(WorldObject, self).__init__(*args, **kwargs)
+
+	def getId(self):
+		return self.__id
+
+	@classmethod
+	def getObjectById(cls, id):
+		pass
+
