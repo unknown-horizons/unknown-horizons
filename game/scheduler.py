4d16c312e7bb55fd6454545f0953a4a3a28058e9
yonibear <yonibear@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-07-21 18:07:07 +0000

    Scheduler reworked to use weak references, may contain bugs
    git-svn-id: http://svn.unknown-horizons.org/trunk@1127 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- game/scheduler.py
+++ game/scheduler.py
@@ -21,6 +21,7 @@
 
 import game.main
 from game.util import livingObject
+import weakref
 
 class Scheduler(livingObject):
 	""""Class providing timed callbacks.
@@ -40,21 +41,23 @@ class Scheduler(livingObject):
 		"""
 		self.cur_tick = tick_id
 		if self.schedule.has_key(self.cur_tick):
-			for object in self.schedule[self.cur_tick]:
-				object.callback()
-				if object.loops > 0 or object.loops is -1:
-					self.add_object(object) # readd object
+			for callback in self.schedule[self.cur_tick]:
+				callback.callback()
+				assert callback.loops >= -1
+				if callback.loops is not 0:
+					self.add_object(callback) # readd object
 			del self.schedule[self.cur_tick]
+		assert (len(self.schedule) == 0) or self.schedule.keys()[0] > self.cur_tick
 
-	def add_object(self, object):
+	def add_object(self, callback_obj):
 		"""Adds a new CallbackObject instance to the callbacks list
-		@param object: CallbackObject type object, containing all neccessary  information
+		@param callback_obj: CallbackObject type object, containing all neccessary  information
 		"""
-		if not self.schedule.has_key(self.cur_tick + object.runin):
-			self.schedule[self.cur_tick + object.runin] = []
-		if object.loops > 0:
-			object.loops -= 1
-		self.schedule[self.cur_tick + object.runin].append(object)
+		if not self.schedule.has_key(self.cur_tick + callback_obj.runin):
+			self.schedule[self.cur_tick + callback_obj.runin] = []
+		if callback_obj.loops > 0:
+			callback_obj.loops -= 1
+		self.schedule[self.cur_tick + callback_obj.runin].append(callback_obj)
 
 	def add_new_object(self, callback, class_instance, runin=1, loops=1):
 		"""Creates a new CallbackObject instance and calls the self.add_object() function.
@@ -62,15 +65,26 @@ class Scheduler(livingObject):
 		@param class_instance: class instance the function belongs to.
 		@param runin: int number of ticks after which the callback is called. Standard is 1, run next tick.
 		@param loops: How often the callback is called. -1 = infinit times. Standard is 1, run once."""
-		object = CallbackObject(callback, class_instance, runin, loops)
-		self.add_object(object)
+		
+		callback_obj = CallbackObject(self, callback, class_instance, runin, loops)
+		self.add_object(callback_obj)
+		
+	def rem_object(self, callback_obj):
+		"""Removes a CallbackObject from all callback lists
+		@param callback_obj: CallbackObject to remove
+		"""
+		for key in self.schedule:
+			for i in xrange(0, self.schedule[key].count(callback_obj)):
+				self.schedule[key].remove(callback_obj)			
+		
 
+	#TODO: Check if this is still necessary for weak referenced objects
 	def rem_all_classinst_calls(self, class_instance):
 		"""Removes all callbacks from the scheduler that belong to the class instance class_inst."""
 		for key in self.schedule:
-			for object in self.schedule[key]:
-				if object.class_instance is class_instance:
-					self.schedule[key].remove(object)
+			for callback_obj in self.schedule[key]:
+				if callback_obj.class_instance is class_instance:
+					self.schedule[key].remove(callback_obj)
 
 	def rem_call(self, instance, callback):
 		"""Removes all callbacks of 'instance' that are 'callback'
@@ -78,10 +92,10 @@ class Scheduler(livingObject):
 		@param callback: the function to remove
 		"""
 		for key in self.schedule:
-			for object in self.schedule[key]:
-				if object.class_instance is instance and object.callback == callback:
-					print 'REM_CALL', object.callback
-					self.schedule[key].remove(object)
+			for callback_obj in self.schedule[key]:
+				if callback_obj.class_instance is instance and callback_obj.callback == callback:
+					print 'REM_CALL', callback_obj.callback
+					self.schedule[key].remove(callback_obj)
 
 	def end(self):
 		self.schedule = {}
@@ -91,14 +105,40 @@ class Scheduler(livingObject):
 
 class CallbackObject(object):
 	"""Class used by the TimerManager Class to organize callbacks."""
-	def __init__(self,  callback, class_instance, runin=1, loops=1):
+	def __init__(self, scheduler, callback, class_instance, runin=1, loops=1):
 		"""Creates the CallbackObject instance.
+		@param scheduler: reference to the scheduler, necessary to react properly on weak reference callbacks
 		@param callback: lambda function callback, which is called runin ticks.
 		@param class_instance: class instance the original function(not the lambda function!) belongs to.
 		@param runin: int number of ticks after which the callback is called. Standard is 1, run next tick.
 		@param loops: How often the callback is called. -1 = infinit times. Standard is 1, run once.
+		@param weakref_aciton: A callback to register with the weak reference
 		"""
-		self.callback = callback
-		self.class_instance = class_instance
+		
+		# Do some input validation, otherwise errors occure long after wrong data was added
+		# TODO: Use garbage collector to check for strong references in debug mode
+		if not callable(callback):
+			raise ValueError("callback parameter is not callable")
+
+		if runin < 1:
+			raise ValueError("Can't schedule callbacks in the past, runin must be a positive number")
+			
+		if (loops < -1) or (loops is 0):
+			raise ValueError("Loop count must be a positive number or -1 for infinite repeat")
+
+		
+		self.scheduler = scheduler
 		self.runin = runin
 		self.loops = loops
+		
+		# We have to unwrap bound methods, because they normally hold a strong reference
+		from new import instancemethod
+		if isinstance(callback, instancemethod):
+			func = callback.im_func
+			ref = weakref.ref(callback.im_self)
+			self.callback = lambda: func(ref())
+		else:
+			self.callback = callback
+			
+		self.class_instance = weakref.ref(class_instance, lambda ref: self.scheduler.rem_object(self))
+	
