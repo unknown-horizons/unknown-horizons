d454240902bd972a25da7014fc712984d5c88d51
totycro <totycro@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-07-29 13:38:20 +0000

    Removed deprecated line
    git-svn-id: http://svn.unknown-horizons.org/trunk@1225 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- game/world/consumer.py
+++ game/world/consumer.py
@@ -19,7 +19,6 @@
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # ###################################################
 from game.world.storageholder import StorageHolder
-from game.world.units.carriage import BuildingCarriage
 from game.util import Rect, Point, WeakList
 import game.main
 
