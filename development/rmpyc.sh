b41d8e9bf1f179428e982dbcf7f13c077a71a264
spq <spq@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-08-01 12:16:26 +0000

    typo.
    git-svn-id: http://svn.unknown-horizons.org/trunk@1231 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- development/rmpyc.sh
+++ development/rmpyc.sh
@@ -2,5 +2,5 @@
 if [ $# -gt 0 ]; then
 	find "$@" -type f -iname '*.pyc' -o  -iname '*.pyo' -exec rm {} \;
 else
-	find "$@" -type f -iname '*.pyc' -o  -iname '*.pyo' -exec rm {} \;
+	find . -type f -iname '*.pyc' -o  -iname '*.pyo' -exec rm {} \;
 fi
