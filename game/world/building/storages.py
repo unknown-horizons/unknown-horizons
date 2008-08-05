28b0bcfbcba05bdce5be74eb7264f76e0b392de6
totycro <totycro@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-08-05 19:04:11 +0000

    Made storage building carriages travel merely on streets
    git-svn-id: http://svn.unknown-horizons.org/trunk@1285 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- game/world/building/storages.py
+++ game/world/building/storages.py
@@ -38,9 +38,7 @@ class StorageBuilding(Selectable, BuildableSingle, Consumer, Provider, Building)
 		self.inventory.addChangeListener(self._changed)
 
 	def create_carriage(self):
-		# reset to unit 8, as soon as pathfinding over streets is implemented
-		#self._Consumer__local_carriages.append(game.main.session.entities.units[8](self))
-		self._Consumer__local_carriages.append(game.main.session.entities.units[2](self))
+		self._Consumer__local_carriages.append(game.main.session.entities.units[8](self))
 
 	def select(self):
 		"""Runs neccesary steps to select the unit."""
