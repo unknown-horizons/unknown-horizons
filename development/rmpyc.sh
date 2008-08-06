8a763c8ce7f6b446fcaeef603a459de4277a31bf
lynxis <lynxis@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-08-06 17:41:32 +0000

    * rmpyc.sh : fixed it dirty. It's not so important a clear solution. But this script works now.
    git-svn-id: http://svn.unknown-horizons.org/trunk@1306 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- development/rmpyc.sh
+++ development/rmpyc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if [ $# -gt 0 ]; then
-	find "$@" -type f -iname '*.pyc' -o  -iname '*.pyo' -exec rm {} \;
+	find "$@" -type f -iname '*.pyc' -exec rm {} \;
+	find "$@" -type f -iname '*.pyo' -exec rm {} \;
 else
-	find . -type f -iname '*.pyc' -o  -iname '*.pyo' -exec rm {} \;
+	find . -type f -iname '*.pyc' -exec rm {} \;
+	find . -type f -iname '*.pyo' -exec rm {} \;
 fi
+
