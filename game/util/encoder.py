ce3094e68a14ea828c8b0d3856afff34e0c21ae8
spq <spq@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-08-15 12:15:12 +0000

    add gpl header, move classes listing out of function calls
    git-svn-id: http://svn.unknown-horizons.org/trunk@1354 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- game/util/encoder.py
+++ game/util/encoder.py
@@ -1,4 +1,27 @@
-def encode(obj, *classes):
+# ###################################################
+# Copyright (C) 2008 The OpenAnno Team
+# team@openanno.org
+# This file is part of OpenAnno.
+#
+# OpenAnno is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+classes=[]
+
+def encode(obj):
 	if obj is None:
 		return 'n'
 	if type(obj) == bool:
@@ -10,22 +33,22 @@ def encode(obj, *classes):
 	if type(obj) == str:
 		return 's' + str(len(obj)) + ':' + obj
 	if type(obj) == list:
-		return 'l' + str(len(obj)) + ''.join(encode(i, *classes) for i in obj)
+		return 'l' + str(len(obj)) + ''.join(encode(i) for i in obj)
 	if type(obj) == tuple:
-		return 't' + str(len(obj)) + ''.join(encode(i, *classes) for i in obj)
+		return 't' + str(len(obj)) + ''.join(encode(i) for i in obj)
 	if type(obj) == set:
-		return 'z' + str(len(obj)) + ''.join(encode(i, *classes) for i in obj)
+		return 'z' + str(len(obj)) + ''.join(encode(i) for i in obj)
 	if type(obj) == frozenset:
-		return 'f' + str(len(obj)) + ''.join(encode(i, *classes) for i in obj)
+		return 'f' + str(len(obj)) + ''.join(encode(i) for i in obj)
 	if type(obj) == dict:
-		return 'd' + str(len(obj)) + ''.join(encode(i, *classes) + encode(j, *classes) for i, j in obj.items())
+		return 'd' + str(len(obj)) + ''.join(encode(i) + encode(j) for i, j in obj.items())
 	if obj.__class__ in classes:
 		attrs = [i for i in dir(obj) if type(i) != str or i[0] != '_']
-		return 'o' + encode(obj.__class__.__name__, *classes) + str(len(attrs)) + ':' + ''.join(encode(i, *classes) + encode(getattr(obj,i), *classes) for i in attrs)
+		return 'o' + encode(obj.__class__.__name__) + str(len(attrs)) + ':' + ''.join(encode(i) + encode(getattr(obj,i)) for i in attrs)
 	raise NotImplementedError("Cant handle object " + repr(obj.__class__))
 
-def decode(text, *classes):
-	return __decode(text, 0, classes)[1]
+def decode(text):
+	return __decode(text, 0)[1]
 
 def __read_number(text, pos):
 	for p in xrange(pos, len(text)):
@@ -33,7 +56,7 @@ def __read_number(text, pos):
 			return text[pos:p]
 	return text[pos:]
 
-def __decode(text, pos, classes):
+def __decode(text, pos):
 	if text[pos] == 'n':
 		return (1, None)
 	if text[pos] == 'b':
@@ -52,7 +75,7 @@ def __decode(text, pos, classes):
 		length = 1 + len(i)
 		r = []
 		for x in xrange(0, int(i)):
-			l, o = __decode(text, pos + length, classes)
+			l, o = __decode(text, pos + length)
 			length += l
 			r.append(o)
 		return (length, tuple(r) if text[pos] == 't' else r)
@@ -61,7 +84,7 @@ def __decode(text, pos, classes):
 		length = 1 + len(i)
 		r = set()
 		for x in xrange(0, int(i)):
-			l, o = __decode(text, pos + length, classes)
+			l, o = __decode(text, pos + length)
 			length += l
 			r.add(o)
 		return (length, frozenset(r) if text[pos] == 'f' else r)
@@ -70,23 +93,23 @@ def __decode(text, pos, classes):
 		length = 1 + len(i)
 		r = {}
 		for x in xrange(0, int(i)):
-			l, k = __decode(text, pos + length, classes)
+			l, k = __decode(text, pos + length)
 			length += l
-			l, v = __decode(text, pos + length, classes)
+			l, v = __decode(text, pos + length)
 			length += l
 			r[k] = v
 		return (length, r)
 	if text[pos] == 'o':
-		length, class_name = __decode(text, pos + 1, classes)
+		length, class_name = __decode(text, pos + 1)
 		i = __read_number(text, 1 + length + pos)
 		length += 1 + len(i)
 		for c in classes:
 			if c.__name__ == class_name:
 				r = c.__new__(c)
 				for x in xrange(0, int(i)):
-					l, k = __decode(text, pos + length, classes)
+					l, k = __decode(text, pos + length)
 					length += l
-					l, v = __decode(text, pos + length, classes)
+					l, v = __decode(text, pos + length)
 					length += l
 					setattr(r, k, v)
 				return (length, r)
