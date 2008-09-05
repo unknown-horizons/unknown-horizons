52b0e722532762ea13a0f42f1eaaede6cf7bacff
nihathrael <nihathrael@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-09-05 09:07:55 +0000

    * Fixed the settler to consume textiles
    git-svn-id: http://svn.unknown-horizons.org/trunk@1424 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- game/world/building/settler.py
+++ game/world/building/settler.py
@@ -32,38 +32,4 @@ class Settler(SecondaryProducer, BuildableSingle, Selectable, Building):
 		self.inhabitants = 1 # TODE: read initial value from the db
 		self.max_inhabitants = 4 # TODO: read from db!
 
-		# run self.tick every second tick (NOTE: this should be discussed)
-		#game.main.session.scheduler.add_new_object(self.tick, self, 2, -1)
-
-	def tick(self):
-		"""Called by the ticker, to produce gold/inhabitants.
-		"""
-		# check if production is disabled and check if building is in storage mode or is a storage
-		if self.active_production_line == None or self.production[self.active_production_line]['time'] == 0:
-			return
-
-		self._current_production += 1
-		if (self._current_production % (self.production[self.active_production_line]['time']) == 0):
-			# time to produce res
-			for res in self.production[self.active_production_line]['res'].items():
-				# check for needed resources
-				if res[1] < 0:
-					if self.inventory.get_value(res[0]) + res[1] < 0:
-						return
-
-				# check for storage capacity
-				elif self.inventory.get_value(res[0]) == self.inventory.get_size(res[0]):
-					return
-
-			# everything ok, actual production:
-			for res in self.production[self.active_production_line]['res'].items():
-				self.inventory.alter_inventory(res[0], int(res[1]*float(self.inhabitants)/float(self.max_inhabitants)))
-
-				#debug:
-				#if res[1] >0: print "PRODUCING", res[0], "IN", self.id
-
-	def create_carriage(self):
-		# SecondaryProducer is also a consumer. Consumers create carriages by default. Settler shouldn't.
-		pass
-
 	# TODO: saving and loading
