c8e5b5d779ef93e2d0b29b2a0f73ede13f451ea3
totycro <totycro@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2008-09-07 15:42:33 +0000

    Added script to compress map for possible future use
    git-svn-id: http://svn.unknown-horizons.org/trunk@1428 d3468d7a-13f2-4ea4-bf00-366cb6352a40

---
+++ development/compress_map.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# ###################################################
+# Copyright (C) 2008 The OpenAnno Team
+# team@openanno.org
+# This file is part of OpenAnno.
+#
+# OpenAnno is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+import bz2
+import sys
+import os
+
+args = sys.argv[1:]
+
+if len(args) == 0:
+	print "USAGE: "+sys.argv[0]+" map1.sqlite map2.sqlite ... mapx.sqlite"
+	print 
+	print "maps will be saved as \"mapx.map\""
+	print "to convert all maps, use \""+sys.argv[0]+" content/maps/*sqlite\""
+	sys.exit()
+
+for filename in args:
+	if not filename.endswith(".sqlite"):
+		print "Invalid filename:",filename
+		continue
+	infile = None
+	try:
+		infile = open(filename, "r")
+	except IOError,e:
+		print "Error:",e.message
+		continue
+
+	outfile = open(filename.replace('.sqlite','.map'),'w')
+	outfile.write( bz2.compress( infile.read() ) )
+
+	
+
+
