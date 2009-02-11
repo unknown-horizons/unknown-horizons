d50778459111044b59bfe0eea765f7c12cd9fe9c
totycro <totycro@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2009-02-11 12:19:03 +0000

    Added a script for partly automatization of adding new animations to the db You can try it without caution, it just prints out the sql-queries.
    git-svn-id: http://svn.unknown-horizons.org/trunk@1775 d3468d7a-13f2-4ea4-bf00-366cb6352a40

---
+++ development/insert_animations_to_db.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+if ! ls content >/dev/null 2>&1; then
+	echo "please call from openanno root directory"
+	exit 1
+fi
+
+echo "works currently only for animated movement"
+echo 
+
+if [ $# -ne 3 ] ; then 
+	echo "USAGE: $0 animation_id_base action_set_id directory"
+	echo "EXAMPLE: $0 2 3 content/gfx/units/herder/walking_empty"
+	exit 
+fi
+
+animation_id=$(($1))
+action_set_id=$2
+dir=$3
+
+
+if [ $animation_id -eq 0 ] ; then 
+	echo 'invalid animation id'
+	exit 1
+fi
+
+if ! [ -d "$dir" ]; then 
+	echo 'invalid dir'
+	exit 1
+fi
+
+frames=`find "$dir" -name *.png | wc -l`
+action=`basename $dir`
+echo "found $frames frames"
+echo "action: $action"
+echo -n "rotations: "
+ls $dir
+
+echo -n "proceed? (y|n) ";
+read ans
+if [ "$ans" != "y" ] ; then
+	echo "Exiting."
+	exit 1
+fi
+echo
+
+
+ls $dir | while read rotation; do
+	cur_dir=${dir}/${rotation}
+
+	frames=`ls ${cur_dir}/*.png | wc -l`
+
+	frame_length_diff=`echo "scale=3; 1/$frames" | bc`
+	
+	frame_length=0
+
+	sql2="INSERT INTO action(action_set_id, action, rotation, animation_id) VALUES(\"$action_set_id\", \"$action\", \"$rotation\", \"$animation_id\")"
+
+	echo $sql2
+	
+	ls ${cur_dir}/*.png | while read img_file ; do
+	
+		sql1="INSERT INTO animation(animation_id, file, frame_length) VALUES(\"$animation_id\", \"$img_file\", \"$frame_length\");"
+		echo $sql1
+	
+	
+		frame_length=`echo "scale=3; $frame_length+$frame_length_diff" | bc`
+
+	done
+
+	let "animation_id++"
+done
