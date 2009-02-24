b4b4404b17308c5ed1d418edea4295bcac7b3188
kaschte <kaschte@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2009-02-24 22:32:48 +0000

    delete openanno.bat
    git-svn-id: http://svn.unknown-horizons.org/trunk@1853 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- openanno.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set datetime=%date%_%time%
-set datetime=%datetime: =0%
-set datetime=%datetime:.=-%
-set datetime=%datetime:,=-%
-set datetime=%datetime::=-%
-echo Starting Unknown Horizons, output will be written into logfile "unknownhorizons-%datetime%.log"
-run_uh.py >unknownhorizons-%datetime%.log 2>&1
-pause
