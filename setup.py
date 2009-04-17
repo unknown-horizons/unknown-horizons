caead327f83a465678e1e6e2eb1348f8fd71e057
christoph <christoph@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2009-04-17 16:23:06 +0000

    Make setup.py use translations
    git-svn-id: http://svn.unknown-horizons.org/trunk@2048 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- setup.py
+++ setup.py
@@ -9,6 +9,12 @@ data = []
 for i in filter(lambda x: len(x[2])  , os.walk('content')):
 	data.append( ('share/unknown-horizons/%s' % i[0], [ '%s/%s' % (i[0], j) for j in i[2]] ) )
 
+for i in os.listdir('po'):
+	if os.path.isdir('po/%s' % i):
+		if os.path.exists('po/%s/LC_MESSAGES/unknownhorizons.mo' % i):
+			data.append( ('share/locale/%s/LC_MESSAGES' % i, ('po/%s/LC_MESSAGES/unknownhorizons.mo' % i)))
+#trans = glob('po/*/LC_MESSAGES/unknownhorizons.mo')
+
 setup(name='UnknownHorizons',
 	  version='2009.0+svn1989',
 	  description='Realtime Economy Simulation and Strategy Game',
@@ -16,13 +22,14 @@ setup(name='UnknownHorizons',
 	  author_email='team@unknown-horizons.org',
 	  url='http://www.unknown-horizons.org',
 	  packages=['horizons', 
-				'horizons.util', 
-				'horizons.world',
-				'horizons.world.building',
-				'horizons.world.units',
-				'horizons.ai',
-				'horizons.gui',
-				'horizons.command',],
+		    'horizons.util', 
+		    'horizons.world',
+		    'horizons.world.building',
+		    'horizons.world.units',
+		    'horizons.ai',
+		    'horizons.i18n',
+		    'horizons.gui',
+		    'horizons.command',],
 	  data_files=data,
 	  scripts=['unknown-horizons']
 	  )
