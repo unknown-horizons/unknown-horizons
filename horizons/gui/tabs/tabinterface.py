4b242ee86e0ac91d59b0a571995b02f26b11714b
nihathrael <nihathrael@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2009-05-29 11:13:40 +0000

    * Added first version of the TabInterface class
    git-svn-id: http://svn.unknown-horizons.org/trunk@2134 d3468d7a-13f2-4ea4-bf00-366cb6352a40

---
+++ horizons/gui/tabs/tabinterface.py
@@ -0,0 +1,74 @@
+# ###################################################
+# Copyright (C) 2009 The Unknown Horizons Team
+# team@unknown-horizons.org
+# This file is part of Unknown Horizons.
+#
+# Unknown Horizons is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+class TabInterface(object):
+	"""
+	The TabInterface should be used by all classes that represent Tabs for the TabWidget.
+
+	It is important the currently should widget by the tab is always set to self.widget,
+	to ensure proper functionality.
+
+	Use the refresh() method to implement any redrawing of the widget. The TabWidget will
+	call this method based on callbacks. If you set any callbacks yourself, make sure you
+	get them removed when the widget is deleted.
+	"""
+	def __init__(self, **kwargs):
+		self.widget = None
+		pass
+
+	def init_values(self):
+		"""Call this methode after the widget has been initialised."""
+		self.x = self.widget.position[0]
+		self.y = self.widget.position[1]
+
+	def show(self):
+		"""Shows the current widget"""
+		self.widget.show()
+
+	def hide(self):
+		"""Hides the current wigdet"""
+		self.widget.hide()
+
+	def refresh(self):
+		"""This function is called by the TabWidget to redraw the widget."""
+		pass
+
+	def get_x(self):
+		return self.widget.position[0]
+
+	def set_x(self, value):
+		self.widget.position[0] = value
+
+	# Shortcut to set and retrieve the widget's current x position.
+	x = property(get_x, set_x)
+
+	def get_y(self):
+		return self.widget.position[1]
+
+	def set_y(self, value):
+		self.widget.position[1] = value
+
+	# Shortcut to set and retrieve the widget's current y position.
+	y = property(get_y, set_y)
+
+	def __del__(self):
+		"""Do cleanup work here."""
+		pass
