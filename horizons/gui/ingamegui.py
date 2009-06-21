unknown-horizons/unknown-horizons

commit 02d1b07c9904ff814aac09a61420e3560828b446

* Removed a lot of old widgets
git-svn-id: http://svn.unknown-horizons.org/trunk@2232 d3468d7a-13f2-4ea4-bf00-366cb6352a40

content/gui/gamemenu_button.xml
@@ -1,7 +1,0 @@
-<?xml version="1.0"?>
-<Container size="116,220">
-	<ImageButton name="gameMenuButton" up_image="content/gui/images/icons/hud/main/gamemenu.png" down_image="content/gui/images/icons/hud/main/gamemenu_h.png" hover_image="content/gui/images/icons/hud/main/gamemenu_h.png" size="32,32" position="48,143" />
-	
-	
-	<ImageButton name="helpLink" up_image="content/gui/images/icons/hud/main/help.png" down_image="content/gui/images/icons/hud/main/help_h.png" hover_image="content/gui/images/icons/hud/main/help_h.png" size="32,32" position="13,173" />
-</Container>

horizons/gui/ingamegui.py
@@ -47,36 +47,12 @@ class IngameGui(LivingObject):
 		self.resources_needed, self.resource_usable = {}, {}
 		self._old_menu = None
 
-		self.gui['encyclopedia'] = load_xml_translated('encyclopedia_button.xml')
-		self.gui['encyclopedia'].position = (5,	40)
-		self.gui['encyclopedia'].show()
 		self.gui['topmain'] = load_xml_translated('top_main.xml')
 		self.gui['topmain'].stylize('topmain')
 		self.gui['topmain'].position = (
 			horizons.main.fife.settings.getScreenWidth()/2 - self.gui['topmain'].size[0]/2 - 10,
 			5
 		)
-
-		self.gui['camTools'] = load_xml_translated('cam_tools.xml')
-		self.gui['camTools'].position = (0,0)
-		self.gui['camTools'].mapEvents({
-			'zoomIn' : horizons.main.session.view.zoom_in,
-			'zoomOut' : horizons.main.session.view.zoom_out,
-			'rotateRight' : horizons.main.session.view.rotate_right,
-			'rotateLeft' : horizons.main.session.view.rotate_left
-		})
-
-		self.gui['minimap_toggle'] = load_xml_translated('minimap_toggle_button.xml')
-		self.gui['minimap_toggle'].position = (
-			horizons.main.fife.settings.getScreenWidth() - self.gui['minimap_toggle'].size[0] - 15,
-			horizons.main.fife.settings.getScreenHeight() - self.gui['minimap_toggle'].size[1] -15,
-		)
-		self.gui['minimap_toggle'].show()
-		self.gui['minimap_toggle'].mapEvents({
-			'minimapToggle' : self.toggle_minmap,
-			'speedUp' : horizons.main.session.speed_up,
-			'speedDown' : horizons.main.session.speed_down
-		})
 
 		self.gui['minimap'] = load_xml_translated('minimap.xml')
 		self.gui['minimap'].position = (
@@ -109,16 +85,6 @@ class IngameGui(LivingObject):
 		self.gui['status_extra'].stylize('menu_black')
 
 		self.message_widget = MessageWidget(self.gui['topmain'].position[0] + self.gui['topmain'].size[0], 5)
-		self.gui['gamemenu'] = load_xml_translated('gamemenu_button.xml')
-		self.gui['gamemenu'].position = (
-			horizons.main.fife.settings.getScreenWidth() - self.gui['gamemenu'].size[0] - 3,
-			10
-		)
-		self.gui['gamemenu'].mapEvents({
-			'gameMenuButton' : horizons.main.gui.show_pause,
-			'helpLink'	 : horizons.main.gui.on_help
-		})
-		self.gui['gamemenu'].show()
 
 		self.gui['status_gold'] = load_xml_translated('status_gold.xml')
 		self.gui['status_gold'].stylize('menu_black')
@@ -170,10 +136,6 @@ class IngameGui(LivingObject):
 		self.gui['ship'] = load_xml_translated('hud_ship.xml')
 
 	def end(self):
-		self.gui['gamemenu'].mapEvents({
-			'gameMenuButton' : None
-		})
-
 		self.gui['ship'].mapEvents({
 			'foundSettelment' : None
 		})
@@ -185,18 +147,6 @@ class IngameGui(LivingObject):
 			'rotateLeft' : None
 		})
 
-		self.gui['minimap_toggle'].mapEvents({
-			'minimapToggle' : None,
-			'speedUp' : None,
-			'speedDown' : None
-		})
-
-		self.gui['camTools'].mapEvents({
-			'zoomIn' : None,
-			'zoomOut' : None,
-			'rotateRight' : None,
-			'rotateLeft' : None
-		})
 		for w in self.gui.values():
 			if w.parent is None:
 				w.hide()
@@ -332,8 +282,6 @@ class IngameGui(LivingObject):
 		self.gui['minimap'].show()
 		self.gui['leftPanel'].hide()
 		self.gui['leftPanel'].show()
-		self.gui['gamemenu'].hide()
-		self.gui['gamemenu'].show()
 
 	def show_ship(self, ship):
 		self.gui['ship'].findChild(name='buildingNameLabel').text = \
@@ -437,11 +385,6 @@ class IngameGui(LivingObject):
 			self.gui['minimap'].hide()
 		else:
 			self.gui['minimap'].show()
-
-		if self.gui['camTools'].isVisible():
-			self.gui['camTools'].hide()
-		else:
-			self.gui['camTools'].show()
 
 	def set_status_position(self, resource_name):
 		for i in xrange(1, 4):

content/gui/cam_tools.xml
@@ -1,13 +1,0 @@
-<?xml version="1.0"?>
-<Container size="100,100">
-		<ImageButton name="speedUp" up_image="content/gui/images/icons/hud/main/speed_up.png" down_image="content/gui/images/icons/hud/main/speed_up_h.png" hover_image="content/gui/images/icons/hud/main/speed_up_h.png" size="28,28" position="65,12"/>
-	<ImageButton name="speedDown" up_image="content/gui/images/icons/hud/main/speed_down.png" down_image="content/gui/images/icons/hud/main/speed_down_h.png" hover_image="content/gui/images/icons/hud/main/speed_down_h.png" size="28,28" position="45,35"/>
-
-	
-	<ImageButton name="rotateLeft" up_image="content/gui/images/icons/hud/main/rotate_left.png" down_image="content/gui/images/icons/hud/main/rotate_left_h.png" hover_image="content/gui/images/icons/hud/main/rotate_left_h.png" size="28,28" position="91,5" />
-	<ImageButton name="rotateRight" up_image="content/gui/images/icons/hud/main/rotate_right.png" down_image="content/gui/images/icons/hud/main/rotate_right_h.png" hover_image="content/gui/images/icons/hud/main/rotate_right_h.png" size="28,28" position="155,5" />
-
-
-	<ImageButton name="zoomOut" up_image="content/gui/images/icons/hud/main/zoom_out.png" down_image="content/gui/images/icons/hud/main/zoom_out_h.png" hover_image="content/gui/images/icons/hud/main/zoom_out_h.png" size="28,28" position="180,12" />
-	<ImageButton name="zoomIn" up_image="content/gui/images/icons/hud/main/zoom_in.png" down_image="content/gui/images/icons/hud/main/zoom_in_h.png" hover_image="content/gui/images/icons/hud/main/zoom_in_h.png" size="28,28" position="200,33" />	
-</Container>

content/gui/encyclopedia_button.xml
@@ -1,5 +1,0 @@
-<?xml version="1.0"?>
-<Container size="61,60">
-
-
-</Container>

content/gui/minimap.xml
@@ -1,10 +1,10 @@
 <?xml version="1.0"?>
 <Container size="275,260">
 	<Icon image="content/gui/images/background/map_bg.png" position="40,0" />
-		<ImageButton name="speedUp" up_image="content/gui/images/icons/hud/main/speed_up.png" down_image="content/gui/images/icons/hud/main/speed_up_h.png" hover_image="content/gui/images/icons/hud/main/speed_up_h.png" size="28,28" position="62,16"/>
+	<ImageButton name="speedUp" up_image="content/gui/images/icons/hud/main/speed_up.png" down_image="content/gui/images/icons/hud/main/speed_up_h.png" hover_image="content/gui/images/icons/hud/main/speed_up_h.png" size="28,28" position="62,16"/>
 	<ImageButton name="speedDown" up_image="content/gui/images/icons/hud/main/speed_down.png" down_image="content/gui/images/icons/hud/main/speed_down_h.png" hover_image="content/gui/images/icons/hud/main/speed_down_h.png" size="28,28" position="43,38"/>
 	<ImageButton name="rotateLeft" up_image="content/gui/images/icons/hud/main/rotate_left.png" down_image="content/gui/images/icons/hud/main/rotate_left_h.png" hover_image="content/gui/images/icons/hud/main/rotate_left_h.png" size="28,28" position="91,5" />
 	<ImageButton name="rotateRight" up_image="content/gui/images/icons/hud/main/rotate_right.png" down_image="content/gui/images/icons/hud/main/rotate_right_h.png" hover_image="content/gui/images/icons/hud/main/rotate_right_h.png" size="28,28" position="155,5" />
 	<ImageButton name="zoomOut" up_image="content/gui/images/icons/hud/main/zoom_out.png" down_image="content/gui/images/icons/hud/main/zoom_out_h.png" hover_image="content/gui/images/icons/hud/main/zoom_out_h.png" size="28,28" position="181,16" />
-	<ImageButton name="zoomIn" up_image="content/gui/images/icons/hud/main/zoom_in.png" down_image="content/gui/images/icons/hud/main/zoom_in_h.png" hover_image="content/gui/images/icons/hud/main/zoom_in_h.png" size="28,28" position="201,36" />		
+	<ImageButton name="zoomIn" up_image="content/gui/images/icons/hud/main/zoom_in.png" down_image="content/gui/images/icons/hud/main/zoom_in_h.png" hover_image="content/gui/images/icons/hud/main/zoom_in_h.png" size="28,28" position="201,36" />
 </Container>

content/gui/minimap_toggle_button.xml
@@ -1,10 +1,0 @@
-<?xml version="1.0"?>
-<Container size="60,230">
-	<ImageButton name="minimapToggle"  size="60,110" position="0,120" />
-	<ImageButton name="speedUp"  size="48,48" position="6,20"/>
-	<ImageButton name="speedDown"  size="48,48" position="6,70"/>
-
-	<!--<ImageButton name="minimapToggle" up_image="content/gui/images/misc/minimap_toggle_button.png" down_image="content/gui/images/misc/minimap_toggle_button_h.png" hover_image="content/gui/images/misc/minimap_toggle_button_h.png" size="60,110" position="0,120" />
-	<ImageButton name="speedUp" up_image="content/gui/images/icons/hud/main/speed_up.png" down_image="content/gui/images/icons/hud/main/speed_up_h.png" hover_image="content/gui/images/icons/hud/main/speed_up_h.png" size="48,48" position="6,20"/>
-	<ImageButton name="speedDown" up_image="content/gui/images/icons/hud/main/speed_down.png" down_image="content/gui/images/icons/hud/main/speed_down_h.png" hover_image="content/gui/images/icons/hud/main/speed_down_h.png" size="48,48" position="6,70"/>-->
-</Container>

commit ceb052076e62c0051892cae0718347e4b5dc2e36

Fixed bug introduced with r2232
git-svn-id: http://svn.unknown-horizons.org/trunk@2234 d3468d7a-13f2-4ea4-bf00-366cb6352a40

content/gui/gamemenu_button.xml
@@ -1,0 +1,7 @@
+<?xml version="1.0"?>
+<Container size="116,220">
+	<ImageButton name="gameMenuButton" up_image="content/gui/images/icons/hud/main/gamemenu.png" down_image="content/gui/images/icons/hud/main/gamemenu_h.png" hover_image="content/gui/images/icons/hud/main/gamemenu_h.png" size="32,32" position="48,143" />
+	
+	
+	<ImageButton name="helpLink" up_image="content/gui/images/icons/hud/main/help.png" down_image="content/gui/images/icons/hud/main/help_h.png" hover_image="content/gui/images/icons/hud/main/help_h.png" size="32,32" position="13,173" />
+</Container>

horizons/gui/ingamegui.py
@@ -85,6 +85,16 @@ class IngameGui(LivingObject):
 		self.gui['status_extra'].stylize('menu_black')
 
 		self.message_widget = MessageWidget(self.gui['topmain'].position[0] + self.gui['topmain'].size[0], 5)
+		self.gui['gamemenu'] = load_xml_translated('gamemenu_button.xml')
+		self.gui['gamemenu'].position = (
+			horizons.main.fife.settings.getScreenWidth() - self.gui['gamemenu'].size[0] - 3,
+			10
+		)
+		self.gui['gamemenu'].mapEvents({
+			'gameMenuButton' : horizons.main.gui.show_pause,
+			'helpLink'	 : horizons.main.gui.on_help
+		})
+		self.gui['gamemenu'].show()
 
 		self.gui['status_gold'] = load_xml_translated('status_gold.xml')
 		self.gui['status_gold'].stylize('menu_black')
@@ -136,6 +146,10 @@ class IngameGui(LivingObject):
 		self.gui['ship'] = load_xml_translated('hud_ship.xml')
 
 	def end(self):
+		self.gui['gamemenu'].mapEvents({
+			'gameMenuButton' : None
+		})
+
 		self.gui['ship'].mapEvents({
 			'foundSettelment' : None
 		})
@@ -282,6 +296,8 @@ class IngameGui(LivingObject):
 		self.gui['minimap'].show()
 		self.gui['leftPanel'].hide()
 		self.gui['leftPanel'].show()
+		self.gui['gamemenu'].hide()
+		self.gui['gamemenu'].show()
 
 	def show_ship(self, ship):
 		self.gui['ship'].findChild(name='buildingNameLabel').text = \