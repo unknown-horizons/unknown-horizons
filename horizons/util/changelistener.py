c016b7f62fb9e1f13d879ca4cdce8f8f625c6b99
nihathrael <nihathrael@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2009-07-01 06:23:09 +0000

    * Merged totycros and my solution for the changelistener bug
    git-svn-id: http://svn.unknown-horizons.org/trunk@2366 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- horizons/util/changelistener.py
+++ horizons/util/changelistener.py
@@ -19,8 +19,6 @@
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # ###################################################
 
-#because of unclean inheritance, bad usage of parameters and bad use of super in most world object, i cant trust that the init constructor is called.
-
 from living import LivingObject
 from weakmethod import WeakMethod
 
@@ -32,36 +30,24 @@ class Changelistener(LivingObject):
 	def __init(self):
 		self.__listeners = []
 
-	def __ensure_inited(self):
-		"""
-		TODO: Why does this exist? why isn't this inited always? change this.
-		"""
-		if not hasattr(self, '_Changelistener__listeners'):
-			self.__init()
-
 	def addChangeListener(self, listener):
-		self.__ensure_inited()
 		self.__listeners.append(WeakMethod(listener))
 
 	def removeChangeListener(self, listener):
-		self.__ensure_inited()
 		self.__listeners.remove(WeakMethod(listener))
 
 	def hasChangeListener(self, listener):
-		self.__ensure_inited()
 		if WeakMethod(listener) in self.__listeners:
 			return True
 		else:
 			return False
 
 	def _changed(self):
-		self.__ensure_inited()
 		for listener in self.__listeners:
 			listener()
 
 	def load(self, db, world_id):
-		#super(Changelistener, self).load(db, world_id)
-		self.__listeners = []
+		self.__init()
 
 	def end(self):
 		self.__listeners = None
