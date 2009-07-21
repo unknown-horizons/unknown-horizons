305ae839835c3c58ee9fa28e2b9903de3dc18514
totycro <totycro@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2009-07-21 11:57:04 +0000

    Added dirty script to help analysing profiles from the profile module
    git-svn-id: http://svn.unknown-horizons.org/trunk@2486 d3468d7a-13f2-4ea4-bf00-366cb6352a40

---
+++ development/print_profile_output.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import pstats
+import sys
+
+
+if len(sys.argv) == 0:
+	print 'profile_output.py file [ sortstats [ ( callees | callers ) ] ]'
+	sys.exit(1)
+
+p = pstats.Stats(sys.argv[1])
+
+p.strip_dirs()
+
+arg2 = None if len(sys.argv) < 3 else sys.argv[2]
+
+p.sort_stats(-1 if arg2 is None else arg2)
+
+if not len(sys.argv) > 3:
+	p.print_stats()
+elif sys.argv[3] == 'callees':
+	p.print_callees()
+elif sys.argv[3] == 'callers':
+	p.print_callers()
+else
+	print 'invalid arg'
+
