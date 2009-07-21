66d4f99121e01a6db327f2826030e6cdd5f7fd55
totycro <totycro@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2009-07-21 17:35:59 +0000

    Added cache function decorator
    git-svn-id: http://svn.unknown-horizons.org/trunk@2492 d3468d7a-13f2-4ea4-bf00-366cb6352a40

---
+++ horizons/util/decorators.py
@@ -0,0 +1,44 @@
+# ###################################################
+# Copyright (C) 2009 The Unknown Horizons Team
+# team@unknown-horizons.org
+# This file is part of Unknown Horizons.
+#
+# Unknown Horizons is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+"""Save general python function decorators here"""
+
+class cached(object):
+	"""Decorator that caches a function's return value each time it is called.
+   If called later with the same arguments, the cached value is returned, and
+   not re-evaluated.
+   """
+	def __init__(self, func):
+		self.func = func
+		self.cache = {}
+	def __call__(self, *args):
+		import pdb;pdb.set_trace()
+		try:
+			return self.cache[args]
+		except KeyError:
+			self.cache[args] = value = self.func(*args)
+			return value
+		except TypeError:
+			assert False, "Supplied invalid argument to cache decorator"
+	def __repr__(self):
+		"""Return the function's docstring."""
+		return self.func.__doc__
+
