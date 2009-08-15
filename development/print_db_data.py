eae2884e469b2c804d56f821e2a46b7baf47e4be
totycro <totycro@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2009-08-15 12:45:25 +0000

    Extended a script to print human readable data from the db to simplify handling the db
    git-svn-id: http://svn.unknown-horizons.org/trunk@2606 d3468d7a-13f2-4ea4-bf00-366cb6352a40

---
+++ development/print_db_data.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import os.path
+import sys
+
+dbfile = 'content/game.sqlite'
+
+if not os.path.exists(dbfile):
+	print 'please run from uh root dir'
+	sys.exit(1)
+
+sys.path.append(".")
+sys.path.append("./horizons")
+
+import environment
+environment.init()
+
+from dbreader import DbReader
+
+db = DbReader(dbfile)
+
+def get_obj_name(obj):
+	global db
+	if obj < 1000000:
+		return db("SELECT name FROM building where id = ?", obj)[0][0]
+	else:
+		return db("SELECT name FROM unit where id = ?", obj)[0][0]
+
+def get_res_name(res):
+	global db
+	return db("Select name from resource where rowid = ?", res)[0][0]
+
+def print_production_lines():
+	print 'Production Lines:'
+	for prod_line in db("SELECT id, object_id, time, enabled_by_default FROM production_line ORDER BY object_id"):
+		id = prod_line[0]
+		object = prod_line[1]
+	
+		str = ''
+		str += 'ProdLine %s of %s (time:%s;default:%s):\t' % (id, get_obj_name(object), prod_line[2], prod_line[3])
+	
+		str += 'consumes: '
+		for res, amount in db("SELECT resource, amount from production where production_line = ? and amount < 0 order by amount asc", id):
+			str += '%s %s, ' % (-amount, get_res_name(res))
+	
+		str += ';\tproduces: '
+		for res, amount in db("SELECT resource, amount from production where production_line = ? and amount > 0 order by amount asc", id):
+			str +=  '%s %s, ' % (amount, get_res_name(res))
+	
+		print str
+
+def print_res():
+	print "Resources (id: resource (value))"
+	for id, name, value in db("select id, name, value from resource"):
+		print "%s: %s (%s)" % (id, name, value)
+
+def print_building():
+	print "Buildings (id: name from class, size, radius):"
+	for id, name, c_type, c_package, x, y, radius in \
+			db('select id, name, class_type, class_package, size_x, size_y, radius from building'):
+		print "%s:\t%s\tfrom %s.%s, %sx%s, %s" % (id, name, c_package, c_type, x, y, radius)
+
+functions = {  
+		'res' : print_res,
+		'building' : print_building,
+		'lines' : print_production_lines,
+		}
+
+args = sys.argv
+args.remove('--fife-in-library-path')
+
+if len(args) == 1: 
+	print 'Start with one of those args: %s' % functions.keys()
+else:
+	functions[ args[1] ]()
