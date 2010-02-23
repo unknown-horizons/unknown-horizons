d2dc6657152d12a25aaf7358288578b105b74e34
greyghost <greyghost@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2010-02-23 13:34:20 +0000

    Cleanup after myself :P
    git-svn-id: http://svn.unknown-horizons.org/trunk@3245 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- horizons/gui/ingamegui.py
+++ horizons/gui/ingamegui.py
@@ -439,11 +439,9 @@ class IngameGui(LivingObject):
 		"""Applies the change_name dialogs input and hides it"""
 		new_name = self.widgets['change_name'].collectData('new_name')
 		self.widgets['change_name'].findChild(name='new_name').text = u''
-		if len(new_name) == 0 or new_name.isspace():
-			self.hide_change_name_dialog()
-		else:
+		if not (len(new_name) == 0 or new_name.isspace()):
 			instance.set_name(new_name)
-			self.hide_change_name_dialog()
+		self.hide_change_name_dialog()
 
 	def toggle_ingame_pause(self):
 		"""Called when the hotkey for pause is pressed. Displays pause notification and does
