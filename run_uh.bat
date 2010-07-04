16c6c99353e8e66b583c8f77a2af65cf554594ac
greyghost <greyghost@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2010-07-04 18:16:21 +0000

    Extend .bat to use debug with '--debug'.. deafult is still non debug.
    git-svn-id: http://svn.unknown-horizons.org/trunk@3447 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- run_uh.bat
+++ run_uh.bat
@@ -1,5 +1,21 @@
 @echo off
 echo Starting Unknown Horizons
+if '%1' == '--debug' goto debugMode
+if '%1' == '--help' goto help
+
 echo output will be written to logfile "unknownhorizons-DATETIME.log"
 run_uh.py --debug-log-only
 pause
+goto:EOF
+
+:debugMode
+python_d.exe run_uh.py --debug-log-only
+if errorlevel 1 (echo.
+echo Error! Please check if you have Python Debug installed!
+echo.)
+pause
+goto:EOF
+
+:help
+echo '--debug': Debug mode. Needs Python debug with FIFE debug compiled.
+echo '--help': Display Help information.
