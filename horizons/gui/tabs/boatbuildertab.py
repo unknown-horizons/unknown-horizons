10d5223d9255c25a4ca290ff4531bd619837324a
totycro <totycro@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2010-07-31 19:23:39 +0000

    Made new main boatbuilder tab work dynamically in principle.
    git-svn-id: http://svn.unknown-horizons.org/trunk@3626 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- horizons/gui/tabs/boatbuildertab.py
+++ horizons/gui/tabs/boatbuildertab.py
@@ -28,6 +28,7 @@ from tabinterface import TabInterface
 from horizons.command.production import AddProduction
 from horizons.gui.widgets  import TooltipButton
 from horizons.gui.tabs import OverviewTab
+from horizons.i18n import load_xml_translated
 
 class BoatbuilderTab(OverviewTab):
 	def __init__(self, instance):
@@ -35,27 +36,66 @@ class BoatbuilderTab(OverviewTab):
 			widget = 'tab_widget/boatbuilder/boatbuilder.xml',
 			instance = instance
 		)
-		if hasattr(self.instance, 'name') and self.widget.child_finder('name'):
-			name_widget = self.widget.child_finder('name')
-			name_widget.text = _(unicode(self.instance.name))
-		self.tooltip = u"Boat Builder \\n Overview" 
-		self.widget.findChild(name='BB_builtship_label').stylize("headline")
+		self.tooltip = u"Boat Builder \\n Overview"
 
 	def refresh(self):
 		"""This function is called by the TabWidget to redraw the widget."""
-		if hasattr(self.instance, 'running_costs') and \
-		   self.widget.child_finder('running_costs'):
-			self.widget.child_finder('running_costs').text = \
-			    unicode( self.instance.running_costs )
+		super(BoatbuilderTab, self).refresh()
 		progress = self.instance.get_production_progress()
 		self.widget.findChild(name='progress').progress = progress*100
-		self.widget.findChild(name='BB_progress_perc').text = unicode(math.floor(progress*100))+"%"
+		self.widget.findChild(name='BB_progress_perc').text = unicode(math.floor(progress*100))+u"%"
+
+		main_container = self.widget.findChild(name="BB_main_tab")
+		container_active = self.widget.findChild(name="container_active")
+		container_inactive = self.widget.findChild(name="container_inactive")
+		if self.instance.is_active() or True: # debug cond
+			# TODO: fill in acctual values here
+			main_container.removeChild( container_inactive )
+			container_active.findChild(name="BB_builtship_label").text = u"Loveboat"
+			container_active.findChild(name="BB_cur_ship_icon").tooltip = "$NAME $CLASS $COST"
+
+			if True: # if production is paused
+				container_active.removeChild( \
+					container_active.findChild(name="toggle_active_active") \
+				)
+				# TODO: make this button do sth
+			else:
+				container_active.removeChild( \
+					container_active.findChild(name="toggle_active_inactive") \
+				)
+				# TODO: make this button do sth
+
+			upgrades_box = container_active.findChild(name="BB_upgrades_box")
+			upgrades_box.addChild( pychan.widgets.Label(text=u"+ love") )
+			upgrades_box.addChild( pychan.widgets.Label(text=u"+ affection") )
+			upgrades_box.stylize('menu_black')
+
+			container_active.findChild(name='BB_builtship_label').stylize("headline")
+
+			still_needed_res = {  3: 5, 4 : 42 }
+			i = 1
+			needed_res_container = self.widget.findChild(name="BB_needed_resources_container")
+			main_container.findChild(name="BB_needed_res_label").text = _(u'Resources still needed:')
+			for res, amount in still_needed_res.iteritems():
+				assert i <= 3, "Only 3 still needed res for ships are currently supported"
+
+				icon = self.instance.session.db("SELECT icon_small FROM resource WHERE id = ?", res)[0][0]
+				needed_res_container.findChild(name="BB_needed_res_icon_"+str(i)).image = icon
+				needed_res_container.findChild(name="BB_needed_res_lbl_"+str(i)).text = unicode(amount)+u't'
+				i += 1
+
+			# TODO: cancel building button
+
+		else: # display sth when nothing is produced
+			main_container.removeChild( container_active )
+			main_container.findChild(name="BB_needed_res_label").text = u''
+
 		self.widget.adaptLayout()
 
 # this tab additionally requests functions for:
 # * decide: show [start view] = nothing but info text, look up the xml, or [building status view]
 # * get: currently built ship: name / image / upgrades
-# * resources still needed: 
+# * resources still needed:
 #	(a) which ones? three most important (image, name)
 #	(b) how many? sort by amount, display (amount, overall amount needed of them, image)
 # * pause production (keep order and "running" running costs [...] but collect no new resources)
@@ -110,7 +150,7 @@ class BoatbuilderConfirmTab(OverviewTab):
 		)
 		events = { 'create_unit': self.start_production }
 		self.widget.mapEvents(events)
-		self.tooltip = u"Confirm order" 
+		self.tooltip = u"Confirm order"
 		self.widget.findChild(name='BB_builtship_label').stylize("headline")
 		self.widget.findChild(name='headline_upgrades').stylize("headline")
 
@@ -119,10 +159,10 @@ class BoatbuilderConfirmTab(OverviewTab):
 
 # this "tab" additionally requests functions for:
 # * get: currently ordered ship: name / image / type (fisher/trade/war)
-# * => get: currently ordered ship: description text / costs / available upgrades 
+# * => get: currently ordered ship: description text / costs / available upgrades
 #						(fisher/trade/war, builder level)
 # * if resource icons not hardcoded: resource icons, sort them by amount
-# UPGRADES: * checkboxes * check for boat builder level (+ research) * add. costs (get, add, display) 
+# UPGRADES: * checkboxes * check for boat builder level (+ research) * add. costs (get, add, display)
 # * def start_production(self):  <<< actually start to produce the selected ship unit with the selected upgrades
 #	(use inventory or go collect resources, switch focus to overview tab).
 #	IMPORTANT: lock this button until unit is actually produced (no queue!)
