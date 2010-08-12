511077ec434d0f0cee2ba9fd5c96aebcb0899b03
nihathrael <nihathrael@d3468d7a-13f2-4ea4-bf00-366cb6352a40>
2010-08-12 15:08:03 +0000

    Fixed a bug in rect.get_radius_coordinates() if include_self=False
    git-svn-id: http://svn.unknown-horizons.org/trunk@3637 d3468d7a-13f2-4ea4-bf00-366cb6352a40

--- horizons/util/shapes/rect.py
+++ horizons/util/shapes/rect.py
@@ -218,9 +218,8 @@ class Rect(object):
 						if t not in self_coords:
 							yield t
 				else: # coords of this rect cannot appear here
-					for y, x_range in borders.iteritems():
-						for x in xrange(x_range[0], x_range[1]+1):
-							yield (x, y)
+					for x in xrange(x_range[0], x_range[1]+1):
+						yield (x, y)
 		else:
 			for y, x_range in borders.iteritems():
 				for x in xrange(x_range[0], x_range[1]+1):
