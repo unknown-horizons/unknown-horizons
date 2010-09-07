3016973c4706ccad2f9e67cc2f12e0f42d21f7b6
Simon Ruderich <simon@ruderich.org>
2010-09-07 18:49:28 +0200

    Update create_pot.sh to use xgettext and also msgmerge the po files.
    Remove po/POTFILES.in and po/POTFILES.in.in which are no longer
    necessary.

--- development/create_pot.sh
+++ development/create_pot.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
 
-pygettext -d unknownhorizons -p po .
+# Create po/unknownhorizons.pot and if -u is given the .po files are merged
+# with the new .pot file.
+
+
+python development/extract_strings_from_sqlite.py > po/sqlite_strings.pot
+
+# Get all files to translate.
+(
+    for python in *.py; do echo $python; done
+    find editor   -name \*.py
+    find horizons -name \*.py
+    echo po/sqlite_strings.pot
+) | xgettext --files-from=- --output-dir=po --output=unknownhorizons.pot \
+             --from-code=UTF-8 --add-comments --no-wrap --sort-by-file
+
+rm po/sqlite_strings.pot
+
+if [ x$1 != x-u ]; then
+    exit
+fi
+
+cd po && for file in *.po; do
+    echo $file
+    msgmerge -U $file unknownhorizons.pot
+    msgfmt --statistics -o /dev/null $file
+done
