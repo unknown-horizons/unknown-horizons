e25bd7257b2457a81e92d4e63d76c6a2807cad8c
eoc <eoc.eoc.eoc@gmail.com>
2010-11-07 18:32:57 +0100

    * Continued xml moving with the old folders content/gui/* for * in (build_menu,buildings_gui,tab_widget). * Deleted one file that did not get used. * Adapted python files to new names of buildmenu tabs and place_building.xml.

--- horizons/gui/mousetools/buildingtool.py
+++ horizons/gui/mousetools/buildingtool.py
@@ -106,7 +106,7 @@ class BuildingTool(NavigationTool):
 
 	def load_gui(self):
 		if self.gui is None:
-			self.gui = load_xml_translated("hud_builddetail.xml")
+			self.gui = load_xml_translated("place_building.xml")
 			self.gui.stylize('menu_black')
 			self.gui.findChild(name='headline').stylize('headline')
 			self.gui.findChild(name='building_name').stylize('headline')
