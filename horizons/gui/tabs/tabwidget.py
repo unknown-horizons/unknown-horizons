dc6d839d08729260365796c42248f628a64e4920
eoc <eoc.eoc.eoc@gmail.com>
2010-11-26 01:03:08 +0100

    Patch by hassanibraheem, thanks for contributing!
    * Disabling focus for widgets that shouldn't be focusable.
    
    Closes #584.
    Related ticket at fife trac: 507

--- horizons/gui/tabs/tabwidget.py
+++ horizons/gui/tabs/tabwidget.py
@@ -72,6 +72,7 @@ class TabWidget(object):
 				button.up_image = tab.button_up_image
 			button.down_image = tab.button_down_image
 			button.hover_image = tab.button_hover_image
+			button.is_focusable = False
 			button.size = (50, 50)
 			button.capture(pychan.tools.callbackWithArguments(self._show_tab, index))
 			if hasattr(tab, 'tooltip') and tab.tooltip is not None:
