17a6f9684458c0e2bd838b9ef0884c7f9d6025f7
totycro <totycro@unknown-horizons.org>
2011-03-18 23:39:52 +0100

    Fixed merge error, that was autointroduced by git

--- horizons/world/building/production.py
+++ horizons/world/building/production.py
@@ -58,10 +58,6 @@ class Distillery(SelectableBuilding, CollectingProducerBuilding, BuildableSingle
 class Hunter(SelectableBuilding, CollectingProducerBuilding, BuildableSingle, BasicBuilding):
 	pass
 
-class Fisher(SelectableBuilding, CollectingProducerBuilding, BuildableSingleOnCoast, BasicBuilding):
-	#range_applies_only_on_island = False
-	pass
-
 class IronRefiner(SelectableBuilding, CollectingProducerBuilding, BuildableSingle, BasicBuilding):
 	pass
 
