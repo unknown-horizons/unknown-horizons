f7d14608b2033cd1999912c83cf01e5ce1fdf9fd
Siddharth <sids.aquarius@gmail.com>
2011-03-20 23:03:19 +0530

    Getting rid of the KeyError by using 'discard' instead of 'remove' on _buildable_tiles (set)

--- horizons/gui/mousetools/buildingtool.py
+++ horizons/gui/mousetools/buildingtool.py
@@ -1,4 +1,4 @@
-# ###################################################
+/# ###################################################
 # Copyright (C) 2011 The Unknown Horizons Team
 # team@unknown-horizons.org
 # This file is part of Unknown Horizons.
@@ -349,7 +349,7 @@ class BuildingTool(NavigationTool):
 				island = self.session.world.get_island(building.position.origin)
 				for position in building.position:
 					tile = island.get_tile(position)
-					self._buildable_tiles.remove(tile)
+					self._buildable_tiles.discard(tile)
 					self.renderer.removeColored(tile._instance)
 				built = True
 				self._remove_listeners() # Remove changelisteners for update_preview
