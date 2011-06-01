5092b9f51e3cec7da825de1652b1f87a2ae1eae6
totycro <totycro@unknown-horizons.org>
2011-06-02 00:18:09 +0200

    Make tearing tool stick if shift is pressed Fixes #909

--- horizons/gui/mousetools/tearingtool.py
+++ horizons/gui/mousetools/tearingtool.py
@@ -76,8 +76,10 @@ class TearingTool(NavigationTool):
 			self._mark(self.coords, (int(round(coords.x)), int(round(coords.y))))
 			for i in self.selected:
 				Tear(i).execute(self.session)
-			self.tear_tool_active = False
-			self.session.cursor = SelectionTool(self.session)
+
+			if not evt.isShiftPressed():
+				self.tear_tool_active = False
+				self.session.cursor = SelectionTool(self.session)
 			evt.consume()
 
 	def mousePressed(self,  evt):
