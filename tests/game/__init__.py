5d55d55d8110ffe8bc80d8bdaf06e4c08a083c8c
squiddy <mr.squidd@gmail.com>
2011-06-16 16:15:28 +0200

    Add infrastructure to run tests of the game itself and add a small example.
    Compared to unit tests, these tests involve far more components, you may call
    these system tests, or big integration tests (they don't include the GUI
    or rendering engine).

---
+++ tests/game/__init__.py
@@ -0,0 +1,189 @@
+# ###################################################
+# Copyright (C) 2011 The Unknown Horizons Team
+# team@unknown-horizons.org
+# This file is part of Unknown Horizons.
+#
+# Unknown Horizons is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+
+import os
+import shutil
+import tempfile
+
+import horizons.main
+import horizons.world	# needs to be imported before session
+from horizons.ai.trader import Trader
+from horizons.command.building import Build
+from horizons.command.unit import CreateUnit
+from horizons.constants import PATHS, GROUND, UNITS, BUILDINGS, GAME_SPEED
+from horizons.entities import Entities
+from horizons.ext.dummy import Dummy
+from horizons.extscheduler import ExtScheduler
+from horizons.scheduler import Scheduler
+from horizons.spsession import SPSession
+from horizons.util import (Color, DbReader, Rect, WorldObject, NamedObject, LivingObject,
+						   SavegameAccessor, Point)
+from horizons.world import World
+
+
+def setup_package():
+	"""
+	Setup read-only database. This might have to change in the future, tests should not
+	fail only because a production now takes 1 second more in the game.
+	"""
+	horizons.main.db = horizons.main._create_db()
+
+
+def create_map():
+	"""
+	Create a map with a square island (20x20) at position (20, 20) and return the path
+	to the database file.
+	"""
+
+	# Create island.
+	islandfile = tempfile.mkstemp()[1]
+
+	db = DbReader(islandfile)
+	db("CREATE TABLE ground(x INTEGER NOT NULL, y INTEGER NOT NULL, ground_id INTEGER NOT NULL)")
+	db("CREATE TABLE island_properties(name TEXT PRIMARY KEY NOT NULL, value TEXT NOT NULL)")
+
+	db("BEGIN TRANSACTION")
+	tiles = []
+	for x, y in Rect.init_from_topleft_and_size(0, 0, 20, 20).tuple_iter():
+		if (2 < x < 18) or (2 < y < 18):
+			ground = GROUND.DEFAULT_LAND
+		else:
+			# Add coastline at the borders.
+			ground = 49
+		tiles.append((x, y, ground))
+	db.execute_many("INSERT INTO ground VALUES(?, ?, ?)", tiles)
+	db("COMMIT")
+
+	# Create savegame with the island above.
+	savegame = tempfile.mkstemp()[1]
+	shutil.copyfile(PATHS.SAVEGAME_TEMPLATE, savegame)
+
+	db = DbReader(savegame)
+	db("BEGIN TRANSACTION")
+	db("INSERT INTO island (x, y, file) VALUES(?, ?, ?)", 20, 20, islandfile)
+	db("COMMIT")
+
+	return (savegame, islandfile)
+
+
+class SPTestSession(SPSession):
+
+	def __init__(self, db):
+		"""
+		Unfortunately, right now there is no other way to setup Dummy versions of the GUI,
+		View etc., unless we want to patch the references in the session module.
+		"""
+		super(LivingObject, self).__init__()
+		self.gui = Dummy()
+		self.db = db
+		self.savecounter = 0	# this is a new game.
+		self.is_alive = True
+
+		WorldObject.reset()
+		NamedObject.reset()
+
+		# Game
+		self.current_tick = 0
+		self.random = self.create_rng()
+		self.timer = self.create_timer()
+		Scheduler.create_instance(self.timer)
+		ExtScheduler.create_instance(Dummy)
+		self.manager = self.create_manager()
+		self.view = Dummy()
+		self.view.renderer = Dummy()
+		Entities.load(self.db)
+		self.scenario_eventhandler = Dummy()
+		self.campaign = {}
+
+		# GUI
+		self.gui.session = self
+		self.ingame_gui = Dummy()
+
+		GAME_SPEED.TICKS_PER_SECOND = 16
+
+	def load(self, db_files, players):
+		"""
+		Stripped version of the original code. We don't need to load selections,
+		or a scenario, setting up the gui or view.
+		"""
+		self.db_files = db_files	# keep the paths to the databases, so we can clean up
+		savegame_db = SavegameAccessor(db_files[0])
+
+		self.world = World(self)
+		self.world._init(savegame_db)
+		for i in sorted(players):
+			self.world.setup_player(i['id'], i['name'], i['color'], i['local'])
+		self.manager.load(savegame_db)
+
+	def end(self):
+		"""
+		Clean up temporary files.
+		"""
+		super(SPTestSession, self).end()
+		for f in self.db_files:
+			os.remove(f)
+
+	def run(self, ticks=1, seconds=None):
+		"""
+		Run the scheduler the given count of ticks or (in-game) seconds. Default is 1 tick,
+		if seconds are passed, they will overwrite the tick count.
+		"""
+		if seconds:
+			ticks = self.timer.get_ticks(seconds)
+
+		for i in range(ticks):
+			Scheduler().tick(self.current_tick)
+			self.current_tick += 1
+
+
+def new_session(mapgen=create_map):
+	"""
+	Create a new session with a map, add one human player and a trader (it will crash
+	otherwise). It returns both session and player to avoid making the function-baed
+	tests too verbose.
+	"""
+	session = SPTestSession(horizons.main.db)
+	players = [{'id': 1, 'name': 'foobar', 'color': Color[1], 'local': True}]
+
+	session.load(create_map(), players)
+	session.world.trader = Trader(session, 99999, 'Free Trader', Color())
+
+	return session, session.world.player
+
+
+def new_settlement(session, pos=Point(20, 20)):
+	"""
+	Creates a settlement at the given position. It returns the settlement and the island
+	where it was created on, to avoid making function-baed tests too verbose.
+	"""
+	island = session.world.get_island(pos)
+	assert island, "No island found at %s" % pos
+	player = session.world.player
+
+	ship = CreateUnit(player.worldid, UNITS.PLAYER_SHIP_CLASS, pos.x, pos.y)(player)
+	for res, amount in session.db("SELECT resource, amount FROM start_resources"):
+		ship.inventory.alter(res, amount)
+
+	building = Build(BUILDINGS.BRANCH_OFFICE_CLASS, pos.x, pos.y, island, ship=ship)(player)
+	assert building, "Could not build branch office at %s" % pos
+
+	return (player.settlements[0], island)
---
+++ tests/game/test_example.py
@@ -0,0 +1,76 @@
+# ###################################################
+# Copyright (C) 2011 The Unknown Horizons Team
+# team@unknown-horizons.org
+# This file is part of Unknown Horizons.
+#
+# Unknown Horizons is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+
+from horizons.command.building import Build
+from horizons.constants import RES
+
+from tests.game import new_session, new_settlement
+
+
+def test_example():
+	"""
+	Build a farm and 2 pastures. Confirm raw wool is produced at the
+	pastures and used by the farm to produce wool.
+	"""
+	PASTURE, FARM = 18, 20
+
+	s, p = new_session()
+	settlement, island = new_settlement(s)
+	settlement.inventory.alter(RES.GOLD_ID, 5000)
+	settlement.inventory.alter(4, 50)
+	settlement.inventory.alter(6, 50)
+
+	farm = Build(FARM, 30, 30, island, settlement=settlement)(p)
+	assert farm
+
+	# Pause the production, we want to start it explicitly later.
+	production = farm._get_production(7)
+	production.pause()
+
+	# Farm has no raw wool or wool.
+	assert farm.inventory[2] == 0
+	assert farm.inventory[10] == 0
+
+	# Build pastures, let the game run for 31 seconds. Pastures currently need
+	# 30s to produce wool.
+	p1 = Build(PASTURE, 27, 30, island, settlement=settlement)(p)
+	p2 = Build(PASTURE, 33, 30, island, settlement=settlement)(p)
+	assert p1 and p2
+
+	s.run(seconds=31)
+
+	assert p1.inventory[2]
+	assert p2.inventory[2]
+
+	# Give farm collectors a chance to get the wool from the pastures.
+	s.run(seconds=5)
+
+	assert farm.inventory[2]
+
+	# Resume the production, let the game run for a second. The farm should have
+	# produced wool now.
+	production.pause(pause=False)
+	s.run(seconds=1)
+	assert farm.inventory[10]
+
+	s.end()
+
