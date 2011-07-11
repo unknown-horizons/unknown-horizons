221db5333c88466d000a6936d4ea26f4c66b9010
totycro <totycro@unknown-horizons.org>
2011-07-11 21:01:16 +0200

    This fails on some systems such as debian, who have no python2 symlink (yet).
    Revert "Use python2 instead of the system default python, which sometimes is actually python3"
    
    This reverts commit a9e96a628a0304e01e57c90eb934ddb3dddf8f4e.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 # ###################################################
 # Copyright (C) 2011 The Unknown Horizons Team
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 from __future__ import with_statement
 from distutils.core import setup
