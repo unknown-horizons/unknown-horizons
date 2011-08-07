4ec2d3217c126ff4f4df1433ffcbaaf2a6c2da91
Mihai Bivol <mm.bivol@gmail.com>
2011-08-07 20:51:47 +0300

    made it easy to reproduce mouseDragged segfault, frigates die in 15 seconds after starting the game

--- horizons/gui/mousetools/selectiontool.py
+++ horizons/gui/mousetools/selectiontool.py
@@ -63,6 +63,9 @@ class SelectionTool(NavigationTool):
 									min(self.select_begin[1], evt.getY()), \
 									abs(evt.getX() - self.select_begin[0]), \
 									abs(evt.getY() - self.select_begin[1])) if do_multi else fife.ScreenPoint(evt.getX(), evt.getY()), self.session.view.layers[LAYERS.OBJECTS])
+			for i in instances:
+				print 'try to get id for', i, 'if no id on the next line then getId() crashed'
+				print i.getId()
 			# Only one unit, select anyway
 			if len(instances) == 1:
 				try:
