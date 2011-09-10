7edb22d51dc73973f01954fbe333dc3bab007ede
totycro <totycro@unknown-horizons.org>
2011-09-10 18:05:03 +0200

    Added discontent over taxes

--- horizons/world/building/settler.py
+++ horizons/world/building/settler.py
@@ -196,6 +196,11 @@ class Settler(SelectableBuilding, BuildableSingle, CollectingProducerBuilding, B
 		difference = 1.0 - self.settlement.tax_settings[self.level]
 		happiness_decrease = 45 * difference - 15 * abs(difference)
 		happiness_decrease = int(round(happiness_decrease))
+		# NOTE: this forumla was actually designed for a different use case, where the happiness
+		# is calculated from the number of available goods -/+ a certain tax factor.
+		# to simulate the more dynamic, currently implemented approach (where every event changes
+		# the happiness), we simulate discontent of taxes by this:
+		happiness_decrease -= 10
 		self.inventory.alter(RES.HAPPINESS_ID, happiness_decrease)
 
 		self._changed()
