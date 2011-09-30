d0b663b29d50fbfd644d929d611f3431d9b2bfd8
totycro <totycro@unknown-horizons.org>
2011-09-30 19:27:59 +0200

    Fixed bug reported by jiri that caused buildings to pay full production costs after loading when they are inactive. Thanks for the hint, jiri!

--- horizons/world/building/building.py
+++ horizons/world/building/building.py
@@ -110,6 +110,7 @@ class BasicBuilding(AmbientSound, ConcretObject):
 				self.running_costs_inactive, self.running_costs
 
 	def running_costs_active(self):
+		"""Returns whether the building currently payes the running costs for status 'active'"""
 		return (self.running_costs > self.running_costs_inactive)
 
 	def get_payout(self):
