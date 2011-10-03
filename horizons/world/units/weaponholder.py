9c7c7585c6c4c262ff55651f3468e2c399d18ddf
nihathrael <thomas.kinnen@gmail.com>
2011-10-03 16:11:58 +0200

    Fix error in the AttackCommand
    The AttackCommand attempted to pickle the targets instance itself instead of
    only pickleing the target's world id

--- horizons/world/units/weaponholder.py
+++ horizons/world/units/weaponholder.py
@@ -31,6 +31,7 @@ from horizons.world.component.stancecomponent import HoldGroundStance, Aggressiv
 	NoneStance, FleeStance
 from horizons.world.storage import PositiveTotalNumSlotsStorage
 from horizons.world.units.ship import Ship
+from horizons.util.worldobject import WorldObject
 
 import gc
 
@@ -282,11 +283,12 @@ class WeaponHolder(object):
 
 		self.try_attack_target()
 
-	def user_attack(self, target):
+	def user_attack(self, targetid):
 		"""
 		Called when the user triggeres the attack, executes the user_attack_issued callbacks
+		@param targetid: world id of the unit that is to be attacked
 		"""
-		self.attack(target)
+		self.attack(WorldObject.get_object_by_id(targetid))
 		self.on_user_attack_issued()
 
 	def is_attacking(self):
