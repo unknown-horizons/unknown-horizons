fa32773ef2ad1b28df364d264765c7120ded6371
karottenreibe <git@rottenrei.be>
2011-11-03 14:19:32 +0100

    spaces --> tabs

--- editor/editor.py
+++ editor/editor.py
@@ -25,34 +25,34 @@ import os
 import sys
 
 class DictObj:
-    def __init__(self, d):
-        self.d = d
+	def __init__(self, d):
+		self.d = d
 
-    def __getattr__(self, m):
-        return self.d.get(m)
+	def __getattr__(self, m):
+		return self.d.get(m)
 
 if __name__ == '__main__':
-    import gettext
-    gettext.install('', unicode=True) # necessary for init_environment
+	import gettext
+	gettext.install('', unicode=True) # necessary for init_environment
 
-    # prepare sys path and init UH
-    def up(path):
-        return os.path.split(path)[0]
-    uh_path = up(up(os.path.realpath(sys.argv[0])))
-    sys.path.append(uh_path)
-    from run_uh import init_environment, get_fife_path
-    init_environment()
+	# prepare sys path and init UH
+	def up(path):
+		return os.path.split(path)[0]
+	uh_path = up(up(os.path.realpath(sys.argv[0])))
+	sys.path.append(uh_path)
+	from run_uh import init_environment, get_fife_path
+	init_environment()
 
-    editor_path = get_fife_path() + '/tools/editor'
-    os.chdir(editor_path)
-    sys.path.append(editor_path)
+	editor_path = get_fife_path() + '/tools/editor'
+	os.chdir(editor_path)
+	sys.path.append(editor_path)
 
-    # Start editor
-    options = {'plugin_dir': uh_path + '/editor/plugins'}
-    options = DictObj(options)
-    mapfile = None
-    if len(sys.argv) > 1:
-        mapfile = sys.argv[1]
-    from scripts.editor import Editor
-    app = Editor(options, mapfile)
-    app.run()
+	# Start editor
+	options = {'plugin_dir': uh_path + '/editor/plugins'}
+	options = DictObj(options)
+	mapfile = None
+	if len(sys.argv) > 1:
+		mapfile = sys.argv[1]
+	from scripts.editor import Editor
+	app = Editor(options, mapfile)
+	app.run()
--- editor/plugins/UHMapLoader.py
+++ editor/plugins/UHMapLoader.py
@@ -27,87 +27,87 @@ import scripts.plugin
 
 class MapLoader:
 
-    GRID_TYPE = "square"
-    GROUND_LAYER_NAME = "ground"
+	GRID_TYPE = "square"
+	GROUND_LAYER_NAME = "ground"
 
-    def __init__(self, engine, callback, debug, extensions):
-        """ Initialize the map loader """
-        self._engine = engine
-        self._callback = callback
-        self._debug = debug
+	def __init__(self, engine, callback, debug, extensions):
+		""" Initialize the map loader """
+		self._engine = engine
+		self._callback = callback
+		self._debug = debug
 
-    def loadResource(self, path):
-        """ Loads the map from the given sqlite file """
-        model = self._engine.getModel()
-        map = model.createMap("name") # TODO: get the model name from the db
-        grid = map.getCellGrid(GRID_TYPE)
+	def loadResource(self, path):
+		""" Loads the map from the given sqlite file """
+		model = self._engine.getModel()
+		map = model.createMap("name") # TODO: get the model name from the db
+		grid = map.getCellGrid(GRID_TYPE)
 
-        # add layers
-        ground_layer = map.createLayer(GROUND_LAYER_NAME, grid)
+		# add layers
+		ground_layer = map.createLayer(GROUND_LAYER_NAME, grid)
 
-        map_db = DBReader(path)
-        # TODO: check the map version number
+		map_db = DBReader(path)
+		# TODO: check the map version number
 
-        # load objects catalogue
-        self._loadObjects(map_db, model)
+		# load objects catalogue
+		self._loadObjects(map_db, model)
 
-        # load all islands
-        islands = map_db("SELECT x, y, file FROM islands")
-        for island in islands:
-            self._loadIsland(ground_layer, *island)
+		# load all islands
+		islands = map_db("SELECT x, y, file FROM islands")
+		for island in islands:
+			self._loadIsland(ground_layer, *island)
 
-    def _loadObjects(self, map_db, model):
-        pass
+	def _loadObjects(self, map_db, model):
+		pass
 
-    def _loadIsland(self, ground_layer, x, y, file):
-        """ Loads an island from the given file """
-        island_db = DBReader(file)
+	def _loadIsland(self, ground_layer, x, y, file):
+		""" Loads an island from the given file """
+		island_db = DBReader(file)
 
-        # load ground tiles
-        ground = island_db("SELECT x, y FROM ground")
-        for (x, y) in ground:
-            pass # TODO: place ground tile
+		# load ground tiles
+		ground = island_db("SELECT x, y FROM ground")
+		for (x, y) in ground:
+			pass # TODO: place ground tile
 
 class UHMapLoader(scripts.plugin.Plugin):
-    """ The B{UHMapLoader} allows to load the UH map format in FIFEdit
-    """
+	""" The B{UHMapLoader} allows to load the UH map format in FIFEdit
+	"""
 
-    def __init__(self):
-        # Editor instance
-        self._editor = None
+	def __init__(self):
+		# Editor instance
+		self._editor = None
 
-        # Plugin variables
-        self._enabled = False
+		# Plugin variables
+		self._enabled = False
 
-        # Current mapview
-        self._mapview = None
+		# Current mapview
+		self._mapview = None
 
 
-    #--- Plugin functions ---#
-    def enable(self):
-        """ Enable plugin """
-        if self._enabled is True:
-            return
+	#--- Plugin functions ---#
+	def enable(self):
+		""" Enable plugin """
+		if self._enabled is True:
+			return
 
-        # Fifedit plugin data
-        self._editor = scripts.editor.getEditor()
+		# Fifedit plugin data
+		self._editor = scripts.editor.getEditor()
 
-        mapLoaders.addMapLoader('sqlite', MapLoader)
+		mapLoaders.addMapLoader('sqlite', MapLoader)
 
-    def disable(self):
-        """ Disable plugin """
-        if self._enabled is False:
-            return
+	def disable(self):
+		""" Disable plugin """
+		if self._enabled is False:
+			return
 
-    def isEnabled(self):
-        """ Returns True if plugin is enabled """
-        return self._enabled;
+	def isEnabled(self):
+		""" Returns True if plugin is enabled """
+		return self._enabled;
 
-    def getName(self):
-        print("name")
-        """ Return plugin name """
-        return u"UHMapLoader"
+	def getName(self):
+		print("name")
+		""" Return plugin name """
+		return u"UHMapLoader"
 
-    #--- End plugin functions ---#
+	#--- End plugin functions ---#
 
 
