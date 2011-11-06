911adaabcfc596a05b73dbb33c17a7ae65223cbf
Christopher Oelmueller <eoc.eoc.eoc@gmail.com>
2011-11-06 22:37:25 +0100

    * Minor fixes to dev scripts for copying and compiling translations

--- development/copy_pofiles.sh
+++ development/copy_pofiles.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+###############################################################################
 # Copies all files (not en*.po as they are used for string change suggestions)
 # from our translation repo into po/
 # Requires a checkout of the UH translation repo with the following structure:
@@ -9,17 +10,34 @@
 
 TL_SOURCE='../translations'  #origin (TL repo pootle export)
 TL_DIR='po/'  #destination
-TL_REGEX='s,:,,g;s,.po,  ,g;s,alencia,,g;s,(../translations/uh/|messages|translations),\t,g;s/[.,]//g'
-TL_TUT_REGEX='s,:,,g;s,.po,,g;s,alencia,,g;s,(../translations/uh-tutorial/|messages|translations),\t,g;s/[.,]//g'
+TL_REGEX='s,:,,g;s,.po,  ,g;s,alencia,,g;s,(../translations/uh/|messages|message|translations),\t,g;s/[.,]//g'
+TL_TUT_REGEX='s,:,,g;s,.po,,g;s,alencia,,g;s,(../translations/uh-tutorial/|messages|message|translations),\t,g;s/[.,]//g'
 TL_TUT_TEMPDIR='po/po_temp_tutorial/'
-TUT_NOT_SHIPPING=(en-suggest)
+TL_NOT_SHIPPING=(en-suggest) # languages that will never end up in UH (helpers)
 
-function build_tl_tut_ungrep {
+# remove languages from INCOMPLETE if you see that their stats are similar or
+# better numbers than included languages.  statistics are always printed,
+# regardless of whether the language was considered INCOMPLETE or not.
+# languages that would show up if more strings are translated:
+TL_INCOMPLETE=( da )
+TL_TUT_INCOMPLETE=( bg  ca@valencia  da  et  hu  lt  nb  sl)
+###############################################################################
+
+function build_ungrep {
 	c='grep -v -e en.po'
-	for i in "${TUT_NOT_SHIPPING[@]}"; do
+	for i in "${TL_NOT_SHIPPING[@]}"; do
 		c+=" -e $i.po "
 	done
 	TL_TUT_UNGREP="$c"
+
+	d='grep -v -e en.po'
+	for i in "${TL_INCOMPLETE[@]}"; do
+		d+=" -e $i.po "
+	done
+	for i in "${TL_NOT_SHIPPING[@]}"; do
+		d+=" -e $i.po "
+	done
+	TL_UNGREP="$d"
 }
 
 function tl_check {
@@ -35,8 +53,17 @@ function tl_tut_check {
 
 
 
+build_ungrep
 
-for file in $(ls $TL_SOURCE/uh/*.po |grep -v -e en.po -e en-suggest.po); do
+echo '=> Removing these interface translations:'
+for file in ${TL_INCOMPLETE[@]}; do
+	tl_check "$TL_SOURCE/uh/$file.po" | perl -npe "$TL_REGEX"
+done
+
+echo
+echo '=> Copying these interface translations:'
+
+for file in $(ls $TL_SOURCE/uh/*.po | $TL_UNGREP); do
 	tl_check $file
 	if [ $? -ne 3 ]; then
 		echo '  !!This file seems to be broken. Skipping.'
@@ -45,33 +72,36 @@ for file in $(ls $TL_SOURCE/uh/*.po |grep -v -e en.po -e en-suggest.po); do
 		cp $file $TL_DIR
 	fi
 done;
-echo 'Main translation files copied.'
-echo '=============================='
+
 echo
+echo '=> Interface translation files copied.'
+echo '   To compile them, run   setup.py build_i18n'
+echo
+echo
+echo '=> Checking tutorial translations:'
 
 if [ ! -d $TL_TUT_TEMPDIR ] ; then
 	mkdir $TL_TUT_TEMPDIR
 fi
-
-build_tl_tut_ungrep
 
 rm -rf "$TL_TUT_TEMPDIR/en-suggest.po"
 for tutfile in $(ls $TL_SOURCE/uh-tutorial/*.po |$TL_TUT_UNGREP); do
 	#tl_tut_check $tutfile
 	#if [ $? -ne 3 ]; then
 	#	echo '  !!This file has not enough content or is broken. Skipping.'
-		#which means that less than 40% of the strings are completed (unfuzzy)
+		#which means that less than 33% of the strings are completed (unfuzzy)
 	#else
 		rm -rf "$TL_TUT_TEMPDIR/$tutfile"
 		cp $tutfile $TL_TUT_TEMPDIR
 	#fi
 done;
 
-INCOMPLETE=( bg  ca@valencia  da  et  hu  lt  nb  sl)
-echo 'Removing these tutorial translations:'
-for file in ${INCOMPLETE[@]}; do
-	tl_tut_check "$TL_TUT_TEMPDIR/$file.po" | perl -npe 's,(po_temp_tutorial//|messages|translations),\t,g'
+echo '=> Removing these tutorial translations:'
+for file in ${TL_TUT_INCOMPLETE[@]}; do
+	tl_tut_check "$TL_TUT_TEMPDIR/$file.po" | perl -npe 's,(po_temp_tutorial//|messages|message|translations),\t,g'
 	rm "$TL_TUT_TEMPDIR/$file.po"
 done
 
 sh ./development/create_scenario_pot.sh tutorial $TL_TUT_TEMPDIR
+echo
+echo '=> Success.'
