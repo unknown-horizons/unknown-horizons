9332ac844b9e738068a80aad8ef4cb66abcad9de
totycro <totycro@unknown-horizons.org>
2011-11-09 22:34:43 +0100

    Respect ship construction time. At least != at most.

--- horizons/world/production/unitproduction.py
+++ horizons/world/production/unitproduction.py
@@ -108,7 +108,7 @@ class UnitProduction(ChangingProduction):
 		all_needed_res = sum( i[1] for i in self.original_prod_line.consumed_res.iteritems() if i[0] != RES.GOLD_ID )
 		part_of_whole_production = float(removed_res_without_gold) / all_needed_res
 		prod_time = Scheduler().get_ticks( part_of_whole_production * self._prod_line.time )
-		prod_time = min(prod_time, 1) # wait at least 1 tick
+		prod_time = max(prod_time, 1) # wait at least 1 tick
 		# do part of production and call this again when done
 		Scheduler().add_new_object(self._produce, self, prod_time)
 
