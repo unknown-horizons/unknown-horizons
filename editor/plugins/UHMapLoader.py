ed5f03a5dd3fa5e1eecab4d32a76cf8268bc1def
karottenreibe <git@rottenrei.be>
2011-11-14 16:02:35 +0100

    split plugins in two (object and map loader)

--- editor/plugins/UHMapLoader.py
+++ editor/plugins/UHMapLoader.py
@@ -126,11 +126,6 @@ class UHMapLoader(scripts.plugin.Plugin):
 		self._editor = scripts.editor.getEditor()
 		self._engine = self._editor.getEngine()
 
-		# load UH objects
-		self._fixupFife()
-		self._fixupHorizons()
-		self._loadObjects()
-
 		mapLoaders.addMapLoader('sqlite', MapLoader)
 		exts = list(mapLoaders.fileExtensions)
 		exts.append('sqlite')
@@ -150,83 +145,4 @@ class UHMapLoader(scripts.plugin.Plugin):
 		return u"UHMapLoader"
 
 	#--- End plugin functions ---#
-
-	def _fixupHorizons(self):
-		"""Fixes some UH quirks that have to do with globals"""
-		class PatchedFife:
-			imagemanager = self._engine.getImageManager()
-			use_atlases = False
-			pass
-		horizons.main.fife = PatchedFife()
-		uh_path = getUHPath()
-		horizons.main.PATHS.TILE_SETS_DIRECTORY = os.path.join(uh_path, horizons.main.PATHS.TILE_SETS_DIRECTORY)
-		horizons.main.PATHS.ACTION_SETS_DIRECTORY = os.path.join(uh_path, horizons.main.PATHS.ACTION_SETS_DIRECTORY)
-		horizons.main.PATHS.DB_FILES = map(lambda file: os.path.join(uh_path, file), horizons.main.PATHS.DB_FILES)
-
-	def _fixupFife(self):
-		"""Fixes some FIFE quirks that have to do with VFS"""
-		vfs = self._engine.getVFS()
-		vfs.addNewSource(getUHPath())
-		vfs.addNewSource("/")
-
-	def _loadObjects(self):
-		# get fifedit objects
-		model = self._engine.getModel()
-
-		# get UH db and loaders
-		db = horizons.main._create_db()
-		TileSetLoader.load()
-		ActionSetLoader.load()
-		animationloader = SQLiteAnimationLoader()
-
-		# load all ground tiles
-		print("loading UH ground tiles...")
-		tile_sets = TileSetLoader.get_sets()
-
-		for tile_set_id in tile_sets:
-			tile_set = tile_sets[tile_set_id]
-			object = model.createObject(str(tile_set_id), 'ground')
-			fife.ObjectVisual.create(object)
-
-			# load animations
-			for action_id in tile_sets[tile_set_id].iterkeys():
-				action = object.createAction(action_id+"_"+str(tile_set_id))
-				fife.ActionVisual.create(action)
-				for rotation in tile_sets[tile_set_id][action_id].iterkeys():
-					anim = animationloader.loadResource( \
-						str(tile_set_id)+"+"+str(action_id)+"+"+ \
-						str(rotation) + ':shift:center+0,bottom+8')
-					action.get2dGfxVisual().addAnimation(int(rotation), anim)
-					action.setDuration(anim.getDuration())
-
-		# load all buildings
-		print("loading UH buildings...")
-		all_action_sets = ActionSetLoader.get_sets()
-		for (building_id, building_name) in db("SELECT id, name FROM building"):
-			building_action_sets = db("SELECT action_set_id FROM action_set WHERE object_id=?", building_id)
-			size_x, size_y = db("SELECT size_x, size_x FROM building WHERE id = ?", building_id)[0]
-			object = model.createObject(str(building_name), 'building')
-			fife.ObjectVisual.create(object)
-
-			for (action_set_id,) in building_action_sets:
-				for action_id in all_action_sets[action_set_id].iterkeys():
-					action = object.createAction(action_id+"_"+str(action_set_id))
-					fife.ActionVisual.create(action)
-					for rotation in all_action_sets[action_set_id][action_id].iterkeys():
-						if rotation == 45:
-							command = 'left-32,bottom+' + str(size_x * 16)
-						elif rotation == 135:
-							command = 'left-' + str(size_y * 32) + ',bottom+16'
-						elif rotation == 225:
-							command = 'left-' + str((size_x + size_y - 1) * 32) + ',bottom+' + str(size_y * 16)
-						elif rotation == 315:
-							command = 'left-' + str(size_x * 32) + ',bottom+' + str((size_x + size_y - 1) * 16)
-						else:
-							assert False, "Bad rotation for action_set %(id)s: %(rotation)s for action: %(action_id)s" % \
-								   { 'id': action_set_id, 'rotation': rotation, 'action_id': action_id }
-						anim = animationloader.loadResource(str(action_set_id)+"+"+str(action_id)+"+"+str(rotation) + ':shift:' + command)
-						action.get2dGfxVisual().addAnimation(int(rotation), anim)
-						action.setDuration(anim.getDuration())
-
-		print("finished loading UH objects")
 
