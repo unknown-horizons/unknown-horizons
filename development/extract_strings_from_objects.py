528d1f313b92a033c017b3cacd1511cfb6bf54f1
Christopher Oelmueller <eoc.eoc.eoc@gmail.com>
2011-12-23 17:36:28 +0100

    * Script to extract translatable strings
    Looks for definitions with the following structure:
    key: _ value
    and writes _(value) to a file similar to what we use
    for translating text originating from xml widgets.
    
    Right now, the output is empty because no object de-
    finitions have been converted to use _ syntax yet.

---
+++ development/extract_strings_from_objects.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python2
+# ###################################################
+# Copyright (C) 2011 The Unknown Horizons Team
+# team@unknown-horizons.org
+# This file is part of Unknown Horizons.
+#
+# Unknown Horizons is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+
+HEADER = '''# ###################################################
+# Copyright (C) 2011 The Unknown Horizons Team
+# team@unknown-horizons.org
+# This file is part of Unknown Horizons.
+#
+# Unknown Horizons is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+# ###################################################################
+# WARNING: This file is generated automagically.
+#          You need to update it to see changes to strings in-game.
+#          DO NOT MANUALLY UPDATE THIS FILE (by editing strings).
+#          The script to generate .pot templates calls the following:
+# ./development/extract_strings_from_objects.py  horizons/i18n/objecttranslations.py
+#          If you changed strings in code, you might just run this
+#          command as well.
+# NOTE: In string-freeze mode (shortly before releases, usually
+#       announced in a meeting), updates to this file must not happen
+#       without permission of the responsible translation admin!
+# ###################################################################
+
+from horizons.constants import VERSION
+
+object_translations = dict()
+
+def set_translations():
+\tglobal object_translations
+\tobject_translations = {
+'''
+
+FOOTER = '''\n\t}\n'''
+ROWINDENT = '\n\t\t'
+
+locations_to_translate = {
+	'content/objects/buildings/',
+	'content/objects/units/ships/',
+	}
+
+files_to_skip = {
+	'usablefisher.yaml',
+	}
+
+import os
+import sys
+
+from yaml import load
+from yaml import SafeLoader as Loader
+
+def list_all_files():
+	result = []
+	for folder in locations_to_translate:
+		for directory, subdirs, files in os.walk(folder):
+			for filename in files:
+				if filename.endswith('.yaml') and filename not in files_to_skip:
+					result.append('{directory}/{filename}'.format(
+					                directory=directory, filename=filename))
+	return sorted(result)
+
+def content_from_file(filename):
+	#print '     = {name}'.format(name=filename)
+	parsed = load(file(filename, 'r'), Loader=Loader)
+	object_strings = []
+	for component, value in parsed.iteritems():
+		if isinstance(value, str) or isinstance(value, unicode):
+			if value.startswith('_ '):
+				text = '_("{value}")'.format(value=value[2:])
+				object_strings.append('%-30s: %s' % (('"%s"') % component, text))
+
+	strings = sorted(object_strings)
+
+	if strings:
+		return ('\n\t"%s" : {' % filename) + \
+		       (ROWINDENT + '%s,' % (','+ROWINDENT).join(strings)) + ROWINDENT + '},'
+	else:
+		return ''
+
+filesnippets = (content_from_file(filename) for filename in list_all_files())
+filesnippets = (content for content in filesnippets if content != '')
+
+output = '%s%s%s' % (HEADER, '\n'.join(filesnippets), FOOTER)
+
+if len(sys.argv) > 1:
+	file(sys.argv[1], 'w').write(output)
+else:
+	print
+	print output
