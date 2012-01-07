69481659f90cc732f047c3db6ed7128dd30e60a3
squiddy <mr.squidd@gmail.com>
2012-01-07 23:48:18 +0100

    Add gui test for #1224, inconsistent running costs for boat builder

---
+++ tests/gui/ingame/test_boatbuilder.py
@@ -0,0 +1,62 @@
+# ###################################################
+# Copyright (C) 2012 The Unknown Horizons Team
+# team@unknown-horizons.org
+# This file is part of Unknown Horizons.
+#
+# Unknown Horizons is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+from horizons.constants import BUILDINGS, PRODUCTION
+from horizons.world.production.producer import Producer
+from tests.gui import TestFinished, gui_test
+
+
+@gui_test(use_fixture='boatbuilder_1224')
+def test_ticket_1224(gui):
+	"""
+	Boat builder running costs are inconsistent.
+	"""
+	yield # test needs to be a generator for now
+
+	settlement = gui.session.world.player.settlements[0]
+	boatbuilder = settlement.get_buildings_by_id(BUILDINGS.BOATBUILDER_CLASS)[0]
+
+	gui.select([boatbuilder])
+
+	def running_costs():
+		c = gui.find(name='BB_main_tab')
+		return c.findChild(name='running_costs').text
+
+	# Check (inactive) running costs
+	assert running_costs() == '10', "Expected 10, got %s" % running_costs()
+
+	# Select trade ships tab
+	c = gui.find(name='tab_base')
+	gui.trigger(c, '1/action/default')
+
+	# Build huker
+	c = gui.find(name='boatbuilder_trade')
+	gui.trigger(c, 'BB_build_trade_1/action/default')
+
+	# Wait until production starts
+	producer = boatbuilder.get_component(Producer)
+	while producer._get_current_state() != PRODUCTION.STATES.producing:
+		yield
+
+	# Check (active) running costs
+	assert running_costs() == '25', "Expected 25, got %s" % running_costs()
+
+	raise TestFinished
