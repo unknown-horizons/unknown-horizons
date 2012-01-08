ede78e960544a4c906080435392a8e002c557fc8
Chris Oelmueller <chris.oelmueller+uh@gmail.com>
2012-01-08 05:18:33 +0100

    * Use clickable labels to select color instead of ListBox strings #1364
    This adds a new setting ColorID to the settings file. Partially fixes a
    bug caused by the different SP menu modes. Still observed when trying to
    enter a new player name and then clicking on a different SP game mode.
    The text in the player name input field is reset during this process.

--- horizons/gui/modules/playerdataselection.py
+++ horizons/gui/modules/playerdataselection.py
@@ -21,8 +21,9 @@
 
 
 import horizons.main
+from fife.extensions.pychan.widgets import HBox, Label
 
-from horizons.util import Color
+from horizons.util import Callback, Color
 
 
 class PlayerDataSelection(object):
@@ -38,12 +39,45 @@ class PlayerDataSelection(object):
 		widgets.reload( 'playerdataselection' )
 		self.gui = widgets[ 'playerdataselection' ]
 
-		self.gui.distributeInitialData({ 'playercolor' : [ _(color.name) for color in Color ] })
+		self.colors = self.gui.findChild(name = 'playercolor')
+		self.selected_color = horizons.main.fife.get_uh_setting("ColorID") # starts at 1!
+		self._set_color(self.selected_color)
+
+		colorlabels = []
+		events = {}
+
+		# need the id to save it as int in settings file.
+		for color_id, color in enumerate(Color):
+			label = Label(name = '{color}'.format(color=color.name),
+			              text = u"    ",
+			              max_size = (20,20),
+			              min_size = (20,20),
+			              background_color = color)
+			events['{label}/mouseClicked'.format(label=color.name)] = \
+			                             Callback(self._set_color, color_id+1)
+			colorlabels.append(label)
+
+		# split into three rows with at max 5 entries in each row
+		# right now there are 14 different colors to choose from.
+		for i in xrange(0, len(colorlabels), 5):
+			hbox = HBox(name='line_{index}'.format(index=i))
+			hbox.addChildren(colorlabels[i:i+5])
+			self.colors.addChild(hbox)
+
 		self.gui.distributeData({
 			'playername': unicode(horizons.main.fife.get_uh_setting("Nickname")),
-			'playercolor': 0
 		})
 		parent_gui.findChild(name="playerdataselectioncontainer").addChild( self.gui )
+		parent_gui.mapEvents(events)
+
+	def _set_color(self, color_id):
+		"""Updates the background color of large label where players
+		see their currently chosen color. Stores result in settings.
+		@param color_id: int. Gets converted to FIFE Color object.
+		"""
+		self.selected_color = Color[color_id]
+		horizons.main.fife.set_uh_setting("ColorID", color_id)
+		self.gui.findChild(name='selectedcolor').background_color = Color[color_id]
 
 	def get_player_name(self):
 		"""Returns the name that was entered by the user"""
@@ -51,5 +85,5 @@ class PlayerDataSelection(object):
 
 	def get_player_color(self):
 		"""Returns the color that the player selected as Color obj"""
-		return Color[self.gui.collectData('playercolor')+1] # +1 cause list entries start with 0, color indexes with 1
+		return self.selected_color
 
