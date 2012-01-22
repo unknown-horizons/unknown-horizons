fc44dd5aae117b9f763df8926d0cc48711aa6834
gscai <uh@otinn.com>
2012-01-22 20:56:59 +0000

    Move all long tests to a separate folder.

--- tests/game/test_load_save.py
+++ tests/game/test_load_save.py
@@ -21,43 +21,14 @@
 
 import os
 import tempfile
-from functools import partial
 
 from horizons.command.building import Build
 from horizons.command.production import ToggleActive
 from horizons.constants import BUILDINGS, PRODUCTION
-from horizons.util.random_map import generate_map_from_seed
-from horizons.util.savegameaccessor import SavegameAccessor
 from horizons.util.worldobject import WorldObject
 from horizons.world.production.producer import Producer
 
 from tests.game import game_test, new_session, settle, load_session
-
-
-@game_test(mapgen=partial(generate_map_from_seed, 2), human_player=False, ai_players=2, timeout=0)
-def test_save_trivial(session, _):
-	"""
-	Let 2 AI players play for a while, then attempt to save the game.
-
-	Be aware, this is a pretty simple test and it doesn't actually check what is
-	beeing saved.
-	"""
-	session.run(seconds=4 * 60)
-
-	fd, filename = tempfile.mkstemp()
-	os.close(fd)
-
-	assert session.save(savegamename=filename)
-
-	SavegameAccessor(filename)
-
-	os.unlink(filename)
-
-
-# this disables the test in general and only makes it being run when
-# called like this: run_tests.py -a long
-test_save_trivial.long = True
-
 
 @game_test(timeout=0, manual_session=True)
 def test_load_inactive_production():
