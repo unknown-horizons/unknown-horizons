39f3f0d178ae000fe89465a1ba221ee8b56084e8
totycro <totycro@unknown-horizons.org>
2012-02-05 00:55:01 +0100

    Refactored resource bar (ordinary resources)

--- horizons/gui/widgets/resourceoverviewbar.py
+++ horizons/gui/widgets/resourceoverviewbar.py
@@ -19,10 +19,14 @@
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # ###################################################
 
+import weakref
+
 from horizons.constants import RES
 from horizons.world.component.storagecomponent import StorageComponent
-from horizons.util.gui import load_uh_widget
+from horizons.util.gui import load_uh_widget, get_res_icon
 from horizons.util import PychanChildFinder
+from horizons.util.python.decorators import cachedmethod
+
 
 class ResourceOverviewBar(object):
 	"""The thing on the top left.
@@ -50,26 +54,72 @@ class ResourceOverviewBar(object):
 	GOLD_ENTRY_GUI_FILE = "resource_overview_bar_gold.xml"
 	ENTRY_GUI_FILE = "resource_overview_bar_entry.xml"
 
+	STYLE = "resource_bar"
+
+	DEFAULT_RESOURCES = [ RES.FOOD_ID,
+	                      RES.TOOLS_ID,
+	                      RES.BOARDS_ID,
+	                      RES.BRICKS_ID,
+	                      RES.TEXTILE_ID ]
+
 	def __init__(self, session):
 		from horizons.session import Session
 		assert isinstance(session, Session)
 		self.session = session
-		self.gold_gui = load_uh_widget(self.__class__.GOLD_ENTRY_GUI_FILE, style="resource_bar")
+
+		self.gold_gui = load_uh_widget(self.__class__.GOLD_ENTRY_GUI_FILE, style=self.__class__.STYLE)
 		self.gold_gui.child_finder = PychanChildFinder(self.gold_gui)
+		# set appropriate icon
+		self.gold_gui.findChild(name="res_icon").image = get_res_icon(RES.GOLD_ID)[4] # the 32 one
 		self.gui = [] # list of slots
+
+		self.resource_configurations = weakref.WeakKeyDictionary()
+
+		self.current_instance = weakref.ref(self) # can't weakref to None
+
 
 	def load(self):
 		# called when any game (also new ones) start
 		# register at player inventory for gold updates
 		inv = self.session.world.player.get_component(StorageComponent).inventory
 		inv.add_change_listener(self._update_gold, call_listener_now=True)
-
 		self.gold_gui.show()
+		self._update_gold() # call once more to make pychan happy
 
 	def set_inventory_instance(self, instance):
 		"""Display different inventory. May change resources that are displayed"""
 		print 'set instance: ', instance
-		# TODO
+		if self.current_instance() is instance:
+			return # caller is drunk yet again
+
+		# TODO: optimise
+		for i in self.gui:
+			i.hide()
+		self.gui = []
+
+		if instance is None:
+			# show nothing instead
+			self.current_instance = weakref.ref(self) # can't weakref to None
+			return
+
+		self.current_instance = weakref.ref(instance)
+
+		# construct new slots (fill values later)
+		initial_offset = 100
+		offset = 55
+		resources = self._get_current_resources()
+		for i, res in enumerate(resources):
+			entry = load_uh_widget(self.ENTRY_GUI_FILE, style=self.__class__.STYLE)
+			entry.findChild(name="entry").position = (initial_offset + offset * i, 10)
+			entry.findChild(name="res_icon").image = get_res_icon(res)[2] # the 24 one
+			entry.findChild(name="background_icon").tooltip = self.session.db.get_res_name(res)
+			self.gui.append(entry)
+			# show it just when values are entered, this appeases pychan
+
+		# fill values
+		inv = self.current_instance().get_component(StorageComponent).inventory
+		inv.add_change_listener(self._update_resources, call_listener_now=True)
+
 
 	def set_construction_mode(self, resource_source_instance, build_costs):
 		"""Show resources relevant to construction and build costs
@@ -96,6 +146,46 @@ class ResourceOverviewBar(object):
 
 		# reposition according to magic forumula passed down from the elders in order to support centering
 		self.gold_gui.resizeToContent() # update label size
-		icon_position = self.gold_gui.child_finder(name="gold_icon").position
-		gold_available_lbl.position = (icon_position[0] - gold_available_lbl.size[0]/2 + 33,  51)
+		gold_available_lbl.position = (33 - gold_available_lbl.size[0]/2,  51)
 
+
+	def _update_resources(self):
+		"""Same as _update_gold but for all other slots"""
+		inv = self.current_instance().get_component(StorageComponent).inventory
+		for i, res in enumerate(self._get_current_resources()):
+			cur_gui = self.gui[i]
+
+			# set amount
+			label = cur_gui.findChild(name="res_available")
+			label.text = unicode( inv[res] )
+
+			# reposition according to magic forumula passed down from the elders in order to support centering
+			cur_gui.show() # show to update values size values
+			label.position = (24 - label.size[0]/2, 45)
+
+	def _get_current_resources(self):
+		"""Return list of resources to display now"""
+		return self.resource_configurations.get(self.current_instance(),
+		                                        self.__class__.DEFAULT_RESOURCES)
+
+
+
+	##
+	# CODE FOR REFERENCE
+	##
+
+	@cachedmethod
+	def _get_res_background_icon_position(self):
+		# currently unused since always 0
+		# can be used for relative positioning of labels
+		# old formula: label.position = (icon.position[0] - label.size[0]/2 + xoffset, yoffset)
+		gui = load_uh_widget( self.__class__.ENTRY_GUI_FILE )
+		icon = gui.findChild(name="background_icon")
+		return icon.position
+
+	@cachedmethod
+	def _get_gold_background_icon_position(self):
+		# see above
+		gui = load_uh_widget( self.__class__.GOLD_ENTRY_GUI_FILE )
+		icon = gui.findChild(name="background_icon")
+		return icon.position
