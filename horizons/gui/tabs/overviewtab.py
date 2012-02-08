545fd4ae880a9443cd8e3de8d48abc4db2933527
Christopher Oelmueller <chris.oelmueller@gmail.com>
2012-02-08 14:33:12 +0100

    Revert "* Fix crash when clicking on mountain (Kilian)"
    This reverts commit 8a30ddc7c936245ba741949bf7034c2178c4b986.

--- horizons/gui/tabs/overviewtab.py
+++ horizons/gui/tabs/overviewtab.py
@@ -87,10 +87,9 @@ class OverviewTab(TabInterface):
 			self.instance.add_change_listener(self.refresh)
 		if not self.instance.has_remove_listener(self.on_instance_removed):
 			self.instance.add_remove_listener(self.on_instance_removed)
-		if hasattr(self.instance, 'settlement'):
-			if not self.instance.settlement.has_change_listener(self.refresh):
-				# listen for settlement name changes displayed as tab headlines
-				self.instance.settlement.add_change_listener(self.refresh)
+		if hasattr(self.instance, 'settlement') and not self.instance.settlement.has_change_listener(self.refresh):
+			# listen for settlement name changes displayed as tab headlines
+			self.instance.settlement.add_change_listener(self.refresh)
 
 	def hide(self):
 		super(OverviewTab, self).hide()
@@ -99,9 +98,8 @@ class OverviewTab(TabInterface):
 				self.instance.remove_change_listener(self.refresh)
 			if self.instance.has_remove_listener(self.on_instance_removed):
 				self.instance.remove_remove_listener(self.on_instance_removed)
-		if hasattr(self.instance, 'settlement'):
-			if self.instance.settlement.has_change_listener(self.refresh):
-				self.instance.settlement.remove_change_listener(self.refresh)
+		if hasattr(self.instance, 'settlement') and self.instance.settlement.has_change_listener(self.refresh):
+			self.instance.settlement.remove_change_listener(self.refresh)
 
 	def on_instance_removed(self):
 		self.on_remove()
