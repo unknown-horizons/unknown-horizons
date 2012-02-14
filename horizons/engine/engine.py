6f8a7e9fd2362a9eb56888d257a569083517d8c5
totycro <totycro@unknown-horizons.org>
2012-02-14 12:43:05 +0100

    Respect default values properly when upgrading settings. Thanks @ gtk!

--- horizons/engine/engine.py
+++ horizons/engine/engine.py
@@ -22,6 +22,7 @@
 
 import os
 import shutil
+import locale
 
 from fife import fife
 from fife.extensions.basicapplication import ApplicationBase
@@ -31,9 +32,6 @@ from fife.extensions.serializers.simplexml import SimpleXMLSerializer
 
 from fife.extensions.fife_settings import FIFE_MODULE
 
-import horizons.main
-
-import horizons.gui.style
 from horizons.util import SQLiteAnimationLoader, SQLiteAtlasLoader
 from horizons.constants import LANGUAGENAMES, PATHS
 from horizons.engine.settingshandler import SettingsHandler, get_screen_resolutions
@@ -89,20 +87,27 @@ class Fife(ApplicationBase):
 			template_settings_version = template_config_parser.get("meta", "SettingsVersion")
 
 			if template_settings_version > user_settings_version: # we have to update the file
+				print 'Discovered old settings file, auto-upgrading: %s -> %s' % \
+				      (user_settings_version, template_settings_version)
 				# create settings so we have a list of all settings
 				self._setup_settings(check_file_version=False)
 
 				# save settings here
 				entries = []
+
+				# need safe default value
+				default_value = object()
+
 				def update_value(modulename, entryname):
 					# retrieve values from loaded settings file
 					try:
-						value = self._setting.get(modulename, entryname)
+						value = self._setting.get(modulename, entryname, default_value)
 					except UnicodeEncodeError: # this can happen when unicode data is saved as str
 						value = "default"
-					entries.append( (modulename, entryname, value ) )
+					if value is not default_value:
+						entries.append( (modulename, entryname, value ) )
 
-				# update known settings and unreferenced settings
+				# collect values from known settings and unreferenced settings
 				for modulename, module in self._setting.entries.iteritems():
 					for entryname in module.iterkeys():
 						update_value(modulename, entryname)
@@ -217,9 +222,8 @@ class Fife(ApplicationBase):
 		for locale_code, langname in LANGUAGENAMES.items():
 			if langname == self.get_uh_setting('Language'):
 				return locale_code
-		# TODO : better way to find 'System default' ?
+		default_locale, default_encoding = locale.getdefaultlocale()
 		try:
-			default_locale, default_encoding = locale.getdefaultlocale()
 			return default_locale.split('_')[0]
 		except:
 			# If default locale could not be detected use 'EN' as fallback
