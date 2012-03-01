8b0031efe6f9f5d4beadf45be5e08cf121593db8
Christopher Oelmueller <chris.oelmueller@gmail.com>
2012-03-01 23:21:01 +0100

    * Remove TextBox style definitions, fix TextField
    TextBoxes became extinct eons ago (we do not use them and never did)

--- horizons/gui/style.py
+++ horizons/gui/style.py
@@ -53,9 +53,12 @@ STYLES = {
 		('Slider','StepSlider') : {
 			'base_color' : fife.Color(80,80,40,128),
 		},
-		'TextBox' : {
+		'TextField' : {
+			'selection_color' : fife.Color(255,255,255),
+			'background_color' : fife.Color(255,255,255,64),
+			'base_color' : fife.Color(0,0,0,0),
+			'foreground_color' : fife.Color(80,80,40),
 			'font' : '14_black',
-			'opaque': 0
 		},
 		'ListBox' : {
 			'background_color' : fife.Color(0,0,0,0),
@@ -249,10 +252,6 @@ STYLES = {
 		('Slider','StepSlider') : {
 			'base_color' : fife.Color(80,80,40,128),
 		},
-		'TextBox' : {
-			'font' : '14_black',
-			'opaque': 0
-		},
 		'ListBox' : {
 			'background_color' : fife.Color(0,0,0,0),
 			'foreground_color' : fife.Color(80,80,40),
@@ -289,10 +288,6 @@ STYLES = {
 		},
 		('Label','TooltipLabel') : {
 			'font' : '14_black',
-		},
-		'TextBox' : {
-			'font' : '14_black',
-			'opaque': 0
 		},
 		'ListBox' : {
 			'font' : '14_black',
