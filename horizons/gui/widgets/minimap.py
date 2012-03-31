d69f0b4e19966bc3bc3bc99e60fdb6bc1f334731
Kilian <kiliankoeppchen@googlemail.com>
2012-04-01 01:02:00 +0200

    Add modified version of UnknownScribe's icon for groundunits on the minimap.
    Conflicts:
    	horizons/gui/widgets/minimap.py

--- horizons/gui/widgets/minimap.py
+++ horizons/gui/widgets/minimap.py
@@ -59,6 +59,7 @@ class Minimap(object):
 	WAREHOUSE_IMAGE = "content/gui/icons/minimap/warehouse.png"
 	SHIP_NEUTRAL = "content/gui/icons/minimap/ship_neutral.png"
 	SHIP_PIRATE = "content/gui/icons/minimap/pirate.png"
+	GROUND_UNIT_IMAGE = "content/gui/icons/minimap/groundunit.png"
 
 	SHIP_DOT_UPDATE_INTERVAL = 0.5 # seconds
 
