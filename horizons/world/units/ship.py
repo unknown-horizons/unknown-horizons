3ff75885433b890f5e0cc3d45a5f1e9b141a6a38
totycro <totycro@unknown-horizons.org>
2012-04-07 22:35:46 +0200

    Fix ship_map housekeeping when the ship stops for combative business Fixes #1682

--- horizons/world/units/ship.py
+++ horizons/world/units/ship.py
@@ -106,6 +106,14 @@ class Ship(Unit):
 			self.session.world.ship_map[self.position.to_tuple()] = weakref.ref(self)
 			self.session.world.ship_map[self._next_target.to_tuple()] = weakref.ref(self)
 
+	def _movement_finished(self):
+		if self.in_ship_map:
+			# if the movement somehow stops, the position sticks, and the unit isn't at next_target any more
+			ship = self.session.world.ship_map.get(self._next_target.to_tuple())
+			if ship is not None and ship() is self:
+				del self.session.world.ship_map[self._next_target.to_tuple()]
+		super(Ship, self)._movement_finished()
+
 	def go(self, x, y):
 		#disable the trading route
 		if hasattr(self, 'route'):
