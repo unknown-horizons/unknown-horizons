5dd23fd6972f9b3ce5358ab8c1751273dc8029c7
Vivek Sagar <vivek-sagar@hotmail.com>
2012-04-16 23:49:53 +0530

    Replaced all occurances of loose.ogg to lose.ogg

--- horizons/scenario/actions.py
+++ horizons/scenario/actions.py
@@ -117,7 +117,7 @@ def do_lose(session):
 	"""Called when player lost"""
 	show_db_message(session, 'YOU_LOST')
 	#TODO rename this file to 'lose.ogg'
-	horizons.main.fife.play_sound('effects', 'content/audio/sounds/events/scenario/loose.ogg')
+	horizons.main.fife.play_sound('effects', 'content/audio/sounds/events/scenario/lose.ogg')
 	# drop events after this event
 	Scheduler().add_new_object(session.scenario_eventhandler.drop_events, session.scenario_eventhandler)
 
