58d62d129ef4d576cc79e5abfbafa22f320d124f
Magnus Knutas <magnusknutas@gmail.com>
2012-05-07 12:41:15 +0200

    makedirs is more sufficient than mkdir. And I exchanged header

--- stage_build_mac.py
+++ stage_build_mac.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
-'''
-Created on Apr 15, 2012
 
-@author: Magnus Knutas
-'''
+# ###################################################
+# Copyright (C) 2012 The Unknown Horizons Team
+# team@unknown-horizons.org
+# This file is part of Unknown Horizons.
+#
+# Unknown Horizons is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
 
 import shutil
 import sys
@@ -43,9 +59,7 @@ def setup(fife_dir):
     
     if verbose: print "Create src directory"
     # The source files, for building app correctly
-    os.mkdir('./src/')
-    os.mkdir('./src/Contents/')
-    os.mkdir('./src/Contents/Resources/')
+    os.makedirs('./src/Contents/Resources/')
     
     #Copy fife and content
     if verbose: print "Copying Icon.icns"
