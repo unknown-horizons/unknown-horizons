04ec2a0f5c306900f757641f099c9fdaacc7ad15
Mesutcan Kurt <mesutcank@gmail.com>
2012-06-25 00:03:52 +0300

    add changing color option when same colored player is in game already

--- horizons/network/networkinterface.py
+++ horizons/network/networkinterface.py
@@ -136,13 +136,16 @@ class NetworkInterface(object):
 		"""Join a game with a certain uuid"""
 		i = 2
 		try:
-			while i < 10: # try 10 different names
+			while i < 10: # try 10 different names and colors
 				try:
 					self._client.joingame(uuid)
 					return True
-				except CommandError:
+				except CommandError as e:
 					self.log.debug("NetworkInterface: failed to join")
-				self.change_name( self.__get_player_name() + unicode(i), save=False )
+					if e.message.find('name') != -1:
+						self.change_name( self.__get_player_name() + unicode(i), save=False )
+					elif e.message.find('color') != -1:
+						self.change_color(self.__get_player_color() + i, save=False)
 				i += 1
 			self._client.joingame(uuid)
 		except NetworkException as e:
