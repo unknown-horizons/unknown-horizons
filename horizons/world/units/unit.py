20d208689339b789b7bcd44aaa4ade6319aa612c
Chris Oelmueller <chris.oelmueller@gmail.com>
2012-07-22 00:50:10 +0100

    * More clear comparison logic

--- horizons/world/units/unit.py
+++ horizons/world/units/unit.py
@@ -134,14 +134,14 @@ class Unit(MovingObject, ResourceTransferHandler):
 		mid_node_up = fife.RendererNode(self._instance, relative_up)
 		mid_node_down = fife.RendererNode(self._instance, relative_dn)
 
-		if health != 0: # draw healthy part of health bar
+		if health > 0: # draw healthy part of health bar
 			renderer.addQuad(render_name,
 			                fife.RendererNode(self._instance, fife.Point(-width/2, y_pos - height)),
 			                fife.RendererNode(self._instance, fife.Point(-width/2, y_pos)),
 			                mid_node_down,
 			                mid_node_up,
 			                0, 255, 0)
-		if health != max_health: # draw damaged part
+		if health < max_health: # draw damaged part
 			renderer.addQuad(render_name,
 			                 mid_node_up,
 			                 mid_node_down,
