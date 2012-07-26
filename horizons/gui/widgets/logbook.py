8bd67654aa2c2c8ed2641afd6b63d38a7c23d839
Chris Oelmueller <chris.oelmueller@gmail.com>
2012-07-26 04:35:56 +0100

    * New logbook subwidget: [BoldLabel, text goes here]
    Since pychan has no support for inline text markup, this is as much
    as we can do at the moment (regular text is in italics already).

--- horizons/gui/widgets/logbook.py
+++ horizons/gui/widgets/logbook.py
@@ -218,8 +218,11 @@ class LogBook(PickBeltWidget):
 			for image in parameter[1]:
 				add.addChild(Icon(image=str(image)))
 		elif parameter_type == u'Headline':
-			add = Label(text=parameter[1])
-			add.stylize('headline')
+			add = Label(text=unicode(parameter[1]), wrap_text=True,
+			            min_size=(335, 0), max_size=(335, 508), font='headline')
+		elif parameter_type == u'BoldLabel':
+			add = Label(text=unicode(parameter[1]), wrap_text=True,
+			            min_size=(335, 0), max_size=(335, 508), font='14_bold')
 		elif parameter_type == u'Message':
 			add = None
 			# parameters are re-read on page reload.
