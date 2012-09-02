0bf0cf07fcd23de5fbf934cfc16724e7aac5860f
Chris Oelmueller <chris.oelmueller@gmail.com>
2012-09-02 18:16:09 +0100

    * Style cleanup: Trash unused message_text style

--- horizons/gui/style.py
+++ horizons/gui/style.py
@@ -137,27 +137,6 @@ STYLES = {
 		}
 },
 
-'message_text': {
-		'default' : {
-			'border_size': 0,
-			'margins': (0,0),
-			'opaque': 0,
-			'base_color' : fife.Color(0,0,0,0),
-			'foreground_color' : fife.Color(255,255,255),
-			'background_color' : fife.Color(0, 0, 0, 0),
-			'selection_color' : fife.Color(0,0,0,0),
-			'font' : 'small'
-		},
-		'Button' : {
-			'border_size': 0,
-			'margins' : (0,0)
-		},
-		'Label' : {
-			'margins': (0,0),
-			'font' : 'small'
-		}
-},
-
 'city_info': { # style for city info
 		'default' : {
 			'border_size': 0,
