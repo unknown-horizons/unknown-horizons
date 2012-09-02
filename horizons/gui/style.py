cee46b27e3b843537ec00831056fa0866d404255
Chris Oelmueller <chris.oelmueller@gmail.com>
2012-09-02 18:36:50 +0100

    * Style cleanup: Cut back cityinfo style a bit

--- horizons/gui/style.py
+++ horizons/gui/style.py
@@ -124,13 +124,7 @@ STYLES = {
 
 'city_info': { # style for city info
 		'default' : {
-			'border_size': 0,
-			'margins': (0,0),
-			'opaque': 0,
-			'base_color' : fife.Color(0,0,0,0),
-			'foreground_color' : fife.Color(255,255,255),
 			'background_color' : fife.Color(0, 0, 0, 0),
-			'selection_color' : fife.Color(0,0,0,0),
 			'font' : '18'
 		},
 		'Label': {
