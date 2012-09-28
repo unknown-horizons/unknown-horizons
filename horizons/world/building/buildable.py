8bbe5e628a4f799165f4e84d8ae283cdbb73991a
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-09-28 16:50:11 +0200

    Use tabs instead of spaces

--- horizons/world/building/buildable.py
+++ horizons/world/building/buildable.py
@@ -332,17 +332,17 @@ class BuildableRect(Buildable):
 		area.top -= (cls.size[1] - 1) / 2
 		area.bottom -= (cls.size[1] - 1) / 2
 
-                xstart, xend = area.left, area.right+1
-                xstep = cls.size[0]
-                if point1.x > point2.x:
-                        xstart, xend = area.right, area.left-1
-                        xstep *= -1
+		xstart, xend = area.left, area.right+1
+		xstep = cls.size[0]
+		if point1.x > point2.x:
+			xstart, xend = area.right, area.left-1
+			xstep *= -1
 
-                ystart, yend = area.top, area.bottom+1
-                ystep = cls.size[1]
-                if point1.y > point2.y:
-                        ystart, yend = area.bottom, area.top-1
-                        ystep *= -1
+		ystart, yend = area.top, area.bottom+1
+		ystep = cls.size[1]
+		if point1.y > point2.y:
+			ystart, yend = area.bottom, area.top-1
+			ystep *= -1
 
 		for x in xrange(xstart, xend, xstep):
 			for y in xrange(ystart, yend, ystep):
