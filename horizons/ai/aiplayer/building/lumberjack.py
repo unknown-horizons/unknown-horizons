3fd4209b139765dba71104326438424d84eff0c4
gscai <uh@otinn.com>
2012-11-11 17:30:29 +0100

    Speed up lumberjack construction.

--- horizons/ai/aiplayer/building/lumberjack.py
+++ horizons/ai/aiplayer/building/lumberjack.py
@@ -39,6 +39,7 @@ class AbstractLumberjack(AbstractBuilding):
 
 class LumberjackEvaluator(BuildingEvaluator):
 	__template_outline = None
+	__radius_offsets = None
 
 	@classmethod
 	def __init_outline(cls):
@@ -53,13 +54,11 @@ class LumberjackEvaluator(BuildingEvaluator):
 				coords = (x + dx, y + dy)
 				if coords not in coords_list:
 					result.add(coords)
-		cls.__template_outline = list(result)
+		cls.__template_outline = sorted(list(result))
+		cls.__radius_offsets = sorted(position.get_radius_coordinates(Entities.buildings[BUILDINGS.LUMBERJACK].radius))
 
 	@classmethod
 	def _get_outline(cls, x, y):
-		if cls.__template_outline is None:
-			cls.__init_outline()
-
 		result = []
 		for dx, dy in cls.__template_outline:
 			result.append((x + dx, y + dy))
@@ -67,14 +66,18 @@ class LumberjackEvaluator(BuildingEvaluator):
 
 	@classmethod
 	def create(cls, area_builder, x, y, orientation):
-		builder = area_builder.make_builder(BUILDINGS.LUMBERJACK, x, y, True, orientation)
-		if not builder:
+		if (x, y) not in area_builder.simple_collector_area_cache.cache[(3, 3)]:
 			return None
+
+		# TODO: create a late initialization phase for this kind of stuff
+		if cls.__radius_offsets is None:
+			cls.__init_outline()
 
 		area_value = 0
 		coastline = area_builder.land_manager.coastline
 		personality = area_builder.owner.personality_manager.get('LumberjackEvaluator')
-		for coords in builder.position.get_radius_coordinates(Entities.buildings[BUILDINGS.LUMBERJACK].radius):
+		for dx, dy in cls.__radius_offsets:
+			coords = (x + dx, y + dy)
 			if coords in area_builder.plan and coords not in coastline:
 				purpose = area_builder.plan[coords][0]
 				if purpose == BUILDING_PURPOSE.NONE:
@@ -88,6 +91,8 @@ class LumberjackEvaluator(BuildingEvaluator):
 		personality = area_builder.owner.personality_manager.get('LumberjackEvaluator')
 		alignment = cls._get_alignment_from_outline(area_builder, cls._get_outline(x, y))
 		value = area_value + alignment * personality.alignment_importance
+		builder = area_builder.make_builder(BUILDINGS.LUMBERJACK, x, y, True, orientation)
+		assert builder
 		return LumberjackEvaluator(area_builder, builder, value)
 
 	@property
