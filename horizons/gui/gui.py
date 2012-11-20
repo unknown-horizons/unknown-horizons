412bf43db2d0faf7afda88500c96469932e14769
Chris Oelmueller <chris.oelmueller@gmail.com>
2012-11-20 04:15:24 +0100

    Fix #1926: Key choice disregards scroll wheel input
    According to our decision to not have scroll actions in ListBoxes
    actually change the selection, this must not be focusable.

--- horizons/gui/gui.py
+++ horizons/gui/gui.py
@@ -821,7 +821,7 @@ class Gui(SingleplayerMenu, MultiplayerMenu):
 		popup = self.build_popup(headline, message, size=2, show_cancel_button=True)
 		update_hotkey_info(action, keyname)
 		keybox = pychan.widgets.ScrollArea()
-		listbox = pychan.widgets.ListBox()
+		listbox = pychan.widgets.ListBox(is_focusable=False)
 		keybox.max_size = listbox.max_size = \
 		keybox.min_size = listbox.min_size = \
 		keybox.size = listbox.size = (200, 200)
