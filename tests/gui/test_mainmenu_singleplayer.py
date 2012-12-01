d08afed151bfc13be05209d3043713f75080ea3d
squiddy <mr.squidd@gmail.com>
2012-12-01 21:57:55 +0100

    tests: Add some tests for the singleplayer menu
    They are still pretty simple and don't test all variants or trigger all
    of the code, but it's a start.

---
+++ tests/gui/test_mainmenu_singleplayer.py
@@ -0,0 +1,107 @@
+# ###################################################
+# Copyright (C) 2012 The Unknown Horizons Team
+# team@unknown-horizons.org
+# This file is part of Unknown Horizons.
+#
+# Unknown Horizons is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ###################################################
+
+import mock
+
+import horizons.main
+from tests.gui import gui_test
+
+
+@gui_test()
+def test_show_menu(gui):
+	"""Test that the singleplayer page shows up and closes correctly."""
+	gui.trigger('menu', 'startSingle')
+	gui.trigger('menu', 'cancel')
+
+
+def _start_game(gui):
+	"""Starts the game from the menu and returns the game options used."""
+	with mock.patch('horizons.main.start_singleplayer') as start_mock:
+		gui.trigger('menu', 'okay')
+
+		return start_mock.call_args[0][0]
+
+
+def _cleanup():
+	# FIXME this kills the map preview process before the test ends
+	# it will print out errors otherwise because the user directory
+	# doesn't exist anymore
+	proc = horizons.main._modules.gui.map_preview.calc_proc
+	if proc:
+		proc.kill()
+
+
+@gui_test()
+def test_start_scenario(gui):
+	"""Test starting a scenario."""
+	gui.trigger('menu', 'startSingle')
+	gui.trigger('menu', 'scenario')
+
+	# trigger update of scenario infos
+	gui.trigger('menu', 'maplist')
+	gui.trigger('menu', 'uni_langlist')
+
+	options = _start_game(gui)
+	assert options.is_scenario
+
+	_cleanup()
+
+
+@gui_test()
+def test_start_random_map(gui):
+	"""Test starting a new random map."""
+	gui.trigger('menu', 'startSingle')
+	gui.trigger('menu', 'random')
+
+	# disable pirates and disasters
+	gui.trigger('menu', 'lbl_pirates')
+	gui.trigger('menu', 'lbl_disasters')
+
+	options = _start_game(gui)
+	assert not options.is_scenario
+	assert not options.pirate_enabled
+	assert not options.disasters_enabled
+	assert options.trader_enabled
+
+	_cleanup()
+
+
+@gui_test()
+def test_start_map(gui):
+	"""Test starting an existing map."""
+	gui.trigger('menu', 'startSingle')
+	gui.trigger('menu', 'free_maps')
+
+	# trigger update of map info
+	gui.trigger('menu', 'maplist')
+
+	# disable pirates and trader
+	gui.trigger('menu', 'lbl_pirates')
+	gui.trigger('menu', 'lbl_free_trader')
+
+	options = _start_game(gui)
+	assert 'development' in options.game_identifier
+	assert not options.is_scenario
+	assert not options.pirate_enabled
+	assert not options.trader_enabled
+	assert options.disasters_enabled
+
+	_cleanup()
