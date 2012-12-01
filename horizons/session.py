unknown-horizons/unknown-horizons

commit 1488b1ff6146669fe223e6de81be805d4a71c040

Make the pause menu make sense in editor mode. Fixes #1931.

horizons/session.py
@@ -140,6 +140,9 @@ class Session(LivingObject):
 
 		self._old_autosave_interval = None
 
+	def in_world_editor_mode(self):
+		return False
+
 	def create_production_finished_icon_manager(self):
 		""" Checks the settings if we should display resrouce icons.
 		If True: Create the ProductionFinishedIconManager
@@ -503,12 +506,6 @@ class Session(LivingObject):
 					self.log.debug('Unable to remove unit %s', inst)
 			else:
 				self.log.error('Unable to remove unknown object %s', instance)
-
-	def save_map(self, prefix):
-		maps_folder = os.path.join(PATHS.USER_DIR, 'maps')
-		if not os.path.exists(maps_folder):
-			os.makedirs(maps_folder)
-		self.world.save_map(maps_folder, prefix)
 
 	def _do_save(self, savegame):
 		"""Actual save code.

content/gui/xml/editor/editor_pause_menu.xml
@@ -1,0 +1,60 @@
+<?xml version="1.0"?>
+<Container name="menu" size="250,350" background_image="content/gui/images/background/mainmenu/ingame.png">
+
+<VBox name="button_backgrounds"  padding="1"  position="15,15">
+	<Icon image="content/gui/images/buttons/msg_button.png" />
+	<Icon image="content/gui/images/buttons/msg_button.png" />
+	<Icon image="content/gui/images/buttons/msg_button.png" />
+	<Icon image="content/gui/images/buttons/msg_button.png" />
+	<Icon image="content/gui/images/buttons/msg_button.png" />
+	<Icon image="content/gui/images/buttons/msg_button.png" />
+</VBox>
+
+<VBox name="button_labels"  padding="35"  position="68,32" max_size="185,312" min_size="185,312">
+	<Label name="start" text="Return to editor" />
+	<Label name="savegame" text="Save map"  />
+	<Label name="loadgame" text="Load map" />
+	<Label name="settings" text="Settings" />
+	<Label name="help" text="Help"        />
+	<Label name="quit" text="Exit editor"/>
+</VBox>
+
+<VBox name="button_images"  padding="21"  position="25,25">
+	<ImageButton name="startGame"
+		up_image="content/gui/icons/mainmenu/32/compass_bw.png"
+		down_image="content/gui/icons/mainmenu/32/compass.png"
+		hover_image="content/gui/icons/mainmenu/32/compass.png"
+		is_focusable="0"/>
+
+	<ImageButton name="savegameButton"
+		up_image="content/gui/icons/mainmenu/32/save_bw.png"
+		down_image="content/gui/icons/mainmenu/32/save.png"
+		hover_image="content/gui/icons/mainmenu/32/save.png"
+		is_focusable="0"/>
+
+	<ImageButton name="loadgameButton"
+		up_image="content/gui/icons/mainmenu/32/load_bw.png"
+		down_image="content/gui/icons/mainmenu/32/load.png"
+		hover_image="content/gui/icons/mainmenu/32/load.png"
+		is_focusable="0"/>
+
+	<ImageButton name="settingsLink"
+		up_image="content/gui/icons/mainmenu/32/gears_bw.png"
+		down_image="content/gui/icons/mainmenu/32/gears.png"
+		hover_image="content/gui/icons/mainmenu/32/gears.png"
+		is_focusable="0"/>
+
+	<ImageButton name="helpLink"
+		up_image="content/gui/icons/mainmenu/32/help_bw.png"
+		down_image="content/gui/icons/mainmenu/32/help.png"
+		hover_image="content/gui/icons/mainmenu/32/help.png"
+		is_focusable="0"/>
+
+	<ImageButton name="closeButton"
+		up_image="content/gui/icons/mainmenu/32/door_bw.png"
+		down_image="content/gui/icons/mainmenu/32/door.png"
+		hover_image="content/gui/icons/mainmenu/32/door.png"
+		is_focusable="0"/>
+</VBox>
+
+</Container>

content/gui/xml/editor/save_map.xml
@@ -4,7 +4,7 @@
 	<VBox position="50,30" padding="10">
 		<Label name="headline_change_name" text="Save map" />
 		<Icon image="content/gui/images/background/hr.png" />
-		<Label name="enter_new_name_lbl" text="Enter prefix:" wrap_text="1" max_size="150,40" />
+		<Label name="enter_new_name_lbl" text="Enter name:" wrap_text="1" max_size="150,40" />
 		<TextField name="map_name" max_size="150,20" min_size="150,20" />
 
 		<HBox>

horizons/gui/gui.py
@@ -75,6 +75,7 @@ class Gui(SingleplayerMenu, MultiplayerMenu):
 	  'game_settings' : 'book',
 #	  'credits': 'book',
 	  'editor_select_map': 'book',
+	  'editor_pause_menu': 'headline',
 	  }
 
 	def __init__(self):
@@ -158,24 +159,29 @@ class Gui(SingleplayerMenu, MultiplayerMenu):
 			self.__pause_displayed = True
 			# reload the menu because caching creates spacing problems
 			# see http://trac.unknown-horizons.org/t/ticket/1047
-			self.widgets.reload('ingamemenu')
+			in_editor_mode = self.session.in_editor_mode()
+			menu_name = 'editor_pause_menu' if in_editor_mode else 'ingamemenu'
+			self.widgets.reload(menu_name)
 			def do_load():
 				did_load = self.load_game()
 				if did_load:
+					self.__pause_displayed = False
+			def do_load_map():
+				if self.editor_load_map():
 					self.__pause_displayed = False
 			def do_quit():
 				did_quit = self.quit_session()
 				if did_quit:
 					self.__pause_displayed = False
 			events = { # needed twice, save only once here
-				'e_load' : do_load,
-				'e_save' : self.save_game,
+				'e_load' : do_load_map if in_editor_mode else do_load,
+				'e_save' : self.session.ingame_gui.show_save_map_dialog if in_editor_mode else self.save_game,
 				'e_sett' : self.show_settings,
 				'e_help' : self.on_help,
 				'e_start': self.toggle_pause,
 				'e_quit' : do_quit,
 			}
-			self._switch_current_widget('ingamemenu', center=True, show=False, event_map={
+			self._switch_current_widget(menu_name, center=True, show=False, event_map={
 				  # icons
 				'loadgameButton' : events['e_load'],
 				'savegameButton' : events['e_save'],
@@ -865,7 +871,7 @@ class Gui(SingleplayerMenu, MultiplayerMenu):
 		if not retval:
 			# Dialog cancelled
 			self.current = old_current
-			return
+			return False
 
 		selected_map_index = self.current.collectData('maplist')
 		if selected_map_index == -1:
@@ -875,3 +881,4 @@ class Gui(SingleplayerMenu, MultiplayerMenu):
 		self.current = old_current
 		self.show_loading_screen()
 		horizons.main.edit_map(map_file_display[selected_map_index])
+		return True

horizons/spsession.py
@@ -19,8 +19,11 @@
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # ###################################################
 
+import os
+import os.path
 import random
 
+from constants import PATHS
 from horizons.session import Session
 from horizons.manager import SPManager
 from horizons.constants import SINGLEPLAYER
@@ -29,6 +32,13 @@ from horizons.timer import Timer
 
 class SPSession(Session):
 	"""Session tailored for singleplayer games."""
+
+	def __init__(self,gui, db, rng_seed=None):
+		super(SPSession, self).__init__(gui, db, rng_seed)
+		self.world_editor = None
+
+	def in_editor_mode(self):
+		return self.world_editor is not None
 
 	def create_manager(self):
 		return SPManager(self)
@@ -46,6 +56,9 @@ class SPSession(Session):
 
 	def autosave(self):
 		"""Called automatically in an interval"""
+		if self.in_editor_mode():
+			return
+
 		self.log.debug("Session: autosaving")
 		success = self._do_save(SavegameManager.create_autosave_filename())
 		if success:
@@ -54,6 +67,9 @@ class SPSession(Session):
 
 	def quicksave(self):
 		"""Called when user presses the quicksave hotkey"""
+		if self.in_editor_mode():
+			return
+
 		self.log.debug("Session: quicksaving")
 		# call saving through horizons.main and not directly through session, so that save errors are handled
 		success = self._do_save(SavegameManager.create_quicksave_filename())
@@ -72,6 +88,9 @@ class SPSession(Session):
 		@param savegamename: string with the full path of the savegame file or None to let user pick one
 		@return: bool, whether no error happened (user aborting dialog means success)
 		"""
+		if self.in_editor_mode():
+			return False
+
 		if savegamename is None:
 			savegamename = self.gui.show_select_savegame(mode='save')
 			if savegamename is None:
@@ -82,3 +101,9 @@ class SPSession(Session):
 		if success:
 			self.ingame_gui.message_widget.add(point=None, string_id='SAVED_GAME')
 		return success
+
+	def save_map(self, name):
+		maps_folder = os.path.join(PATHS.USER_DIR, 'maps')
+		if not os.path.exists(maps_folder):
+			os.makedirs(maps_folder)
+		self.world_editor.save_map(maps_folder, name)

horizons/world/__init__.py
@@ -676,12 +676,6 @@ class World(BuildingOwner, WorldObject):
 		Weapon.save_attacks(db)
 		self.disaster_manager.save(db)
 
-	def save_map(self, path, name):
-		"""Save the current map."""
-		if hasattr(self.session, 'world_editor'):
-			# save a map created in the editor
-			self.session.world_editor.save_map(path, name)
-
 	def get_checkup_hash(self):
 		"""Returns a collection of important game state values. Used to check if two mp games have diverged.
 		Not designed to be reliable."""

commit c91188c34334d5ba4037449bcd69ae04cddc027c

Fix pause menu in multiplayer games
This bug was introduced in 1488b1ff6146669fe223e6de81be805d4a71c040.

The method was supposed to be overwritten in SPSession, and to return
False by default (therefore working in multiplayer). However, the names
were different.

horizons/session.py
@@ -140,7 +140,7 @@ class Session(LivingObject):
 
 		self._old_autosave_interval = None
 
-	def in_world_editor_mode(self):
+	def in_editor_mode(self):
 		return False
 
 	def create_production_finished_icon_manager(self):