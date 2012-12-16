e86d8037fdd2a39afdca02f490b0b84aba3e5f96
Chris Oelmueller <chris.oelmueller@gmail.com>
2012-12-17 00:57:30 +0100

    Remove seemingly useless positioning code
    Referring to the previous commit:
    To add insult to injury, i could not observe any change in behavior when
    flipping the parameters between True and False. Indeed completely
    removing seems to not change anything, so this code departs for now.

--- horizons/gui/gui.py
+++ horizons/gui/gui.py
@@ -463,7 +463,6 @@ class Gui(object):
 			bg.image = self._background_image
 		if event_map:
 			self.current.mapEvents(event_map)
-		self.current.position_technique = "automatic" # == "center:center"
 		if show:
 			self.current.show()
 
