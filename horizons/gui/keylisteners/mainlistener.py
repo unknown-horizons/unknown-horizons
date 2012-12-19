8798f98b843a636ca5e33237453054e775d92f8c
squiddy <mr.squidd@gmail.com>
2012-12-19 20:03:45 +0100

    Remove ingame console
    AFAIK it wasn't really used, remove it until we implement a more useful
    console. A replacement fps display will be introduced in the next
    commit.

--- horizons/gui/keylisteners/mainlistener.py
+++ horizons/gui/keylisteners/mainlistener.py
@@ -20,8 +20,6 @@
 # ###################################################
 
 from fife import fife
-import code
-import sys
 import datetime
 import shutil
 import os
@@ -35,43 +33,20 @@ from horizons.gui.keylisteners import KeyConfig
 from horizons.util.living import LivingObject
 from horizons.constants import PATHS
 
-class MainListener(fife.IKeyListener, fife.ConsoleExecuter, LivingObject):
+class MainListener(fife.IKeyListener, fife.ICommandListener, LivingObject):
 	"""MainListener Class to process events of main window"""
 
 	def __init__(self, gui):
 		super(MainListener, self).__init__()
-		sys.displayhook = self # see __call__ for detailed info
 		self.gui = gui
 		fife.IKeyListener.__init__(self)
-		fife.ConsoleExecuter.__init__(self)
 		horizons.globals.fife.eventmanager.addKeyListener(self)
-		horizons.globals.fife.console.setConsoleExecuter(self)
-
-		#ugly but works o_O
-		class CmdListener(fife.ICommandListener): pass
-		self.cmdlist = CmdListener()
-		horizons.globals.fife.eventmanager.addCommandListener(self.cmdlist)
-		self.cmdlist.onCommand = self.onCommand
-
-		self.commandbuffer = ''
-
-	def __call__(self, cmd):
-		"""
-		The default displayhook method would save computation results
-		in a temp variable called _.  This is rather unfortunate because
-		our gettext functionality also is globally imported under that name.
-		To solve, we have to set a custom Executer not storing anything in _.
-		Cf. FIFE ticket: http://fife.trac.cvsdude.com/engine/ticket/560
-
-		Right now does nothing besides printing to interactive and user shell.
-		#TODO Feel free to implement ;-)
-		"""
-		print "calling %s" % cmd
+		fife.ICommandListener.__init__(self)
+		horizons.globals.fife.eventmanager.addCommandListener(self)
 
 	def end(self):
 		horizons.globals.fife.eventmanager.removeKeyListener(self)
 		super(MainListener, self).end()
-
 
 	def keyPressed(self, evt):
 		if evt.isConsumed():
@@ -84,8 +59,6 @@ class MainListener(fife.IKeyListener, fife.ConsoleExecuter, LivingObject):
 
 		if action == _Actions.ESCAPE:
 			self.gui.on_escape()
-		elif action == _Actions.CONSOLE:
-			horizons.globals.fife.console.toggleShowHide()
 		elif action == _Actions.HELP:
 			self.gui.on_help()
 		elif action == _Actions.SCREENSHOT:
@@ -112,7 +85,6 @@ class MainListener(fife.IKeyListener, fife.ConsoleExecuter, LivingObject):
 		if key_event_handled:
 			evt.consume() # prevent other listeners from being called
 
-
 	def keyReleased(self, evt):
 		pass
 
@@ -120,44 +92,3 @@ class MainListener(fife.IKeyListener, fife.ConsoleExecuter, LivingObject):
 		if command.getCommandType() == fife.CMD_QUIT_GAME:
 			horizons.main.quit()
 			command.consume()
-
-	def onConsoleCommand(self, command):
-		if command == ' ':
-			command = ''
-		try:
-			cmd = code.compile_command(self.commandbuffer + "\n" + command)
-		except BaseException as e:
-			self.commandbuffer = ''
-			return str(e)
-		if cmd is None:
-			self.commandbuffer += "\n" + command
-			return ''
-		self.commandbuffer = ''
-		oldout = sys.stdout
-		class console_file(object):
-			def __init__(self, copy=None):
-				self.buffer = ''
-				self.copy = copy
-			def write(self, string):
-				parts = (self.buffer + string).split("\n")
-				self.buffer = parts.pop()
-				for p in parts:
-					horizons.globals.fife.console.println(p)
-				self.copy.write(string)
-			def __del__(self):
-				if self.buffer:
-					self.write('\n')
-		sys.stdout = console_file(oldout)
-		try:
-			exec cmd in globals()
-		except BaseException as e:
-			sys.stdout = oldout
-			return str(e)
-		sys.stdout = oldout
-		return ''
-
-	def onToolsClick(self):
-		"""
-		Define what happens if we click on the Tools button in FIFE console
-		"""
-		self.onConsoleCommand('import debug')
