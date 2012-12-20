unknown-horizons/unknown-horizons

commit 435a19d58b7e5cac0ab2cffe610ff6e6a7a33d25

Drop the specific setting messages in favor of SettingChanged

horizons/gui/widgets/minimap.py
@@ -20,6 +20,8 @@
 
 import itertools
 import json
+import math
+from math import sin, cos
 
 import horizons.globals
 from fife import fife
@@ -29,10 +31,8 @@ from horizons.util.python.decorators import bind_all
 from horizons.util.shapes import Circle, Point, Rect
 from horizons.command.unit import Act
 from horizons.component.namedcomponent import NamedComponent
-from horizons.messaging import MinimapRotationSettingChanged
+from horizons.messaging import SettingChanged
 
-import math
-from math import sin, cos
 
 class Minimap(object):
 	"""A basic minimap.
@@ -130,7 +130,7 @@ class Minimap(object):
 
 		self._rotation_setting = horizons.globals.fife.get_uh_setting("MinimapRotation")
 		if self.use_rotation:
-			MinimapRotationSettingChanged.subscribe(self._on_rotation_setting_change)
+			SettingChanged.subscribe(self._on_setting_changed)
 
 	def end(self):
 		self.disable()
@@ -138,7 +138,7 @@ class Minimap(object):
 		self.session = None
 		self.renderer = None
 		if self.use_rotation:
-			MinimapRotationSettingChanged.unsubscribe(self._on_rotation_setting_change)
+			SettingChanged.unsubscribe(self._on_setting_change)
 
 	def disable(self):
 		"""Due to the way the minimap works, there isn't really a show/hide,
@@ -671,9 +671,10 @@ class Minimap(object):
 			return False
 		return self._rotation_setting
 
-	def _on_rotation_setting_change(self, message):
-		self._rotation_setting = horizons.globals.fife.get_uh_setting("MinimapRotation")
-		self.draw()
+	def _on_setting_changed(self, message):
+		if message.setting_name == "MinimapRotation":
+			self._rotation_setting = message.new_value
+			self.draw()
 
 	_rotations = { 0 : 0,
 				         1 : 3 * math.pi / 2,

horizons/engine/settingshandler.py
@@ -35,7 +35,7 @@ from horizons.extscheduler import ExtScheduler
 from horizons.constants import LANGUAGENAMES, PATHS
 from horizons.network.networkinterface import NetworkInterface
 from horizons.engine import UH_MODULE
-from horizons.messaging import AutosaveIntervalChanged, MinimapRotationSettingChanged
+
 
 class SettingsHandler(object):
 	"""Handles settings-related boilerplate code as well as gui."""
@@ -48,15 +48,8 @@ class SettingsHandler(object):
 		return self.engine._setting
 
 	def add_settings(self):
-		def update_minimap(*args):
-			MinimapRotationSettingChanged.broadcast(None)
-
-		def update_autosave_interval(*args):
-			AutosaveIntervalChanged.broadcast(None)
-
 		#self.createAndAddEntry(self, module, name, widgetname, applyfunction=None, initialdata=None, requiresrestart=False)
-		self._setting.createAndAddEntry(UH_MODULE, "AutosaveInterval", "autosaveinterval",
-		                                applyfunction=update_autosave_interval)
+		self._setting.createAndAddEntry(UH_MODULE, "AutosaveInterval", "autosaveinterval")
 		self._setting.createAndAddEntry(UH_MODULE, "AutosaveMaxCount", "autosavemaxcount")
 		self._setting.createAndAddEntry(UH_MODULE, "QuicksaveMaxCount", "quicksavemaxcount")
 		self._setting.createAndAddEntry(UH_MODULE, "EdgeScrolling", "edgescrolling")
@@ -65,8 +58,7 @@ class SettingsHandler(object):
 		self._setting.createAndAddEntry(UH_MODULE, "MiddleMousePan", "middle_mouse_pan")
 		self._setting.createAndAddEntry(UH_MODULE, "UninterruptedBuilding", "uninterrupted_building")
 		self._setting.createAndAddEntry(UH_MODULE, "AutoUnload", "auto_unload")
-		self._setting.createAndAddEntry(UH_MODULE, "MinimapRotation", "minimaprotation",
-		                                applyfunction=update_minimap)
+		self._setting.createAndAddEntry(UH_MODULE, "MinimapRotation", "minimaprotation")
 
 		self._setting.createAndAddEntry(UH_MODULE, "QuotesType", "quotestype",
 		                                initialdata=QUOTES_SETTINGS)

horizons/messaging/message.py
@@ -202,14 +202,6 @@ class PlayerInventoryUpdated(Message):
 	"""Message sent whenever a player's inventory is updated"""
 	pass
 
-class AutosaveIntervalChanged(Message):
-	"""Sent when the autosave interval is changed."""
-	pass
-
-class MinimapRotationSettingChanged(Message):
-	"""Sent when the setting that enables/disables rotating the minimap with the map is changed."""
-	pass
-
 class LanguageChanged(Message):
 	"""Sent when the language has changed."""
 	pass

horizons/session.py
@@ -51,7 +51,7 @@ from horizons.savegamemanager import SavegameManager
 from horizons.scenario import ScenarioEventHandler
 from horizons.component.ambientsoundcomponent import AmbientSoundComponent
 from horizons.constants import GAME_SPEED
-from horizons.messaging import AutosaveIntervalChanged, MessageBus, SpeedChanged
+from horizons.messaging import SettingChanged, MessageBus, SpeedChanged
 
 class Session(LivingObject):
 	"""The Session class represents the game's main ingame view and controls cameras and map loading.
@@ -127,7 +127,7 @@ class Session(LivingObject):
 		"""Actually starts the game."""
 		self.timer.activate()
 		self.reset_autosave()
-		AutosaveIntervalChanged.subscribe(self._on_autosave_interval_changed)
+		SettingChanged.subscribe(self._on_setting_changed)
 
 	def reset_autosave(self):
 		"""(Re-)Set up autosave. Called if autosave interval has been changed."""
@@ -140,8 +140,9 @@ class Session(LivingObject):
 				self.log.debug("Initing autosave every %s minutes", interval)
 				ExtScheduler().add_new_object(self.autosave, self, interval * 60, -1)
 
-	def _on_autosave_interval_changed(self, message):
-		self.reset_autosave()
+	def _on_setting_changed(self, message):
+		if message.setting_name == 'AutosaveInterval':
+			self.reset_autosave()
 
 	def create_manager(self):
 		"""Returns instance of command manager (currently MPManager or SPManager)"""
@@ -201,7 +202,7 @@ class Session(LivingObject):
 
 		# subscriptions shouldn't survive listeners (except the main Gui)
 		self.gui.unsubscribe()
-		AutosaveIntervalChanged.unsubscribe(self._on_autosave_interval_changed)
+		SettingChanged.unsubscribe(self._on_setting_changed)
 		MessageBus().reset()
 		self.gui.subscribe()
 

commit 0d6c6d131761ea7b8d801e6ec19ae8d0ae822c57

Fix typo when unsubscribing from message in minimap
Small typo introduced in 435a19d58b7e5cac0ab2cffe610ff6e6a7a33d25

horizons/gui/widgets/minimap.py
@@ -138,7 +138,7 @@ class Minimap(object):
 		self.session = None
 		self.renderer = None
 		if self.use_rotation:
-			SettingChanged.unsubscribe(self._on_setting_change)
+			SettingChanged.unsubscribe(self._on_setting_changed)
 
 	def disable(self):
 		"""Due to the way the minimap works, there isn't really a show/hide,