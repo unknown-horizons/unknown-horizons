72b0a5a3a9e6bfb2a3229cef526b2a80a8e67e27
squiddy <mr.squidd@gmail.com>
2012-12-22 00:23:44 +0100

    Move dialog creation code to a new WindowManager

--- horizons/gui/gui.py
+++ horizons/gui/gui.py
@@ -22,20 +22,17 @@
 import glob
 import logging
 import random
-import traceback
 
 from fife import fife
-from fife.extensions import pychan
 
 import horizons.globals
 import horizons.main
 
 from horizons.i18n.quotes import GAMEPLAY_TIPS, FUN_QUOTES
 from horizons.gui.keylisteners import MainListener
-from horizons.gui.widgets.imagebutton import OkButton, CancelButton
+from horizons.gui.widgets.imagebutton import OkButton
 from horizons.gui.widgets.pickbeltwidget import CreditsPickbeltWidget
 from horizons.util.startgameoptions import StartGameOptions
-from horizons.extscheduler import ExtScheduler
 from horizons.messaging import GuiAction
 from horizons.component.ambientsoundcomponent import AmbientSoundComponent
 from horizons.gui.util import LazyWidgetsDict
@@ -44,6 +41,7 @@ from horizons.gui.modules.editorstartmenu import EditorStartMenu
 from horizons.gui.modules import (SingleplayerMenu, MultiplayerMenu, HelpDialog,
                                   SelectSavegameDialog)
 from horizons.gui.widgets.fpsdisplay import FPSDisplay
+from horizons.gui.windows import WindowManager
 from horizons.command.game import PauseCommand, UnPauseCommand
 
 class Gui(object):
@@ -78,9 +76,12 @@ class Gui(object):
 		self.current = None # currently active window
 		self.widgets = LazyWidgetsDict(self.styles) # access widgets with their filenames without '.xml'
 		self.session = None
-		self.current_dialog = None
 
-		self.dialog_executed = False
+		self.windows = WindowManager(self.widgets)
+		# temporary aliases for compatibility with rest of the code
+		self.show_dialog = self.windows.show_dialog
+		self.show_popup = self.windows.show_popup
+		self.show_error_popup = self.windows.show_error_popup
 
 		self.__pause_displayed = False
 		self._background_image = self._get_random_background()
@@ -195,7 +196,7 @@ class Gui(object):
 				'quit'     : events['e_quit'],
 			})
 
-			self.show_modal_background()
+			self.windows.show_modal_background()
 			self.current.show()
 
 			PauseCommand(suggestion=True).execute(self.session)
@@ -268,160 +269,10 @@ class Gui(object):
 		self.log.debug("Gui: hiding current: %s", self.current)
 		if self.current is not None:
 			self.current.hide()
-			self.hide_modal_background()
+			self.windows.hide_modal_background()
 
 	def is_visible(self):
 		return self.current is not None and self.current.isVisible()
-
-	def show_dialog(self, dlg, bind, event_map=None, modal=False):
-		"""Shows any pychan dialog.
-		@param dlg: dialog that is to be shown
-		@param bind: events that make the dialog return + return values{ 'ok': callback, 'cancel': callback }
-		@param event_map: dictionary with callbacks for buttons. See pychan docu: pychan.widget.mapEvents()
-		@param modal: Whether to block user interaction while displaying the dialog
-		"""
-		self.current_dialog = dlg
-		if event_map is not None:
-			dlg.mapEvents(event_map)
-		if modal:
-			self.show_modal_background()
-
-		# handle escape and enter keypresses
-		def _on_keypress(event, dlg=dlg): # rebind to make sure this dlg is used
-			from horizons.engine import pychan_util
-			if event.getKey().getValue() == fife.Key.ESCAPE: # convention says use cancel action
-				btn = dlg.findChild(name=CancelButton.DEFAULT_NAME)
-				callback = pychan_util.get_button_event(btn) if btn else None
-				if callback:
-					pychan.tools.applyOnlySuitable(callback, event=event, widget=btn)
-				else:
-					# escape should hide the dialog default
-					horizons.globals.fife.pychanmanager.breakFromMainLoop(returnValue=False)
-					dlg.hide()
-			elif event.getKey().getValue() == fife.Key.ENTER: # convention says use ok action
-				btn = dlg.findChild(name=OkButton.DEFAULT_NAME)
-				callback = pychan_util.get_button_event(btn) if btn else None
-				if callback:
-					pychan.tools.applyOnlySuitable(callback, event=event, widget=btn)
-				# can't guess a default action here
-
-		dlg.capture(_on_keypress, event_name="keyPressed")
-
-		# show that a dialog is being executed, this can sometimes require changes in program logic elsewhere
-		self.dialog_executed = True
-		ret = dlg.execute(bind)
-		self.dialog_executed = False
-		if modal:
-			self.hide_modal_background()
-		return ret
-
-	def show_popup(self, windowtitle, message, show_cancel_button=False, size=0, modal=True):
-		"""Displays a popup with the specified text
-		@param windowtitle: the title of the popup
-		@param message: the text displayed in the popup
-		@param show_cancel_button: boolean, show cancel button or not
-		@param size: 0, 1 or 2. Larger means bigger.
-		@param modal: Whether to block user interaction while displaying the popup
-		@return: True on ok, False on cancel (if no cancel button, always True)
-		"""
-		popup = self.build_popup(windowtitle, message, show_cancel_button, size=size)
-		# ok should be triggered on enter, therefore we need to focus the button
-		# pychan will only allow it after the widgets is shown
-		def focus_ok_button():
-			popup.findChild(name=OkButton.DEFAULT_NAME).requestFocus()
-		ExtScheduler().add_new_object(focus_ok_button, self, run_in=0)
-		if show_cancel_button:
-			return self.show_dialog(popup, {OkButton.DEFAULT_NAME : True,
-			                                CancelButton.DEFAULT_NAME : False},
-			                        modal=modal)
-		else:
-			return self.show_dialog(popup, {OkButton.DEFAULT_NAME : True},
-			                        modal=modal)
-
-	def show_error_popup(self, windowtitle, description, advice=None, details=None, _first=True):
-		"""Displays a popup containing an error message.
-		@param windowtitle: title of popup, will be auto-prefixed with "Error: "
-		@param description: string to tell the user what happened
-		@param advice: how the user might be able to fix the problem
-		@param details: technical details, relevant for debugging but not for the user
-		@param _first: Don't touch this.
-
-		Guide for writing good error messages:
-		http://www.useit.com/alertbox/20010624.html
-		"""
-		msg = u""
-		msg += description + u"\n"
-		if advice:
-			msg += advice + u"\n"
-		if details:
-			msg += _("Details: {error_details}").format(error_details=details)
-		try:
-			self.show_popup( _("Error: {error_message}").format(error_message=windowtitle),
-			                 msg, show_cancel_button=False)
-		except SystemExit: # user really wants us to die
-			raise
-		except:
-			# could be another game error, try to be persistent in showing the error message
-			# else the game would be gone without the user being able to read the message.
-			if _first:
-				traceback.print_exc()
-				print 'Exception while showing error, retrying once more'
-				return self.show_error_popup(windowtitle, description, advice, details, _first=False)
-			else:
-				raise # it persists, we have to die.
-
-	def build_popup(self, windowtitle, message, show_cancel_button=False, size=0):
-		""" Creates a pychan popup widget with the specified properties.
-		@param windowtitle: the title of the popup
-		@param message: the text displayed in the popup
-		@param show_cancel_button: boolean, include cancel button or not
-		@param size: 0, 1 or 2
-		@return: Container(name='popup_window') with buttons 'okButton' and optionally 'cancelButton'
-		"""
-		if size == 0:
-			wdg_name = "popup_230"
-		elif size == 1:
-			wdg_name = "popup_290"
-		elif size == 2:
-			wdg_name = "popup_350"
-		else:
-			assert False, "size should be 0 <= size <= 2, but is "+str(size)
-
-		# NOTE: reusing popup dialogs can sometimes lead to exit(0) being called.
-		#       it is yet unknown why this happens, so let's be safe for now and reload the widgets.
-		self.widgets.reload(wdg_name)
-		popup = self.widgets[wdg_name]
-
-		if not show_cancel_button:
-			cancel_button = popup.findChild(name=CancelButton.DEFAULT_NAME)
-			cancel_button.parent.removeChild(cancel_button)
-
-		popup.headline = popup.findChild(name='headline')
-		popup.headline.text = _(windowtitle)
-		popup.message = popup.findChild(name='popup_message')
-		popup.message.text = _(message)
-		popup.adaptLayout() # recalculate widths
-		return popup
-
-	def show_modal_background(self):
-		""" Loads transparent background that de facto prohibits
-		access to other gui elements by eating all input events.
-		Used for modal popups and our in-game menu.
-		"""
-		height = horizons.globals.fife.engine_settings.getScreenHeight()
-		width = horizons.globals.fife.engine_settings.getScreenWidth()
-		image = horizons.globals.fife.imagemanager.loadBlank(width, height)
-		image = fife.GuiImage(image)
-		self.additional_widget = pychan.Icon(image=image)
-		self.additional_widget.position = (0, 0)
-		self.additional_widget.show()
-
-	def hide_modal_background(self):
-		try:
-			self.additional_widget.hide()
-			del self.additional_widget
-		except AttributeError:
-			pass # only used for some widgets, e.g. pause
 
 	def show_loading_screen(self):
 		self._switch_current_widget('loadingscreen', show=True)
--- horizons/gui/modules/multiplayermenu.py
+++ horizons/gui/modules/multiplayermenu.py
@@ -144,7 +144,7 @@ class MultiplayerMenu(object):
 		"""Error callback"""
 		if fatal and self.mainmenu.session is not None:
 			self.mainmenu.session.timer.ticks_per_second = 0
-		if self.mainmenu.dialog_executed:
+		if self.mainmenu.windows.dialog_executed:
 			# another message dialog is being executed, and we were called by that action.
 			# if we now trigger another message dialog, we will probably loop.
 			return
