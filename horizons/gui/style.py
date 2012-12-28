b0ede618a662643c30a81df10698d9bd35de833b
Chris Oelmueller <chris.oelmueller@gmail.com>
2012-12-28 01:03:21 +0100

    style: Remove empty attribute hacks from nondefault styles
    Still keep them for the default style. You never know.

--- horizons/gui/style.py
+++ horizons/gui/style.py
@@ -101,17 +101,11 @@ STYLES = {
 		'default': {
 			'font': '18',
 		},
-		'Label': {
-			#HACK apply defaults to xml widgets without adaptLayout code
-		},
 },
 
 'headline': { # style for headlines
 		'default': {
 			'font': 'headline',
-		},
-		# NOTE: This is a hack to add padding attributs to boxes of this style
-		('Container', 'HBox', 'VBox'): {
 		},
 },
 
@@ -120,8 +114,6 @@ STYLES = {
 		'default': {
 			'font': 'tooltip',
 		},
-		'Label': {
-		},
-	},
+},
 
 }
