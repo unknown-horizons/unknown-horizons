2d64bbcf1c45fd104c96d9a56c86c437dc128605
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-01-05 22:43:40 +0100

    Less obtrusive buildability highlights (cherry picked from commit bf12586c4b08ca6aba51cd01937c8a6c5f55aa5a)

--- horizons/gui/mousetools/buildingtool.py
+++ horizons/gui/mousetools/buildingtool.py
@@ -83,8 +83,8 @@ class BuildingTool(NavigationTool):
 	log = logging.getLogger("gui.buildingtool")
 
 	buildable_color = (255, 255, 255)
-	not_buildable_color = (255, 0, 0)
-	related_building_color = (0, 192, 0)
+	not_buildable_color = (255, 0, 0, 160)
+	related_building_color = (0, 192, 0, 160)
 	related_building_outline = (16, 228, 16, 2)
 	nearby_objects_radius = 4
 
