c4ef78db0af89fc10f72d3610a8b1ce8d8b0aea6
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-01-28 15:29:18 +0100

    progressbar: `fill` attribute for xml definition
    Also roll back ProgressBar to one resizable image instead of tiling:
    * There were too many images with a TilingBG of 1px tiles
    * Stretching 1px tiles via pychan yields the exact same result as tiling
    
    Might add a TilingProgressBar sometime later if we decide it's useful.

--- horizons/gui/widgets/progressbar.py
+++ horizons/gui/widgets/progressbar.py
@@ -34,27 +34,36 @@ class ProgressBar(AutoResizeContainer):
 	widgets progress attribute to set the progress. Pretty straight forward.
 	The progress is a value from 0 to 100. Think of it as percent.
 	"""
-	ATTRIBUTES = AutoResizeContainer.ATTRIBUTES + [IntAttr('progress'), Attr('background')]
+	ATTRIBUTES = AutoResizeContainer.ATTRIBUTES + [
+		IntAttr('progress'), Attr('fill'), Attr('background'),
+	]
 
-	def __init__(self, progress=0, background=None, **kwargs):
+	def __init__(self, progress=0, fill=None, background=None, **kwargs):
 		super(ProgressBar, self).__init__(**kwargs)
-		self.fill = "content/gui/images/background/widgets/progressbar_fill.png"
 		if self.max_size == Widget.DEFAULT_MAX_SIZE:
 			self.max_size = (100, 16)
 		self.__progress = progress
+		self.__fill = fill
 		self.__background = background
-		self.tiles = TilingHBox()
-		self.tiles.tiles_img = self.fill
-		self.tiles.start_img = self.tiles.final_img = None
-		self.addChild(self.tiles)
+		self.tiles = None
 
 	def _set_progress(self, progress):
 		self.__progress = progress
-		self.tiles.amount = int(progress * self.max_size[0] / 100)
+		if self.tiles is None:
+			self.fill = "content/gui/images/background/widgets/progressbar_fill.png"
+		self.tiles.size = (int(progress * self.max_size[0] / 100), self.max_size[1])
 		self.adaptLayout()
 
 	def _get_progress(self):
 		return self.__progress
+
+	def _set_fill_image(self, image):
+		self.__fill = image
+		self.tiles = Icon(image=image)
+		self.addChild(self.tiles)
+
+	def _get_fill_image(self):
+		return self.__fill
 
 	def _set_background(self, background):
 		self.__background = background
@@ -66,4 +75,5 @@ class ProgressBar(AutoResizeContainer):
 		return self.__background
 
 	progress = property(_get_progress, _set_progress)
+	fill = property(_get_fill_image, _set_fill_image)
 	background = property(_get_background, _set_background)
