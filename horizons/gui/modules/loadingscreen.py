f9d0c50ad150e5a1afc8401bc93156fada171093
squiddy <mr.squidd@gmail.com>
2013-01-28 14:29:17 +0100

    update progress bar in discrete steps

--- horizons/gui/modules/loadingscreen.py
+++ horizons/gui/modules/loadingscreen.py
@@ -19,9 +19,7 @@
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # ###################################################
 
-import pprint
 import random
-import time
 
 import horizons.globals
 from horizons.i18n.quotes import GAMEPLAY_TIPS, FUN_QUOTES
@@ -30,31 +28,18 @@ from horizons.gui.windows import Window
 from horizons.messaging import LoadingProgress
 
 
-# run game with LoadingScreen.profile True to get these values
-LOAD_PROGRESS = {
-	'finish': 100,
-	'load_objects': 0,
-	'session_create_world': 4,
-	'session_index_fish': 64,
-	'session_load_gui': 90,
-	'start': 0,
-	'world_init_water': 22,
-	'world_load_buildings': 17,
-	'world_load_map': 4,
-	'world_load_stuff': 24,
-	'world_load_units': 24
-}
-
-
 class LoadingScreen(Window):
 	"""Show quotes/gameplay tips while loading the game"""
-	profile = False
+
+	# how often the LoadingProgress message is send when loading a game,
+	# used to update the progress bar
+	total_steps = 11
 
 	def __init__(self):
 		self._widget = load_uh_widget('loadingscreen.xml')
 		self._widget.position_technique = "center:center"
 
-		self._times = []
+		self._current_step = 0
 
 	def show(self):
 		qotl_type_label = self._widget.findChild(name='qotl_type_label')
@@ -76,27 +61,18 @@ class LoadingScreen(Window):
 		self._widget.show()
 		LoadingProgress.subscribe(self._update)
 
-		if self.profile:
-			self._times.append(('start', time.time()))
-
 	def hide(self):
-		LoadingProgress.unsubscribe(self._update)
+		self._current_step = 0
+		LoadingProgress.discard(self._update)
 		self._widget.hide()
 
-		if self.profile:
-			start = self._times[0][1]
-			total = self._times[-1][1] - start
-			pprint.pprint(dict([(stage, int(100 * (t - start) / total)) for (stage, t) in self._times]))
-
 	def _update(self, message):
-		if self.profile:
-			self._times.append((message.stage, time.time()))
+		self._current_step += 1
 
 		label = self._widget.findChild(name='loading_stage')
 		label.text = unicode(message.stage)
 		label.adaptLayout()
 
-		if not self.profile:
-			self._widget.findChild(name='loading_progress').progress = LOAD_PROGRESS[message.stage]
+		self._widget.findChild(name='loading_progress').progress = int(100 * self._current_step / self.total_steps)
 
 		horizons.globals.fife.engine.pump()
