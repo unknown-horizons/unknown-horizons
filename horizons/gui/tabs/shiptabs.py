1f042627686e66e49d6fb349b2cf4b351a03b598
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-02-06 20:51:36 +0100

    tabwidget: Rewrite and relayout overview_enemy{building,unit,warehouse}
    Entirely removes the currently redundant overview for enemy units.
    To be added at a later point where such units' information displayed in
    those tabs is actually different from enemy buildings.
    
    Conflicts:
    	content/gui/xml/ingame/tabwidget/overview/overview_enemywarehouse.xml
    	horizons/gui/tabs/shiptabs.py

--- horizons/gui/tabs/shiptabs.py
+++ horizons/gui/tabs/shiptabs.py
@@ -173,10 +173,6 @@ class TraderShipOverviewTab(OverviewTab):
 	helptext = _lazy("Ship overview")
 
 class EnemyShipOverviewTab(OverviewTab):
-	widget = 'overview_enemyunit.xml'
+	widget = 'overview_enemybuilding.xml'
 	icon_path = 'icons/tabwidget/ship/ship_inv'
 	helptext = _lazy("Ship overview")
-
-	def init_widget(self):
-		super(ShipOverviewTab, self).init_widget()
-		self.widget.findChild(name="headline").text = self.instance.owner.name
