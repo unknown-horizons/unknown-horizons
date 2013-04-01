736e13807a7a4ffd83a87f643a0d5281e71cc13d
ThePawnBreak <mocanu.cristian93@gmail.com>
2013-04-01 19:43:47 +0300

    Bind events to secondary buttons.
    The problem is that now major modifications to the apply_changes function must be made, and there are some cases in which it's not very clear what to do.

--- horizons/gui/modules/hotkeys_settings.py
+++ horizons/gui/modules/hotkeys_settings.py
@@ -39,6 +39,7 @@ class HotkeyConfiguration(Window):
 		self._session = session
 		self.widget = load_uh_widget('hotkeys.xml')
 		self.buttons = []
+		self.secondary_buttons = []
 
 		self.actions = sorted([action for action in horizons.globals.fife.get_hotkey_settings()])
 
@@ -51,11 +52,13 @@ class HotkeyConfiguration(Window):
 		self.current_button = None
 		self.current_index = None
 		self.last_combination = []
+		self.last_column = 1
 
 		self.keys = self.keyconf.get_keys_by_value()
 
 		self.widget.mapEvents({self.widget.name + '/keyPressed' : self._detect_keypress})
-		self.widget.findChild(name=OkButton.DEFAULT_NAME).capture(self._windows.close)
+		self.widget.findChild(name=OkButton.DEFAULT_NAME).capture(self.save_settings)
+		self.widget.mapEvents({OkButton.DEFAULT_NAME : self.save_settings})
 		self.widget.findChild(name="reset_to_default").capture(self.reset_to_default)
 
 	def _build_interface(self):
@@ -65,13 +68,15 @@ class HotkeyConfiguration(Window):
 		for i in range(len(self.actions)):
 			action = self.actions[i]
 			label = self._create_label(action)
-			button = self._create_button(action, 'prim', i)
-			button2 = self._create_button(action, 'sec', i)
-			button.mapEvents({button.name + '/mouseClicked' : Callback(self._detect_click_on_button, button)})
+			button = self._create_button(action, 0, i)
+			button2 = self._create_button(action, 1, i)
+			button.mapEvents({button.name + '/mouseClicked' : Callback(self._detect_click_on_button, button, 1)})
+			button2.mapEvents({button.name + '/mouseClicked' : Callback(self._detect_click_on_button, button2, 2)})
 			container.addChild(label)
 			button_container.addChild(button)
 			sec_button_container.addChild(button2)
 			self.buttons.append(button)
+			self.secondary_buttons.append(button2)
 
 	def _create_label(self, action):
 		#xgettext:python-format
@@ -80,18 +85,35 @@ class HotkeyConfiguration(Window):
 		label.max_size = (130,42)
 		return label
 
-	def _create_button(self, action, prefix, index):
+	def _create_button(self, action, action_index, index):
+		current_binding = self.keyconf.get_current_keys(action)
+
+		if len(current_binding) <= action_index:
+				# if there are less bindings than buttons
+				keyname = "-"
+		else:
+				keyname = current_binding[action_index]
+
 		#xgettext:python-format
-		keyname = _(self.keyconf.get_current_keys(action)[0])
+		keyname = _(keyname)
 		button = Button(text=keyname)
 		button.name = str(index)
 		button.max_size = (130,18)
 		return button
 
-	def _detect_click_on_button(self, button):
+	def get_current_bindings(self):
+		""" Returns a dict mapping action -> list of keys"""
+		bindings = {}
+		for action in self.actions:
+			keys = self.keyconf.get_current_keys(action)
+			bindings[action] = keys
+		return bindings
+
+	def _detect_click_on_button(self, button, column):
 		self.detecting = True
 		self.current_button = button
 		self.current_index = int(button.name)
+		self.current_column = column
 		#xgettext:python-format
 		button.text = _("Press desired key")
 
@@ -106,8 +128,11 @@ class HotkeyConfiguration(Window):
 		return self.keys[value]
 
 	def key_is_set(self, key):
-		keys_mappings = self.keyconf.keyval_action_mappings
-		if keys_mappings.get(key.getValue()):
+		key_name = self.keyName(key)
+		custom_key_actions = horizons.globals.fife.get_hotkey_settings()
+		for action in custom_key_actions:
+			k = custom_key_actions[action]
+			if key_name in k:
 				return True
 		return False
 
@@ -124,21 +149,25 @@ class HotkeyConfiguration(Window):
 		for action in self.actions:
 			default_key = horizons.globals.fife.get_keys_for_action(action, default=True)
 			horizons.globals.fife.set_key_for_action(action, default_key)
-		horizons.globals.fife.save_settings()
 
 		self.update_buttons_text()
 
 	def update_buttons_text(self):
 		for i in range(len(self.buttons)):
-			button = self.buttons[i]
 			action = self.actions[i]
-			keyname = _(self.keyconf.get_current_keys(action)[0])
-			button.text = keyname
+			bindings = self.keyconf.get_current_keys(action)
+			button = self.buttons[i]
+			secondary_button = self.secondary_buttons[i]
+			button.text = _(bindings[0])
+			if len(bindings) > 1:
+				secondary_button.text = _(bindings[1])
+			else:
+				secondary_button.text = _("-")
 
 	def apply_change(self):
 		key = self.last_combination[0]
-		action = self.actions[self.current_index]
 		key_name = self.keyName(key)
+		action = self.actions[self.current_index]
 
 		if not self.key_is_set(key):
 			horizons.globals.fife.set_key_for_action(action, key_name)
@@ -153,11 +182,12 @@ class HotkeyConfiguration(Window):
 				horizons.globals.fife.set_key_for_action(oldaction, oldkey)
 				horizons.globals.fife.set_key_for_action(action, key_name)
 
-
-		horizons.globals.fife.save_settings()
-
 		self.update_buttons_text()
 		self.last_combination = []
+
+	def save_settings(self):
+		horizons.globals.fife.save_settings()
+		self._windows.close()
 
 	def show(self):
 		self.widget.show()
