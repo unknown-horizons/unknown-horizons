unknown-horizons/unknown-horizons

commit 5f97b54df47f8f0c7aa739351166263b39eb78b1

Make test fail in a better way (fail-early for ToggleActive when production to be toggled isn't there)

horizons/command/production.py
@@ -30,7 +30,14 @@ class ToggleActive(GenericComponentCommand):
 	def __call__(self, issuer):
 		# NOTE: special call method, cause production must be saved as id, not as Production obj
 		obj = self._get_object().get_component_by_name(self.component_name)
-		return getattr(obj, self.method)( None if self._production is None else obj._get_production(self._production))
+
+		if self._production is not None:
+			production = obj._get_production(self._production)
+			assert production is not None, str(obj)+" does not have a production "+self._production
+		else:
+			production = None
+
+		return getattr(obj, self.method)( production )
 
 GenericComponentCommand.allow_network(ToggleActive)
 

commit 5bb76cd082ede0b3b43572a4973625f5b37847ed

Fix TypeError on ToggleActive assertion
TypeError: cannot concatenate 'str' and 'int' objects
Introduced with 5f97b54df47f8f0c7aa739351166263b39eb78b1.

horizons/command/production.py
@@ -33,7 +33,7 @@ class ToggleActive(GenericComponentCommand):
 
 		if self._production is not None:
 			production = obj._get_production(self._production)
-			assert production is not None, str(obj)+" does not have a production "+self._production
+			assert production is not None, "%s does not have a production %s" % (obj, self._production)
 		else:
 			production = None
 