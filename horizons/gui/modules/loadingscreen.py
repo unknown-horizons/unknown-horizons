b6e349c57c257dfd6650c549845c4fb211cd52eb
Reiner Gerecke <mr.squidd@gmail.com>
2013-04-08 11:00:11 +0200

    Move quotes to loading screen and make them lazy strings

--- horizons/gui/modules/loadingscreen.py
+++ horizons/gui/modules/loadingscreen.py
@@ -22,11 +22,77 @@
 import random
 
 import horizons.globals
+from horizons.constants import TIER
 from horizons.i18n import _lazy
-from horizons.i18n.quotes import GAMEPLAY_TIPS, FUN_QUOTES
 from horizons.gui.util import load_uh_widget
 from horizons.gui.windows import Window
 from horizons.messaging import LoadingProgress
+
+
+# list of quotes and gameplay tips that are displayed while loading a game
+# NOTE: Try to use not more than 4 lines in a quote/gameplay tip !
+
+FUN_QUOTES = {
+	'name': _lazy("Quotes"),
+	# Fun Quotes should not be translated...
+	'items': [
+		"beer, the cause and solution to all problems of humanity",
+		"trying is the first step t'wards failing. ",
+		"# nobody actually knows how the code below works. ",
+		"here be dragons",
+		"procrastination is the first step towards getting stuff done",
+		"patience is a virtue \n(barra)",
+		"you must really, really love to test \n(portal 2)",
+		"here be bugs",
+		"strength is the capacity to break a chocolate bar into four pieces with your bare hands - and then eat just one of the pieces",
+		"If one does not know to which port one is sailing, no wind is favorable",
+		"The pessimist complains about the wind; \nthe optimist expects it to change; \nthe realist adjusts the sails",
+		"Travel beyond the horizon and discover unknown worlds!",
+		"War... war never changes",
+		"Support Unknown Horizons with Cookies!"
+    ]
+}
+
+
+GAMEPLAY_TIPS = {
+	'name': _lazy("Gameplay Tips"),
+	'items': [
+		_lazy("Press 'ESC' to access Game Menu."),
+		_lazy("Use 'SHIFT' to place multiple buildings."),
+		#TODO: This tip should be removed when all tiers are playable!!
+		#lazyxgettext:python-format
+		_lazy("Currently only the first {tier} tiers are playable.").format(
+				tier=TIER.CURRENT_MAX + 1),
+		_lazy("You can pause the game with 'P'."),
+		_lazy("You can drag roads by holding the left mouse button."),
+		_lazy("You can build multiple buildings by holding the 'SHIFT' key."),
+		_lazy("You can increase the happiness of your inhabitants by lowering the taxes."),
+		_lazy("Build fire stations and doctors to protect your inhabitants from fire and disease."),
+		_lazy("Build storage tents to increase your storage capacity."),
+		_lazy("Make sure every house is in range of a marketplace."),
+		_lazy("Press 'T' to make trees transparent.")
+	]
+}
+
+# This are the options you can select in the Settings what type of quotes should be
+# displayed during load
+# TODO Unfortunately these are not translated
+QUOTES_SETTINGS = (GAMEPLAY_TIPS['name'], FUN_QUOTES['name'], "Mixed")
+
+
+def get_random_quote():
+	quote_type = int(horizons.globals.fife.get_uh_setting("QuotesType"))
+	if quote_type == 2:
+		quote_type = random.randint(0, 1) # choose a random type
+
+	if quote_type == 0:
+		name = GAMEPLAY_TIPS["name"]
+		items = GAMEPLAY_TIPS["items"]
+	elif quote_type == 1:
+		name = FUN_QUOTES["name"]
+		items = FUN_QUOTES["items"]
+
+	return name, random.choice(items)
 
 
 stage_text = {
@@ -61,19 +127,10 @@ class LoadingScreen(Window):
 	def show(self):
 		qotl_type_label = self._widget.findChild(name='qotl_type_label')
 		qotl_label = self._widget.findChild(name='qotl_label')
-		quote_type = int(horizons.globals.fife.get_uh_setting("QuotesType"))
-		if quote_type == 2:
-			quote_type = random.randint(0, 1) # choose a random type
 
-		if quote_type == 0:
-			name = GAMEPLAY_TIPS["name"]
-			items = GAMEPLAY_TIPS["items"]
-		elif quote_type == 1:
-			name = FUN_QUOTES["name"]
-			items = FUN_QUOTES["items"]
-
-		qotl_type_label.text = unicode(name)
-		qotl_label.text = unicode(random.choice(items)) # choose a random quote / gameplay tip
+		name, quote = get_random_quote()
+		qotl_type_label.text = name
+		qotl_label.text = quote
 
 		self._widget.show()
 		LoadingProgress.subscribe(self._update)
