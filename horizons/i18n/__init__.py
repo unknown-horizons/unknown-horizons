b211c90adcf6b3205b35ea6600d5522232381c73
Reiner Gerecke <me@reinergerecke.de>
2013-04-08 11:38:00 +0200

    Merge i18n.utils into i18n

--- horizons/i18n/__init__.py
+++ horizons/i18n/__init__.py
@@ -33,18 +33,55 @@ We will need to make gettext recognize namespaces some time, but hardcoded
 
 import platform
 import gettext
+import glob
 import os
 import logging
 import locale
 
 import horizons.globals
 
-from horizons.constants import LANGUAGENAMES
+from horizons.constants import LANGUAGENAMES, FONTDEFS
 from horizons.ext.speaklater import make_lazy_gettext
-from horizons.i18n.utils import get_fontdef_for_locale, find_available_languages
 from horizons.messaging import LanguageChanged
 
 log = logging.getLogger("i18n")
+
+
+LANGCACHE = {}
+
+def find_available_languages(domain='unknown-horizons', update=False):
+	"""Returns a dict( lang_key -> locale_dir )"""
+	global LANGCACHE
+	if LANGCACHE and not update:
+		return LANGCACHE
+
+	alternatives = ('content/lang',
+	                'build/mo',
+	                '/usr/share/locale',
+	                '/usr/share/games/locale',
+	                '/usr/local/share/locale',
+	                '/usr/local/share/games/locale')
+
+	LANGCACHE = languages = {}
+
+	for i in alternatives:
+		for j in glob('%s/*/*/%s.mo' % (i, domain)):
+			splited = j.split(os.sep)
+			key = splited[-3]
+			if not key in languages:
+				languages[key] = os.sep.join(splited[:-3])
+
+	# there's always a default, which is english
+	languages[LANGUAGENAMES['']] = ''
+	languages['en'] = ''
+
+	return languages
+
+
+def get_fontdef_for_locale(locale):
+	"""Returns path to the fontdef file for a locale. Unifont is default."""
+	fontdef_file = FONTDEFS.get(locale, 'unifont')
+	return os.path.join('content', 'fonts', u'{0}.fontdef'.format(fontdef_file))
 
 
 def change_language(language=None):
