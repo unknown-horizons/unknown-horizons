0fd32d44fe0fc87251637f6e673620e8b295e23d
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-04-12 07:15:07 +0200

    research gui: More mockup work
    Closing and reopening the widget can currently re-layout the second
    research line, if you feel like testing.

--- horizons/gui/modules/research.py
+++ horizons/gui/modules/research.py
@@ -19,21 +19,60 @@
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # ###################################################
 
+from horizons.constants import RESEARCH
 from horizons.gui.util import load_uh_widget
 from horizons.gui.widgets.imagebutton import OkButton
 from horizons.gui.windows import Window
+
+DONE = 'content/gui/images/background/square_120.png'
+
+class ResearchLine(object):
+
+	def __init__(self, research, y_position):
+		super(ResearchLine, self).__init__()
+
+		self._gui = load_uh_widget('research_line.xml')
+		self._gui.position = (self._gui.position[0], self._gui.position[1] + 150 * y_position)
+		self.left_bg = self._gui.findChild(name='left_bg')
+		self.right_bg = self._gui.findChild(name='right_bg')
+
+	def researched(self, first=True, second=False):
+		if first:
+			self.left_bg.image = DONE
+		if second:
+			self.right_bg.image = DONE
 
 
 class ResearchDialog(Window):
 	def __init__(self, windows, session=None):
 		super(ResearchDialog, self).__init__(windows)
 
+		self.research_options = {}
 		self._session = session
+
+	def _init(self):
 		self._gui = load_uh_widget('research.xml')
+
+		for i, research in enumerate(RESEARCH.THINGS):
+			line = ResearchLine(research, i)
+			self.research_options[i] = line
+			self._gui.addChild(line._gui)
+
+		tf = [True, False]
+		self._session.random.shuffle(tf)
+
+		self.researched(0, [True, True])
+		self.researched(1, tf)
+		self.researched(2, [False, False])
 
 		self._gui.findChild(name=OkButton.DEFAULT_NAME).capture(self._windows.close)
 
+	def researched(self, index, todo=None):
+		self.research_options[index].researched(*todo)
+
 	def show(self):
+		self._init()
+
 		self._gui.show()
 
 	def hide(self):
