4cde0e5a40f8b16deba0992f3d34830af985808a
ThePawnBreak <mocanu.cristian93@gmail.com>
2013-05-07 12:56:54 +0300

    Fix size of button changing after showing popup.

--- horizons/gui/modules/hotkeys_settings.py
+++ horizons/gui/modules/hotkeys_settings.py
@@ -92,7 +92,8 @@ class HotkeyConfiguration(object):
 		#xgettext:python-format
 		button = Button()
 		button.name = str(index)
-		button.max_size = (130,18)
+		button.max_size = (100,18)
+		button.min_size = (100,18)
 		return button
 
 	def _detect_click_on_button(self, button, column):
