4d5a69750df4e211e5f4542a4949f1420f4527b5
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-05-09 15:05:42 +0200

    hotkeys: Add missing space

--- horizons/gui/modules/hotkeys_settings.py
+++ horizons/gui/modules/hotkeys_settings.py
@@ -139,7 +139,7 @@ class HotkeyConfiguration(object):
 
 			#xgettext:python-format
 			message = _("{key} is already set to {action}.").format(key=key_name, action=oldaction)
-			message += _("Would you like to overwrite it?")
+			message += u" " + _("Would you like to overwrite it?")
 			confirmed = horizons.main._modules.gui.show_popup(_("Confirmation for overwriting"), message, show_cancel_button=True)
 			if confirmed:
 				horizons.globals.fife.replace_key_for_action(oldaction, key_name, "UNASSIGNED")
