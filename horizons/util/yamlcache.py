8b394987fb3e1fb83cfb0c121c9dcba6b0d4f8b8
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-05-09 22:28:46 +0200

    Code flow

--- horizons/util/yamlcache.py
+++ horizons/util/yamlcache.py
@@ -47,18 +47,20 @@ def parse_token(token, token_klass):
 	"""
 	classes = {'TIER': TIER, 'RES': RES, 'UNITS': UNITS, 'BUILDINGS': BUILDINGS}
 
-	if isinstance(token, unicode):
-		if token.startswith(token_klass):
-			try:
-				return getattr( classes[token_klass], token.split(".", 2)[1])
-			except AttributeError as e: # token not defined here
-				err = "This means that you either have to add an entry in horizons/constants.py in the class %s for %s,\nor %s is actually a typo." % (token_klass, token, token)
-				raise Exception( str(e) + "\n\n" + err +"\n" )
+	if not isinstance(token, basestring):
+		# Probably numeric already
+		return token
+	if not token.startswith(token_klass):
+		# No need to parse anything
+		return token
+	try:
+		return getattr( classes[token_klass], token.split(".", 2)[1])
+	except AttributeError as e: # token not defined here
+		err = ("This means that you either have to add an entry in horizons/constants.py "
+		       "in the class %s for %s,\nor %s is actually a typo."
+		       % (token_klass, token, token))
+		raise Exception( str(e) + "\n\n" + err +"\n" )
 
-		else:
-			return token
-	else:
-		return token # probably numeric already
 
 def convert_game_data(data):
 	"""Translates convenience symbols into actual game data usable by machines"""
