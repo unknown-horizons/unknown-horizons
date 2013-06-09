e3f573e471882925afc86dd76625aeeb11214541
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-06-09 20:03:24 +0200

    Display gameplay tips in help screen (#2098)

--- horizons/gui/modules/help.py
+++ horizons/gui/modules/help.py
@@ -19,7 +19,10 @@
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # ###################################################
 
+from fife.extensions.pychan.widgets import Label
+
 from horizons.command.game import PauseCommand, UnPauseCommand
+from horizons.gui.modules.loadingscreen import GAMEPLAY_TIPS
 from horizons.gui.util import load_uh_widget
 from horizons.gui.widgets.imagebutton import OkButton
 from horizons.gui.windows import Window
@@ -33,6 +36,12 @@ class HelpDialog(Window):
 		self._session = session
 		self.widget = load_uh_widget('help.xml')
 		self.widget.findChild(name=OkButton.DEFAULT_NAME).capture(self._windows.close)
+		self.widget.findChild(name='headline').text = GAMEPLAY_TIPS['name']
+		tip_box = self.widget.findChild(name='tip_box')
+		size = {'max_size': (300, 60), 'min_size': (300, 20)}
+		for tip in GAMEPLAY_TIPS['items']:
+			tip_label = Label(text=unicode(tip), wrap_text=True, **size)
+			tip_box.addChild(tip_label)
 
 	def show(self):
 		self.widget.show()
