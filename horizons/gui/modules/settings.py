18c66948ef0e951d7e65702511993b73ba5fd7fe
Kilian <kiliankoeppchen@googlemail.com>
2013-08-20 17:27:54 +0200

    Fix clb for playsound and add 2 for volume setting

--- horizons/gui/modules/settings.py
+++ horizons/gui/modules/settings.py
@@ -205,9 +205,24 @@ class SettingsDialog(PickBeltWidget, Window):
 			            "and has serious graphical glitches. \n\nUse at own risk!")
 			self._windows.show_popup(headline, message)
 
-	def _on_PlaySounds_changed(seld, old, new):
+	def _on_PlaySounds_changed(self, old, new):
 		if new == True:
 			horizons.globals.fife.sound.enable_sound()
+		else:
+			horizons.globals.fife.sound.disable_sound()
+
+	def _on_VolumeMuisc_changed(self, old, new):
+		if new != old:
+			if self._settings.get(UH_MODULE, 'PlaySounds') == True:
+				horizons.globals.fife.sound.emitter['bgsound'].setGain(new)
+
+	def _on_VolumeEffects_changed(self, old, new):
+		if new != old:
+			if self._settings.get(UH_MODULE, 'PlaySounds') == True:
+				horizons.globals.fife.sound.emitter['effects'].setGain(new)
+				horizons.globals.fife.sound.emitter['speech'].setGain(new)
+				for emitter in horizons.globals.fife.sound.emitter['ambient']:
+					emitter.setGain(value*2)
 
 def get_screen_resolutions(selected_default):
 	"""Create an instance of fife.DeviceCaps and compile a list of possible resolutions.
