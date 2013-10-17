8b3ec8a4cc4e53233f6b80cdb60712f4178f3b46
phaidon <admin@admins-MacBook-Air.local>
2013-10-17 20:39:37 +0200

    Mac Os X app container path fix.

--- run_uh.py
+++ run_uh.py
@@ -189,6 +189,9 @@ def get_content_dir_parent_path():
 	options = []
 	# Try the directory this file is in. This should work in most cases.
 	options.append(os.path.dirname(os.path.realpath(unicode(__file__))))
+	# Try path for Mac Os X app container (Unknown Horizons.app).
+	# Unknown Horizons.app/Contents/Resources/contents
+	options.append(os.path.join(os.getcwd()))
 	# Try often-used paths on Linux.
 	for path in ('/usr/share/games', '/usr/share', '/usr/local/share/games', '/usr/local/share'):
 		options.append(os.path.join(path, u'unknown-horizons'))
