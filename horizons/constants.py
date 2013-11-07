a0d96fdef95d489ba9b9aea4b7e1b7aeee67b89f
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-11-07 21:54:38 +0100

    Rename fertility mappings

--- horizons/constants.py
+++ horizons/constants.py
@@ -311,7 +311,7 @@ class CLIMATE:
 	EXTREME_RATIO = 5
 
 class FERTILITY:
-	MAPPING = {
+	RES_TO_BUILDING = {
 #?		do we want to restrict livestock too?
 	##	needs different codepath (resource deposits)
 
@@ -330,7 +330,7 @@ class FERTILITY:
 		RES.VINES:          BUILDINGS.VINEYARD,
 		RES.ALVEARIES:      BUILDINGS.ALVEARIES,
 	}
-	INVERSE_MAPPING = dict((v,k) for k, v in MAPPING.iteritems())
+	BUILDING_TO_RES = dict((v,k) for k, v in RES_TO_BUILDING.iteritems())
 
 class GROUND:
 	DEFAULT_LAND = (3, "straight", 45)
