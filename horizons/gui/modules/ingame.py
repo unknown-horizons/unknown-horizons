e03c2f0731ab90415c6014fea8c60d0e6220e448
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-11-12 14:45:58 +0100

    Remove fertility display from cityinfo
    This will be an own widget soon.

--- horizons/gui/modules/ingame.py
+++ horizons/gui/modules/ingame.py
@@ -164,14 +164,6 @@ class CityInfo(object):
 			helptext = u""
 			city_name_label.disable_cursor_change_on_hover()
 
-		fertility_box = self._child_finder('fertility_icons')
-		fertility_box.removeAllChildren()
-		fertility_box.padding = 3
-		for res_id in sorted(self._settlement.fertility):
-			icon = create_resource_icon(res_id, self._ingame_gui.session.db)
-			icon.max_size = icon.min_size = icon.size = (20, 20)
-			fertility_box.addChild(icon)
-
 		self._widget.mapEvents({
 			'city_name': cb
 		})
