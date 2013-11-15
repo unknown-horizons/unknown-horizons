cfb789449be6f0a5ebdd2c93474ef0ef9e00a029
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-11-15 01:07:44 +0100

    Missing function docstring

--- horizons/gui/tabs/__init__.py
+++ horizons/gui/tabs/__init__.py
@@ -46,4 +46,5 @@ from diplomacytab import DiplomacyTab
 from selectmultitab import SelectMultiTab
 
 def resolve_tab(tabclass_name):
+	"""Converts a string like 'DiplomacyTab' to the respective class DiplomacyTab."""
 	return getattr(sys.modules[__name__], tabclass_name)
