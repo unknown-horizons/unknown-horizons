7247716a22a53d678a54f96b3e413213b99c682e
Thomas Kinnen <thomas.kinnen@gmail.com>
2013-11-18 22:33:36 +0100

    fixes #2200 - Add brushes of size 4 and 5

--- horizons/editor/gui.py
+++ horizons/editor/gui.py
@@ -207,7 +207,7 @@ class SettingsTab(TabInterface):
 		self._world_editor = world_editor
 
 		# Brush size
-		for i in range(1, 4):
+		for i in range(1, 6):
 			b = self.widget.findChild(name='size_%d' % i)
 			b.capture(Callback(self._change_brush_size, i))
 
