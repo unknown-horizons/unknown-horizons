0a322bfbd7eca1f68f3a06e183ec2c82eb09fbc0
UnknownScribe <daniel.stephens02@gmail.com>
2014-01-26 23:52:49 +0000

    Fix settlement-range test.

--- tests/game/test_settlement_range.py
+++ tests/game/test_settlement_range.py
@@ -22,46 +22,35 @@
 
 from horizons.constants import RES, BUILDINGS
 from horizons.command.building import Build, Tear
-from horizons.component.storagecomponent import StorageComponent
 
 from tests.game import game_test
-
-
+		
 # FIXTURE is settlement with a lookout, some tents and some trees
-
 @game_test(use_fixture='settlement-range')
 def test_settlement_decrease(s):
 	"""
 	Check if destroying a lookout destroys surrounding buildings but not trees.
 	"""
 	settlement = s.world.player.settlements[0]
-
 	lo = settlement.buildings_by_id[ BUILDINGS.LOOKOUT ][0]
 	pos = lo.position.origin
 	owner = lo.owner
 	island = lo.island
-	
+
 	starting_tents = settlement.buildings_by_id[ BUILDINGS.RESIDENTIAL ]
 	old_tents = len(starting_tents)
-	
-	starting_trees = settlement.buildings_by_id[ BUILDINGS.TREE ]
-	old_trees = len(starting_trees)
-	
+
+	old_trees_owned = len(settlement.buildings_by_id[ BUILDINGS.TREE ])
+	old_trees = island.num_trees
+
 	Tear(lo)(owner)
-	
+
 	cur_tents = settlement.buildings_by_id[ BUILDINGS.RESIDENTIAL ]
 	new_tents = len(cur_tents)
-	
-	cur_trees = settlement.buildings_by_id[ BUILDINGS.TREE ]
-	new_trees = len(cur_trees)
-	
+
+	new_trees_owned = len(settlement.buildings_by_id[ BUILDINGS.TREE ])
+	new_trees = island.num_trees
+
 	assert new_trees == old_trees
+	assert old_trees_owned > new_trees_owned
 	assert new_tents < old_tents
-	
-	trees_owned = True
-	for tree in cur_trees:
-		if tree.owner is not owner:
-			trees_owned = False
-			break
-	
-	assert trees_owned == False
