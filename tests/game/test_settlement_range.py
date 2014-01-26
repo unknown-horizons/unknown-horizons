040150288a5495b465a6298dc65ec399b4fd2b2e
UnknownScribe <daniel.stephens02@gmail.com>
2014-01-26 22:43:58 +0000

    Fix the settlement-range decrease test.

--- tests/game/test_settlement_range.py
+++ tests/game/test_settlement_range.py
@@ -19,50 +19,72 @@
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # ###################################################
 
-
 from horizons.constants import RES, BUILDINGS
 from horizons.command.building import Build, Tear
-from horizons.component.storagecomponent import StorageComponent
 
 from tests.game import game_test
 
-
 # FIXTURE is settlement with a lookout, some tents and some trees
-
 @game_test(use_fixture='settlement-range')
 def test_settlement_decrease(s):
 	"""
 	Check if destroying a lookout destroys surrounding buildings but not trees.
 	"""
 	settlement = s.world.player.settlements[0]
-
 	lo = settlement.buildings_by_id[ BUILDINGS.LOOKOUT ][0]
 	pos = lo.position.origin
 	owner = lo.owner
 	island = lo.island
 	
-	starting_tents = settlement.buildings_by_id[ BUILDINGS.RESIDENTIAL ]
-	old_tents = len(starting_tents)
-	
-	starting_trees = settlement.buildings_by_id[ BUILDINGS.TREE ]
-	old_trees = len(starting_trees)
-	
+	old_tents = []
+	for coords in island.ground_map:
+		tile = island.ground_map[coords]
+		building = tile.object
+		if building is None or building.id is not BUILDINGS.RESIDENTIAL:
+			continue
+		if building in old_tents:
+			continue
+		old_tents.append(building)
+
+	old_trees = []
+	for coords in island.ground_map:
+		tile = island.ground_map[coords]
+		building = tile.object
+		if building is None or building.id is not BUILDINGS.TREE:
+			continue
+		if building in old_trees:
+			continue
+		old_trees.append(building)
+
 	Tear(lo)(owner)
 	
-	cur_tents = settlement.buildings_by_id[ BUILDINGS.RESIDENTIAL ]
-	new_tents = len(cur_tents)
-	
-	cur_trees = settlement.buildings_by_id[ BUILDINGS.TREE ]
-	new_trees = len(cur_trees)
-	
-	assert new_trees == old_trees
-	assert new_tents < old_tents
-	
+	new_tents = []
+	for coords in island.ground_map:
+		tile = island.ground_map[coords]
+		building = tile.object
+		if building is None or building.id is not BUILDINGS.RESIDENTIAL:
+			continue
+		if building in new_tents:
+			continue
+		new_tents.append(building)
+
+	new_trees = []
+	for coords in island.ground_map:
+		tile = island.ground_map[coords]
+		building = tile.object
+		if building is None or building.id is not BUILDINGS.TREE:
+			continue
+		if building in new_trees:
+			continue
+		new_trees.append(building)
+
+	assert len(old_tents) > len(new_tents)
+	assert len(new_trees) == len(old_trees)
+
 	trees_owned = True
-	for tree in cur_trees:
+	for tree in new_trees:
 		if tree.owner is not owner:
 			trees_owned = False
 			break
-	
+
 	assert trees_owned == False
-	
