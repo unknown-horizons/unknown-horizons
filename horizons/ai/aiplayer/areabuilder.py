68d23756dc94c48bdbeb9109ed03138a1241bfe9
Thomas Kinnen <thomas.kinnen@gmail.com>
2014-03-05 21:57:48 +0100

    Fixes #2260 * Fix AI trying to enlarge settlement with tents * Fix AI trying to build trees outside the settlement range

--- horizons/ai/aiplayer/areabuilder.py
+++ horizons/ai/aiplayer/areabuilder.py
@@ -252,11 +252,8 @@ class AreaBuilder(WorldObject):
 		return BUILD_RESULT.OK
 
 	def extend_settlement(self, position):
-		"""Build a tent or a storage to extend the settlement towards the given position. Return a BUILD_RESULT constant."""
-		result = self.settlement_manager.village_builder.extend_settlement_with_tent(position)
-		if result != BUILD_RESULT.OK:
-			result = self.settlement_manager.production_builder.extend_settlement_with_storage(position)
-		return result
+		"""Build a storage to extend the settlement towards the given position. Return a BUILD_RESULT constant."""
+		return self.settlement_manager.production_builder.extend_settlement_with_storage(position)
 
 	def handle_lost_area(self, coords_list):
 		"""Handle losing the potential land in the given coordinates list."""
