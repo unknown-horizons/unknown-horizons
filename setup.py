c2676b68d7220f1718d8991f3e561c931263b5d5
Thomas Kowaliczek <linuxdonald@posteo.de>
2016-01-06 14:37:34 +0100

    change to python2

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # ###################################################
 # Copyright (C) 2008-2014 The Unknown Horizons Team
 # team@unknown-horizons.org
