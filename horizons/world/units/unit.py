95aa7410c4b1b9f76eefbafbf9b5fb0d9a0293fd
Kilian Köppchen <kiliankoeppchen@googlemail.com>
2016-01-08 21:32:21 +0100

    Add go command ability to basic unit class

--- horizons/world/units/unit.py
+++ horizons/world/units/unit.py
@@ -30,6 +30,7 @@ from horizons.util.python.weakmethod import WeakMethod
 from horizons.util.shapes import Point
 from horizons.util.worldobject import WorldObject
 from horizons.constants import LAYERS
+from horizons.component.commandablecomponent import CommandableComponent
 from horizons.component.healthcomponent import HealthComponent
 from horizons.extscheduler import ExtScheduler
 from horizons.world.resourcehandler import ResourceTransferHandler
@@ -222,6 +223,9 @@ class Unit(MovingObject, ResourceTransferHandler):
 			if path:
 				return (possible_target, path)
 		return (None, None)
+
+	def go(self, x, y):
+		self.get_component(CommandableComponent).go(x, y)
 
 	@property
 	def classname(self):
