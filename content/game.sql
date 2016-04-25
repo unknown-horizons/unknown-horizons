43183ccc80a7f1ce6754b6da317b0880f25cdf2d
Thomas Kowaliczek <linuxdonald@localhost.localdomain>
2016-04-25 16:59:33 +0200

    Forget to remove the non free markt sound file

--- content/game.sql
+++ content/game.sql
@@ -62,7 +62,7 @@ INSERT INTO "sounds" VALUES(12, 'content/audio/sounds/refresh.ogg');
 INSERT INTO "sounds" VALUES(13, 'content/audio/sounds/click.ogg');
 INSERT INTO "sounds" VALUES(14, 'content/audio/sounds/cow_1.ogg');
 INSERT INTO "sounds" VALUES(15, 'content/audio/sounds/smith.ogg');
-INSERT INTO "sounds" VALUES(16, 'content/audio/sounds/market.ogg');
+INSERT INTO "sounds" VALUES(16, 'content/audio/sounds/main_square.ogg');
 INSERT INTO "sounds" VALUES(17, 'content/audio/sounds/windmill.ogg');
 
 CREATE TABLE "sounds_special" (
