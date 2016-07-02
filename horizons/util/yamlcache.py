a8418596136e3d01d96ba34d109031bf23389cdc
Markus Hackspacher <hackspacher@gmx.de>
2016-07-02 10:10:11 +0200

    use `.format()` function and staticmethod

--- horizons/util/yamlcache.py
+++ horizons/util/yamlcache.py
@@ -55,15 +55,16 @@ def parse_token(token, token_klass):
 		return token
 	try:
 		return getattr( classes[token_klass], token.split(".", 2)[1])
-	except AttributeError as e: # token not defined here
+	except AttributeError as e:  # token not defined here
 		err = ("This means that you either have to add an entry in horizons/constants.py "
-		       "in the class %s for %s,\nor %s is actually a typo."
-		       % (token_klass, token, token))
+		       "in the class {0!s} for {1!s},\nor {2!s} is actually a typo.".
+		       format(token_klass, token, token))
 		raise Exception( str(e) + "\n\n" + err +"\n" )
 
 
 def convert_game_data(data):
-	"""Translates convenience symbols into actual game data usable by machines"""
+	"""Translates convenience symbols into actual game data usable by machines
+	"""
 	if isinstance(data, dict):
 		return dict( [ convert_game_data(i) for i in data.iteritems() ] )
 	elif isinstance(data, (tuple, list)):
@@ -80,7 +81,8 @@ class YamlCache(object):
 	"""Loads and caches YAML files in a persistent cache.
 	Threadsafe.
 
-	Use get_file for files to cache (default case) or load_yaml_data for special use cases (behaves like yaml.load).
+	Use get_file for files to cache (default case) or load_yaml_data
+	for special use cases (behaves like yaml.load).
 	"""
 
 	cache = None
@@ -92,16 +94,18 @@ class YamlCache(object):
 
 	log = logging.getLogger("yamlcache")
 
-	@classmethod
-	def load_yaml_data(self, string_or_stream):
-		"""Use this instead of yaml.load everywhere in uh in case get_file isn't useable"""
-		return yaml.load( string_or_stream, Loader=SafeLoader )
+	@staticmethod
+	def load_yaml_data(string_or_stream):
+		"""Use this instead of yaml.load everywhere in uh in case get_file
+		isn't useable"""
+		return yaml.load(string_or_stream, Loader=SafeLoader)
 
 	@classmethod
 	def get_file(cls, filename, game_data=False):
 		"""Get contents of a yaml file
 		@param filename: path to the file
-		@param game_data: Whether this file contains data like BUILDINGS.LUMBERJACK to resolve
+		@param game_data: Whether this file contains data like
+		                  BUILDINGS.LUMBERJACK to resolve
 		"""
 		with open(filename, 'r') as f:
 			filedata = f.read()
@@ -116,12 +120,12 @@ class YamlCache(object):
 		yaml_file_in_cache = (filename in cls.cache and cls.cache[filename][0] == h)
 		if not yaml_file_in_cache:
 			data = cls.load_yaml_data(filedata)
-			if game_data: # need to convert some values
+			if game_data:  # need to convert some values
 				try:
 					data = convert_game_data(data)
 				except Exception as e:
 					# add info about file
-					to_add = "\nThis error happened in %s ." % filename
+					to_add = "\nThis error happened in {0!s} .".format(filename)
 					e.args = ( e.args[0] + to_add, ) + e.args[1:]
 					e.message = ( e.message + to_add )
 					raise
@@ -134,7 +138,7 @@ class YamlCache(object):
 				ExtScheduler().add_new_object(cls._do_sync, cls, run_in=1)
 			cls.lock.release()
 
-		return cls.cache[filename][1] # returns an object from the YAML
+		return cls.cache[filename][1]  # returns an object from the YAML
 
 	@classmethod
 	def _open_cache(cls):
@@ -144,7 +148,8 @@ class YamlCache(object):
 
 	@classmethod
 	def _do_sync(cls):
-		"""Only write to disc once in a while, it's too slow when done every time"""
+		"""Only write to disc once in a while, it's too slow when done
+		every time"""
 		cls.lock.acquire()
 		cls.sync_scheduled = False
 		cls.cache.sync()
