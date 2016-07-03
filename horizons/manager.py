733f3f5a4b1585d72252a04b88abdf9c3790b977
Markus Hackspacher <hackspacher@gmx.de>
2016-07-03 11:35:37 +0200

    classmethod should be called `cls` and not 'self'

--- horizons/manager.py
+++ horizons/manager.py
@@ -303,7 +303,7 @@ class MPPacket(object):
 		self.player_id = player_id
 
 	@classmethod
-	def allow_network(self, klass):
+	def allow_network(cls, klass):
 		"""
 		NOTE: this is a security related method and may lead to
 		execution of arbritary code if used in a wrong way
