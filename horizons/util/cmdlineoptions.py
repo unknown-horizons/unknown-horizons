afa36c7f803db196a2642286a5c17b889a44e2f8
Thomas Kowaliczek <linuxdonald@posteo.de>
2016-08-14 12:23:46 +0200

    Update Copyright year in some files to 2016

--- horizons/util/cmdlineoptions.py
+++ horizons/util/cmdlineoptions.py
@@ -227,7 +227,7 @@ The bugtracker can be found at \fBhttp://bugs.unknown-horizons.org\fR\&.
 .RE
 .SH "COPYRIGHT"
 .br
-Copyright \(co 2008-2014 The Unknown Horizons Team
+Copyright \(co 2008-2016 The Unknown Horizons Team
 .br
 .PP
 Permission is granted to copy, distribute and/or modify this document under the
