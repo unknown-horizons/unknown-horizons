27873625d9f6ae6953ec90bb832de0e3eb910a14
MarkusHackspacher <hackspacher@gmx.de>
2016-10-12 21:39:32 +0200

    cls.class_mapping[key] == component or key == component:

--- horizons/component/componentholder.py
+++ horizons/component/componentholder.py
@@ -180,6 +180,6 @@ class ComponentHolder(object):
 		for entry in cls.component_templates:
 			if isinstance(entry, dict):
 				for key, value in entry.iteritems():
-					if component in [cls.class_mapping[key], key]:
+					if cls.class_mapping[key] == component or key == component:
 						return value
 		raise KeyError("This class does not contain a component with name: {0}".format(component))
