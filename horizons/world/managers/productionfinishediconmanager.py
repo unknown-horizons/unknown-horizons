0cade60e7bc1e6029d94813084e0fa196fae6a33
Reiner Gerecke <me@reinergerecke.de>
2016-10-13 19:51:16 +0200

    Make render name of icons more unique to prevent crash
    Fixes #2475

--- horizons/world/managers/productionfinishediconmanager.py
+++ horizons/world/managers/productionfinishediconmanager.py
@@ -19,6 +19,8 @@
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # ###################################################
 
+import uuid
+
 from fife import fife
 
 import horizons.globals
@@ -152,4 +154,4 @@ class ProductionFinishedIconManager(object):
 		This key MUST be unique!
 		"""
 		return "produced_resource_" + str(res) + "_" + str(instance.position.origin)\
-		       + "_" + str(Scheduler().cur_tick)
+		       + "_" + str(uuid.uuid1())
