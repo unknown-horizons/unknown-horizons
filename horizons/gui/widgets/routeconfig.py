ef745c1960244556b892017e9c11f3a71b6f0e4b
Reiner Gerecke <me@reinergerecke.de>
2016-10-22 10:36:33 +0200

    Lazy load RouteConfig gui only when necessary
    When creating a new RouteConfig instance, the code would check whether
    it needed to call the CreateRoute command and only then would it
    initialize the gui.
    
    However this is only necessary, when the instance has no preconfigured
    route to begin with. But it does when loading a savegame. In this case,
    we can initialize the gui right away.

--- horizons/gui/widgets/routeconfig.py
+++ horizons/gui/widgets/routeconfig.py
@@ -60,8 +60,10 @@ class RouteConfig(Window):
 		if not hasattr(instance, 'route'):
 			CreateRoute(instance).execute(self.session)
 
-		# We must make sure that the createRoute command has successfully finished, even in network games.
-		Scheduler().add_new_object(self._init_gui, self, run_in=MPManager.EXECUTIONDELAY+2)
+			# We must make sure that the createRoute command has successfully finished, even in network games.
+			Scheduler().add_new_object(self._init_gui, self, run_in=MPManager.EXECUTIONDELAY+2)
+		else:
+			self._init_gui()
 
 	@property
 	def session(self):
