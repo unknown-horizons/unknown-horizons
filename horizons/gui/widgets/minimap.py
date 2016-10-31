unknown-horizons/unknown-horizons

commit 70de4b30a9065522b783e06d51399b297eb0105c

Avoid overwriting on_click method in Minimap class
When creating a new Minimap instance, an alternative on_click handler
can be passed. Previously that would overwrite the one defined on the
class. mypy complained about this, rightly so.

horizons/gui/widgets/minimap.py
@@ -186,8 +186,7 @@ class Minimap(object):
 		self.rotation = 0
 		self.fixed_tooltip = tooltip
 
-		if on_click is not None:
-			self.on_click = on_click
+		self.optional_on_click = on_click
 
 		self.cam_border = cam_border
 		self.use_rotation = use_rotation
@@ -352,7 +351,7 @@ class Minimap(object):
 			icon.name + '/mouseExited' : self._mouse_exited,
 		})
 
-	def on_click(self, event, drag):
+	def default_on_click(self, event, drag):
 		"""Handler for clicks (pressed and dragged)
 		Scrolls screen to the point, where the cursor points to on the minimap.
 		Overwrite this method to your convenience.
@@ -374,12 +373,17 @@ class Minimap(object):
 				self.view.center(*map_coords)
 
 	def _on_click(self, event):
+		if self.optional_on_click:
+			click_handler = self.optional_on_click
+		else:
+			click_handler = self.default_on_click
+
 		if self.world is not None: # supply world coords if there is a world
 			event.map_coords = self._get_event_coords(event)
 			if event.map_coords:
-				self.on_click(event, drag=False)
+				click_handler(event, drag=False)
 		else:
-			self.on_click(event, drag=True)
+			click_handler(event, drag=True)
 
 	def _on_drag(self, event):
 		if self.world is not None: # supply world coords if there is a world

commit 234848c220cafc8df14183360d9ca4472062b8e1

Fix crash when dragging on minimap
Regression introduced in 70de4b30a9065522b783e06d51399b297eb0105c.

Fixes: #2549

horizons/gui/widgets/minimap.py
@@ -186,7 +186,7 @@ class Minimap(object):
 		self.rotation = 0
 		self.fixed_tooltip = tooltip
 
-		self.optional_on_click = on_click
+		self.click_handler = on_click if on_click is not None else self.default_on_click
 
 		self.cam_border = cam_border
 		self.use_rotation = use_rotation
@@ -373,25 +373,20 @@ class Minimap(object):
 				self.view.center(*map_coords)
 
 	def _on_click(self, event):
-		if self.optional_on_click:
-			click_handler = self.optional_on_click
-		else:
-			click_handler = self.default_on_click
-
 		if self.world is not None: # supply world coords if there is a world
 			event.map_coords = self._get_event_coords(event)
 			if event.map_coords:
-				click_handler(event, drag=False)
+				self.click_handler(event, drag=False)
 		else:
-			click_handler(event, drag=True)
+			self.click_handler(event, drag=True)
 
 	def _on_drag(self, event):
 		if self.world is not None: # supply world coords if there is a world
 			event.map_coords = self._get_event_coords(event)
 			if event.map_coords:
-				self.on_click(event, drag=True)
+				self.click_handler(event, drag=True)
 		else:
-			self.on_click(event, drag=True)
+			self.click_handler(event, drag=True)
 
 	def _get_event_coords(self, event):
 		"""Returns position of event as uh map coordinate tuple or None"""