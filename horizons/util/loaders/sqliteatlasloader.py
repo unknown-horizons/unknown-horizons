unknown-horizons/unknown-horizons

commit ead8169a24242cb3bb838e5ae4741de9b817dc97

Adapt loading of animations to current fife interface
Our engine fife changed its interface, so
`fife.Animation.createAnimation()` was removed in favour of using the
animationmanager. This animationmanager can hold on to already created
animations and return them. Thus, instead of always creating new
animations, we can just ask the manager for a reference to an already
created Animation instead.

horizons/util/loaders/sqliteatlasloader.py
@@ -72,11 +72,16 @@ class SQLiteAtlasLoader(object):
 		actionset, action, rotation = id.split('+')
 		commands = zip(commands[0::2], commands[1::2])
 
-		ani = fife.Animation.createAnimation()
+		animationmanager = horizons.globals.fife.animationmanager
+
+		# if we've loaded that animation before, we can finish early
+		if animationmanager.exits(id):
+			return animationmanager.getPtr(id)
+
+		ani = animationmanager.create(id)
 
 		# Set the correct loader based on the actionset
 		loader = self._get_loader(actionset)
-
 
 		frame_start, frame_end = 0.0, 0.0
 		for file in sorted(loader.get_sets()[actionset][action][int(rotation)].iterkeys()):

horizons/component/coloroverlaycomponent.py
@@ -155,18 +155,23 @@ class ColorOverlayComponent(Component):
 				overlay_name, self.instance, self.identifier)
 			return
 
+		animationmanager = horizons.globals.fife.animationmanager
 		self.current_overlays[z_order] = overlay_set
 		for rotation, frames in overlay_set.iteritems():
-			ov_anim = fife.Animation.createAnimation()
-			for frame_img, frame_data in frames.iteritems():
-				try:
-					frame_length = frame_data[0]
-				except TypeError:
-					# not using atlases
-					frame_length = frame_data
-				pic = horizons.globals.fife.animationloader.load_image(frame_img, self.action_set, overlay_name, rotation)
-				frame_milliseconds = int(frame_length * 1000)
-				ov_anim.addFrame(pic, frame_milliseconds)
+			id = '{}+{}'.format(self.identifier, rotation)
+			if animationmanager.exists(id):
+				ov_anim = animationmanager.getPtr(id)
+			else:
+				ov_anim = animationmanager.create(id)
+				for frame_img, frame_data in frames.iteritems():
+					try:
+						frame_length = frame_data[0]
+					except TypeError:
+						# not using atlases
+						frame_length = frame_data
+					pic = horizons.globals.fife.animationloader.load_image(frame_img, self.action_set, overlay_name, rotation)
+					frame_milliseconds = int(frame_length * 1000)
+					ov_anim.addFrame(pic, frame_milliseconds)
 			overlay = fife.OverlayColors(ov_anim)
 			self.fife_instance.addColorOverlay(self.identifier, rotation, z_order, overlay)
 

horizons/component/inventoryoverlaycomponent.py
@@ -22,8 +22,6 @@
 import logging
 from collections import defaultdict
 
-from fife import fife
-
 import horizons.globals
 from horizons.component import Component
 from horizons.messaging import InstanceInventoryUpdated
@@ -75,17 +73,22 @@ class InventoryOverlayComponent(Component):
 			# parameter True: also convert color overlays attached to base frame(s) into animation
 			self.fife_instance.convertToOverlays(self.identifier, True)
 
+		animationmanager = horizons.globals.fife.animationmanager
 		for rotation, frames in overlay_set.iteritems():
-			ov_anim = fife.Animation.createAnimation()
-			for frame_img, frame_data in frames.iteritems():
-				try:
-					frame_length = frame_data[0]
-				except TypeError:
-					# not using atlases
-					frame_length = frame_data
-				pic = horizons.globals.fife.imagemanager.load(frame_img)
-				frame_milliseconds = int(frame_length * 1000)
-				ov_anim.addFrame(pic, frame_milliseconds)
+			id = '{}+{}'.format(self.identifier, rotation)
+			if animationmanager.exists(id):
+				ov_anim = animationmanager.getPtr(id)
+			else:
+				ov_anim = animationmanager.create(id)
+				for frame_img, frame_data in frames.iteritems():
+					try:
+						frame_length = frame_data[0]
+					except TypeError:
+						# not using atlases
+						frame_length = frame_data
+					pic = horizons.globals.fife.imagemanager.load(frame_img)
+					frame_milliseconds = int(frame_length * 1000)
+					ov_anim.addFrame(pic, frame_milliseconds)
 			self.fife_instance.addAnimationOverlay(self.identifier, rotation, z_order, ov_anim)
 
 

horizons/engine/engine.py
@@ -155,6 +155,7 @@ class Fife(object):
 		self.eventmanager = self.engine.getEventManager()
 		self.sound = Sound(self)
 		self.imagemanager = self.engine.getImageManager()
+		self.animationmanager = self.engine.getAnimationManager()
 		self.targetrenderer = self.engine.getTargetRenderer()
 		self.animationloader = None
 

horizons/util/loaders/sqliteanimationloader.py
@@ -19,8 +19,6 @@
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # ###################################################
 
-from fife import fife
-
 import horizons.globals
 from horizons.util.loaders.actionsetloader import ActionSetLoader
 from horizons.util.loaders.tilesetloader import TileSetLoader
@@ -49,11 +47,16 @@ class SQLiteAnimationLoader(object):
 		actionset, action, rotation = id.split('+')
 		commands = zip(commands[0::2], commands[1::2])
 
+		animationmanager = horizons.globals.fife.animationmanager
+
+		# if we've loaded that animation before, we can finish early
+		if animationmanager.exists(id):
+			return animationmanager.getPtr(id)
+
 		# Set the correct loader based on the actionset
-		loader = None
 		loader = self._get_loader(actionset)
 
-		ani = fife.Animation.createAnimation()
+		ani = animationmanager.create(id)
 		frame_start, frame_end = 0.0, 0.0
 		for file in sorted(loader.get_sets()[actionset][action][int(rotation)].iterkeys()):
 			frame_end = loader.get_sets()[actionset][action][int(rotation)][file]

commit 5a68c6cbde728e31b93103965bac2ec2470d167d

Correct typo in `SQLiteAtlasLoader`
Introduced in ead8169a, a game cannot be started with atlas enabled.

horizons/util/loaders/sqliteatlasloader.py
@@ -75,7 +75,7 @@ class SQLiteAtlasLoader(object):
 		animationmanager = horizons.globals.fife.animationmanager
 
 		# if we've loaded that animation before, we can finish early
-		if animationmanager.exits(id):
+		if animationmanager.exists(id):
 			return animationmanager.getPtr(id)
 
 		ani = animationmanager.create(id)