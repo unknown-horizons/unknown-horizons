e9fc6823aabe33e7a8ea36ffba1d42c0a38221a1
MarkusHackspacher <hackspacher@gmx.de>
2017-03-01 23:02:15 +0100

    change to the `.format()` function
    Change to the .format() function in some file in the development/ directory.

--- development/time_uh.py
+++ development/time_uh.py
@@ -94,7 +94,7 @@ def show_data(games):
 		s %= 3600
 		m = s // 60
 		s %= 60
-		print(game.name, '%d:%02d:%06.3fs' % (h, m, s), game.returncode)
+		print(game.name, '[:d}:{:02d}:{:06.3f}s'.format(h, m, s), game.returncode)
 
 if __name__ == '__main__':
 	parser = PassThroughOptionParser()
