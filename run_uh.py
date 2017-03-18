d9f7daae78fd5009cef5ae4eb6f5b1265684cdba
Andy Mender <andymenderunix@gmail.com>
2017-03-18 11:25:05 +0100

    Change supported Python3 version to Python3.4 or higher.

--- run_uh.py
+++ run_uh.py
@@ -62,8 +62,8 @@ def exit_with_error(title, message):
 	exit(1)
 
 def check_python_version():
-	if sys.version_info[:2] != (3,4):
-		exit_with_error('Unsupported Python version', 'For now Python3.4 is explicitly required to run Unknown Horizons.')
+	if sys.version_info[:2] < (3,4):
+		exit_with_error('Unsupported Python version', 'Python3.4 or higher is required to run Unknown Horizons.')
 
 
 check_python_version()
