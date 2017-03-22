35b6c8d260c2552a67f8dbcdf11f1eb47d3d64ef
Andrzej Bylicki <andymenderunix@gmail.com>
2017-03-22 12:03:46 +0100

    Change module commentary to relate to py3 code.

--- horizons/i18n/__init__.py
+++ horizons/i18n/__init__.py
@@ -20,12 +20,12 @@
 # ###################################################
 
 """
-Maps _ to the ugettext unicode gettext call. Use: T(string).
-N_ takes care of plural forms for different languages. It masks ungettext
-calls (unicode, plural-aware T() ) to create different translation strings
-depending on the counter value. Not all languages have only two plural forms
-"One" / "Anything else". Use: N_("{n} dungeon", "{n} dungeons", n).format(n=n)
-where n is a counter.
+Maps _ to the gettext call. Use: T(string). N_ takes care of plural forms for 
+different languages. It masks ngettext calls (str, plural-aware T() ) to create 
+different translation strings depending on the counter value. Not all languages 
+have only two plural forms "One" / "Anything else". Use: N_("{n} dungeon", "{n} 
+dungeons", n).format(n=n) where n is a counter.
+
 We will need to make gettext recognize namespaces some time, but hardcoded
 'unknown-horizons' works for now since we currently only use one namespace.
 """
