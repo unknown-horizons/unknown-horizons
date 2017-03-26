049eaa3b419556e9ee1b96f091f410ff47b9371a
Andy Mender <andymenderunix@gmail.com>
2017-03-26 14:00:41 +0200

    Fix str encoding/decoding issue in horizons/util/yamlcachestorage.py.

--- horizons/util/yamlcachestorage.py
+++ horizons/util/yamlcachestorage.py
@@ -84,7 +84,6 @@ class YamlCacheStorage(object):
 			obj._reload()
 		except Exception as e:
 			# Ignore all exceptions because loading the cache from disk is not critical.
-			e = str(str(e), errors='replace')
 			cls.log.warning("Warning: Failed to open {0!s} as cache: {1!s}\nThis "
 				"warning is expected when upgrading from "
 				"old versions.\n".format(filename, e))
