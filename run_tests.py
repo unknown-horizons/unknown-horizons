ef014828af85c686e17591f8a06013009a760aec
Andy Mender <andymenderunix@gmail.com>
2017-04-02 17:22:02 +0200

    squidds fixes to run_tests.py and fife mocking.

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # ###################################################
-# Copyright (C) 2008-2017 The Unknown Horizons Team
+# Copyright (C) 2008-2016 The Unknown Horizons Team
 # team@unknown-horizons.org
 # This file is part of Unknown Horizons.
 #
@@ -22,9 +22,11 @@
 # ###################################################
 
 
+
 import gettext
 import sys
 
+from horizons.ext.dummy import Dummy
 
 try:
 	import nose
@@ -46,21 +48,22 @@ def mock_fife():
 	Using a custom import hook, we catch all imports of fife and provide a
 	dummy module.
 	"""
-	from tests.dummy import Dummy
+	from importlib.abc import MetaPathFinder, Loader
+	from importlib.machinery import PathFinder, ModuleSpec
 
-	class Importer(object):
-
-		def find_module(self, fullname, path=None):
+	class Finder(PathFinder):
+		@classmethod
+		def find_spec(cls, fullname, path, target=None):
 			if fullname.startswith('fife'):
-				return self
+				return ModuleSpec(fullname, DummyLoader())
+			return PathFinder.find_spec(fullname, path, target)
 
-			return None
+	class DummyLoader(Loader):
+		def load_module(self, module):
+			sys.modules.setdefault(module, Dummy())
 
-		def load_module(self, name):
-			mod = sys.modules.setdefault(name, Dummy())
-			return mod
+	sys.meta_path = [Finder]
 
-	#sys.meta_path = [Importer()]
 
 def setup_horizons():
 	"""
@@ -84,6 +87,8 @@ def setup_horizons():
 
 
 if __name__ == '__main__':
+	gettext.install('') # no translations here
+
 	setup_horizons()
 
 	from tests.gui import GuiTestPlugin
