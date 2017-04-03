706aaa7dcc82e295a2e4f4167e342ef9bd58aea0
Andy Mender <andymenderunix@gmail.com>
2017-04-03 20:37:24 +0200

    Fix use of built-in dir as custom variable name.

--- horizons/util/loaders/actionsetloader.py
+++ horizons/util/loaders/actionsetloader.py
@@ -43,11 +43,11 @@ class ActionSetLoader(object):
 	_loaded = False
 
 	@classmethod
-	def _find_action_sets(cls, dir):
+	def _find_action_sets(cls, Dir):
 		"""Traverses recursively starting from dir to find action sets.
 		It is similar to os.walk, but more optimized for this use case."""
-		for entry in os.listdir(dir):
-			full_path = os.path.join(dir, entry)
+		for entry in os.listdir(Dir):
+			full_path = os.path.join(Dir, entry)
 			if entry.startswith("as_"):
 				cls.action_sets[entry] = GeneralLoader._load_action(full_path)
 			else:
