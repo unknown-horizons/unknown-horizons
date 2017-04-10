5e52a7ddb0560a72e7e3b1674fd9ba284aab228f
Andrzej Bylicki <andymenderunix@gmail.com>
2017-04-10 08:12:19 +0200

    Fix badly placed parenthesis.

--- horizons/world/concreteobject.py
+++ horizons/world/concreteobject.py
@@ -169,7 +169,7 @@ class ConcreteObject(WorldObject):
 				# fall back to one from a higher level.
 				# this does not happen in valid games, but can happen in tests, when level
 				# constraints are ignored.
-				action_set, weight = next(list(action_sets.values()).items())
+				action_set, weight = next(list(action_sets.values().items()))
 
 		return action_set
 
