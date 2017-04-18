63c97c4d52463620f1927eb37ade10b4ab508b90
Sandra Grujovic <sandra.grujovic@tum.de>
2017-04-18 17:31:18 +0200

    add no-audio cmdline option

--- horizons/util/cmdlineoptions.py
+++ horizons/util/cmdlineoptions.py
@@ -64,6 +64,8 @@ def get_option_parser():
 	             help="Edit map <map>.")
 	start_uh.add_option("--edit-game-map", dest="edit_game_map", metavar="<game>",
 	             help="Edit the map from the saved game <game>.")
+	## FIXME: option needs to be added here.
+	start_uh.add_option("--no-audio", dest="no_audio", action= "store_true", help="Starts UH without sounds.")
 	p.add_option_group(start_uh)
 
 	ai_group = optparse.OptionGroup(p, "AI options")
