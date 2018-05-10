b39fe6c3484b5de49672878a5ac08f5d08c85ebf
Reiner Gerecke <me@reinergerecke.de>
2018-05-10 11:08:41 +0200

    Add missing import in gui test setup

--- tests/gui/__init__.py
+++ tests/gui/__init__.py
@@ -83,6 +83,7 @@ class TestRunner:
 	def _custom_setup(self):
 		"""Change build menu to 'per tier' for tests."""
 		import horizons.globals
+		from horizons.gui.tabs import BuildTab
 		horizons.globals.fife.set_uh_setting("Buildstyle", BuildTab.layer_per_tier_index)
 
 	def _filter_traceback(self):
