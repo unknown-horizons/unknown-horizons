acc35d6a5d4a2a0400e55322c3ec4f0064a44c40
shprotru <shprot@mail.ua>
2019-08-26 01:57:19 +0300

    settler level helptext translation

--- horizons/gui/tabs/mainsquaretabs.py
+++ horizons/gui/tabs/mainsquaretabs.py
@@ -132,7 +132,7 @@ class MainSquareSettlerLevelTab(MainSquareTab):
 	def __init__(self, instance):
 		self.max_inhabitants = instance.session.db.get_tier_inhabitants_max(self.__class__.LEVEL)
 		self.min_inhabitants = instance.session.db.get_tier_inhabitants_min(self.__class__.LEVEL)
-		self.helptext = instance.session.db.get_settler_name(self.__class__.LEVEL)
+		self.helptext = LazyT(instance.session.db.get_settler_name(self.__class__.LEVEL))
 
 		icon_path = 'icons/tabwidget/mainsquare/inhabitants{tier}'.format(tier=self.__class__.LEVEL)
 		super().__init__(instance=instance, icon_path=icon_path)
